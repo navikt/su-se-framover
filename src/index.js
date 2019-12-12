059270c9d9304f027ac6deec9eaa53c743c45fc4
Håkon <hakon.arneng.holmstedt@nav.no>
2019-12-12 14:44:27 +0100

    hello world

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Root from './components/Root.jsx';
+
+render(<Root />, document.getElementById("root"));
+
+/* eslint-disable no-undef */
+if (module.hot) {
+    module.hot.accept();
+}
+/* eslint-enable */
