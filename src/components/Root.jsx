a2ee9d3561b5d1ab3ce03b94d4ea37c959fb5a2e
Jacob Meidell <jacob.meidell@nav.no>
2019-12-16 07:49:08 +0100

    Fix issue with plugin-transform-runtime. Added function to test integration with backend. Env vars magically working out of the box?

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Root = () => {
+    const [data, setData] = useState({text: "", isFetching: false});
+
+    useEffect(() => {
+        console.log(process.env.SU_SE_BAKOVER_URL);
+        const fetchUsers = async () => {
+            try {
+                setData({isFetching: true});
+                const response = await fetch(process.env.SU_SE_BAKOVER_URL);
+                setData({text: await response.text(), isFetching: false});
+            } catch (e) {
+                console.log(e);
+                setData({isFetching: false});
+            }
+        };
+        fetchUsers();
+    }, []);
+
     return (
-        <h1>Hello world</h1>
+        <div>
+            <h1>Hello world!</h1>
+            {data.isFetching ? "fetching" : <h2>{data.text}</h2>}
+        </div>
     )
 }
 
