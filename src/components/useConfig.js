552c338cb7d5e5cc492cbdb2b0895f5f60511021
Håkon <hakon.arneng.holmstedt@nav.no>
2019-12-18 13:51:19 +0100

    clean up useConfig

--- src/components/useConfig.js
+++ src/components/useConfig.js
@@ -1,18 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useGet } from './useGet';
 
 export const useConfig = () => {
-    const [config, setConfig] = useState();
     const [url, setUrl] = useState(undefined);
     const data = useGet({ url });
-
+    let config
     if (data.data && data.isFetching == false) {
-        !config && setConfig(data.data);
-    }
-    if (process.env.NODE_ENV === "development") {
-        !config && setConfig({
-            "suSeBakoverUrl": "http://localhost:8080"
-        });
+        config = data.data;
+    } else if (process.env.NODE_ENV === "development") {
+        config = { "suSeBakoverUrl": "http://localhost:8080" }
     } else {
         !url && setUrl("/config.json");
     }
