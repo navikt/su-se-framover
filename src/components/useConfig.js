4329d0eeb66ed23f0662442ceabb367a7766eb64
Håkon <hakon.arneng.holmstedt@nav.no>
2019-12-19 09:31:27 +0100

    fjerner hack for local developlement via hardkoda env-variable. Legger på et langt mindre hack for å generere local config.

--- src/components/useConfig.js
+++ src/components/useConfig.js
@@ -7,8 +7,6 @@ export const useConfig = () => {
     let config
     if (data.data && data.isFetching == false) {
         config = data.data;
-    } else if (process.env.NODE_ENV === "development") {
-        config = { "suSeBakoverUrl": "http://localhost:8080" }
     } else {
         !url && setUrl("/config.json");
     }
