d3361dd315af98177d388af0c51d08fa9a832591
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2020-01-16 17:28:43 +0100

    Lokal utviklingsserver med hardkodet alt

---
+++ start-dev-server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd dev-server
+
+declare -r image_name=dev-server
+declare -r container_name=$image_name-burk
+
+docker build . -t $image_name
+docker kill $container_name &>/dev/null
+docker run -d --rm --name $container_name -p8080:8080 $image_name
