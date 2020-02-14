84a2552f0a666448b24bde59aa10aa6d5f66d940
Jacob Meidell <jacob.meidell@nav.no>
2020-02-14 08:05:19 +0100

    Display data from database.

--- src/pages/Saker.jsx
+++ src/pages/Saker.jsx
@@ -3,114 +3,24 @@ import 'nav-frontend-tabell-style';
 import Lenke from 'nav-frontend-lenker';
 import { Hovedknapp } from 'nav-frontend-knapper';
 import { useHistory } from 'react-router-dom';
+import { useGet } from '../hooks/useGet';
 
 function Saker() {
     const history = useHistory();
 
-    var saker = [];
-    saker.push({
-        sakid: '01234',
-        person: {
-            fnr: '12345678910',
-            fornavn: 'Navn',
-            etternavn: 'Navnesen',
-            adresse: {
-                postnr: '1000',
-                poststed: 'Oslo',
-                gatenavn: 'Gategata',
-                gatenr: '10'
-            },
-            sivilstand: {
-                status: 'Gift',
-                partner: 'Partner Partnersen 010101012351'
-            },
-            telefon: '123456789',
-            epost: {
-                epost: 'navn@navn.no',
-                metadata: {
-                    kilde: 'Kilde',
-                    oppdatert: '2016-01-01'
-                }
-            },
-            statsborgerskap: [
-                {
-                    land: 'ESP',
-                    fom: '2010-01-05'
-                },
-                {
-                    land: 'NOR',
-                    fom: '2019-07-06'
-                }
-            ],
-            foedsel: {
-                foedeland: 'ESP',
-                foedested: 'Barcelona',
-                fodselsedato: '1954-06-01'
-            },
-            opphold: [
-                {
-                    type: 'Midlertidig',
-                    fom: '2017-01-01',
-                    tom: '2018-01-01'
-                },
-                {
-                    type: 'Permanent',
-                    fom: '2018-01-01'
-                }
-            ],
-            folkeregisterpersonstatus: [
-                {
-                    status: 'Bosatt',
-                    fom: '2018-01-01'
-                },
-                {
-                    status: 'Utflyttet',
-                    fom: '2017-01-01',
-                    tom: '2017-01-01'
-                }
-            ]
-        },
-        status: 'Til behandling',
-        stonadsperioder: [
-            {
-                fom: '01.01.20',
-                tom: '01.01.21',
-                krav: [
-                    {
-                        type: 'Fortsettelse',
-                        mottatt: '05.01.20'
-                    }
-                ],
-                vedtak: []
-            },
-            {
-                fom: '01.01.19',
-                tom: '01.01.20',
-                krav: [
-                    {
-                        type: 'Førstegangssøknad',
-                        mottatt: '01.01.19'
-                    }
-                ],
-                vedtak: [
-                    {
-                        status: 'Iverksatt',
-                        årsak: 'Tilstått ytelse'
-                    }
-                ]
-            }
-        ]
-    });
+    const url = '/sak/list';
+    const { data } = useGet({ url });
+    const saker = data ? data : [];
     return (
         <>
             <table className="tabell">
                 <thead>
                     <tr>
                         <th role="columnheader" aria-sort="none">
-                            <Lenke href="#">Fnr</Lenke>
+                            <Lenke href="#">Sak</Lenke>
                         </th>
                         <th role="columnheader" aria-sort="none">
-                            <Lenke href="#">Status</Lenke>
+                            <Lenke href="#">Fnr</Lenke>
                         </th>
                         <th></th>
                     </tr>
@@ -119,10 +29,10 @@ function Saker() {
                     {saker.map((sak, index) => {
                         return (
                             <tr key={index}>
-                                <td>{sak.person.fnr}</td>
-                                <td>{sak.status}</td>
+                                <td>{sak.id}</td>
+                                <td>{sak.fnr}</td>
                                 <td>
-                                    <Hovedknapp onClick={() => history.push('/saksoversikt', saker[0])}>
+                                    <Hovedknapp onClick={() => history.push('/saksoversikt', saker[index])}>
                                         {'Behandle'}
                                     </Hovedknapp>
                                 </td>
