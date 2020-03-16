0eb5203de0b71d57dcf2fd18bd3ed705ca0fc9d9
Ramzi <ramzi.abu.qassim@nav.no>
2020-03-16 14:38:25 +0100

    Test som sjekker at når man trykker på flyktning radio buttons group, at de har den riktige verdien.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     moduleFileExtensions: ['js', 'jsx', 'json'],
     moduleNameMapper: {
-        'nav-(.*)-style': '<rootDir>/__mocks__/fileMock.js'
+        'nav-(.*)-style': '<rootDir>/__mocks__/fileMock.js',
+        '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/fileMock.js'
     },
     watchPathIgnorePatterns: ['<rootDir>/dist', '<rootDir>/node_modules']
 };
