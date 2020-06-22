a53b408740ba092a7087d1fc790d645a3b2041f6
Ingar Almklov <ingara@gmail.com>
2020-06-22 15:04:28 +0200

    Søknad: validering på uførevedtak

--- src/pages/søknad/steg/uførevedtak/Uførevedtak.tsx
+++ src/pages/søknad/steg/uførevedtak/Uførevedtak.tsx
@@ -1,42 +1,67 @@
 import * as React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, RawIntlProvider } from 'react-intl';
 import { useFormik } from 'formik';
 import Bunnknapper from '../../bunnknapper/Bunnknapper';
-import { Søknadsteg } from '../../types';
 import { JaNeiSpørsmål } from '~/components/FormElements';
 import { useAppSelector, useAppDispatch } from '~redux/Store';
 import søknadSlice from '~/features/søknad/søknadSlice';
 import messages from './uførevedtak-nb';
-import TextProvider, { Languages } from '~components/TextProvider';
 import sharedStyles from '../../steg-shared.module.less';
 import { Nullable } from '~lib/types';
+import { Feiloppsummering } from 'nav-frontend-skjema';
+import yup, { formikErrorsTilFeiloppsummering, formikErrorsHarFeil } from '~lib/validering';
+import { useHistory } from 'react-router-dom';
+import sharedI18n from '../steg-shared-i18n';
+import { useI18n } from '../../../../lib/hooks';
 
 interface FormData {
     harUførevedtak: Nullable<boolean>;
 }
 
-const Uførevedtak = () => {
+const schema = yup.object<FormData>({
+    harUførevedtak: yup
+        .boolean()
+        .nullable()
+        .required()
+});
+
+const Uførevedtak = (props: { forrigeUrl: string; nesteUrl: string }) => {
     const harVedtakFraStore = useAppSelector(s => s.soknad.harUførevedtak);
     const dispatch = useAppDispatch();
-
+    const history = useHistory();
     const formik = useFormik<FormData>({
         initialValues: {
             harUførevedtak: harVedtakFraStore
         },
         onSubmit: values => {
             dispatch(søknadSlice.actions.harUførevedtakUpdated(values.harUførevedtak));
-        }
+            history.push(props.nesteUrl);
+        },
+        validationSchema: schema
     });
+
+    const feiloppsummeringref = React.useRef<HTMLDivElement>(null);
+
+    const intl = useI18n({ messages: { ...sharedI18n, ...messages } });
 
     return (
         <div className={sharedStyles.container}>
-            <TextProvider messages={{ [Languages.nb]: messages }}>
-                <form onSubmit={formik.handleSubmit}>
+            <RawIntlProvider value={intl}>
+                <form
+                    onSubmit={e => {
+                        formik.handleSubmit(e);
+                        setTimeout(() => {
+                            if (feiloppsummeringref.current) {
+                                feiloppsummeringref.current.focus();
+                            }
+                        }, 0);
+                    }}
+                >
                     <div className={sharedStyles.formContainer}>
                         <JaNeiSpørsmål
                             id="harUførevedtak"
                             legend={<FormattedMessage id="input.uforevedtak.label" />}
-                            feil={null}
+                            feil={formik.errors.harUførevedtak}
                             state={formik.values.harUførevedtak}
                             onChange={e =>
                                 formik.setValues({
@@ -46,17 +71,26 @@ const Uførevedtak = () => {
                             }
                         />
                     </div>
+                    <div>
+                        <Feiloppsummering
+                            className={sharedStyles.feiloppsummering}
+                            tittel={intl.formatMessage({ id: 'feiloppsummering.title' })}
+                            feil={formikErrorsTilFeiloppsummering(formik.errors)}
+                            hidden={!formikErrorsHarFeil(formik.errors)}
+                            innerRef={feiloppsummeringref}
+                        />
+                    </div>
 
                     <Bunnknapper
-                        next={{
+                        previous={{
                             onClick: () => {
                                 dispatch(søknadSlice.actions.harUførevedtakUpdated(formik.values.harUførevedtak));
-                            },
-                            steg: Søknadsteg.FlyktningstatusOppholdstillatelse
+                                history.push(props.forrigeUrl);
+                            }
                         }}
                     />
                 </form>
-            </TextProvider>
+            </RawIntlProvider>
         </div>
     );
 };
