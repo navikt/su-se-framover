cb8b0aa70864bfa885550e841b2202b05b05e296
Ingar Almklov <ingara@gmail.com>
2020-06-22 15:07:02 +0200

    Søknad: fiks forrige- og neste-knappene på kontakt

--- src/pages/søknad/steg/kontakt/Kontakt.tsx
+++ src/pages/søknad/steg/kontakt/Kontakt.tsx
@@ -1,28 +1,29 @@
 import * as React from 'react';
 
-import { Søknadsteg } from '../../types';
 import Bunnknapper from '../../bunnknapper/Bunnknapper';
 import messages from './kontakt-nb';
 import TextProvider, { Languages } from '~components/TextProvider';
+import { useHistory } from 'react-router-dom';
 
-const Kontakt = () => {
+const Kontakt = (props: { forrigeUrl: string; nesteUrl: string }) => {
+    const history = useHistory();
+
     return (
         <TextProvider messages={{ [Languages.nb]: messages }}>
             <div>
-                <Bunnknapper
-                    previous={{
-                        onClick: () => {
-                            console.log('next');
-                        },
-                        steg: Søknadsteg.ReiseTilUtlandet
+                <form
+                    onSubmit={() => {
+                        history.push(props.nesteUrl);
                     }}
-                    next={{
-                        onClick: () => {
-                            console.log('next');
-                        },
-                        steg: Søknadsteg.Oppsummering
-                    }}
-                />
+                >
+                    <Bunnknapper
+                        previous={{
+                            onClick: () => {
+                                history.push(props.forrigeUrl);
+                            }
+                        }}
+                    />
+                </form>
             </div>
         </TextProvider>
     );
