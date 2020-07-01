5e942f4b525be1a011514808494a71930125a7c3
Ingar Almklov <ingara@gmail.com>
2020-07-01 12:53:29 +0200

    Saksoversikt: forbedre håndtering av hentetilstander

--- src/pages/saksoversikt/Saksoversikt.tsx
+++ src/pages/saksoversikt/Saksoversikt.tsx
@@ -5,7 +5,6 @@ import AlertStripe from 'nav-frontend-alertstriper';
 
 import * as RemoteData from '@devexperts/remote-data-ts';
 import { PersonCard } from '@navikt/nap-person-card';
-import { Header } from '@navikt/nap-header';
 import SideMenu from '@navikt/nap-side-menu';
 
 import NavFrontendSpinner from 'nav-frontend-spinner';
@@ -31,87 +30,89 @@ const Saksoversikt = () => {
 
     return (
         <IntlProvider locale={Languages.nb} messages={messages}>
-            <Header title="Supplerende stønad Ufør" titleHref={'/'}>
-                <div className={styles.headerContainer}>
+            {!RemoteData.isSuccess(søkerFraStore) && (
+                <div>
                     <Søkefelt />
+                    {RemoteData.isPending(søkerFraStore) && <NavFrontendSpinner />}
+                    {RemoteData.isFailure(søkerFraStore) && (
+                        <AlertStripe type="feil">{søkerFraStore.error.message}</AlertStripe>
+                    )}
                 </div>
-            </Header>
+            )}
 
             {pipe(
                 RemoteData.combine(søkerFraStore, sakFraStore),
-                RemoteData.fold(
-                    () => <NavFrontendSpinner />,
-                    () => <NavFrontendSpinner />,
-                    (error) => <AlertStripe type="feil">{error.message}</AlertStripe>,
-                    ([data, sak]) => (
-                        <>
+                RemoteData.map(([data, sak]) => (
+                    <>
+                        <div className={styles.headerContainer}>
                             <PersonCard
                                 fodselsnummer={data.fnr}
                                 gender="unknown"
                                 name={`${data.fornavn} ${data.etternavn}`}
-                                isActive
                             />
-                            <div className={styles.container}>
-                                <SideMenu
-                                    links={[
-                                        {
-                                            label: 'Sak',
-                                            active: meny === undefined,
-                                        },
-                                        {
-                                            label: 'Søknad',
-                                            active: meny === SaksbehandligMenyValg.Søknad,
-                                        },
-                                        {
-                                            label: 'Vilkår',
-                                            active: meny === SaksbehandligMenyValg.Vilkår,
-                                        },
-                                        {
-                                            label: 'Behandlig',
-                                            active: meny === SaksbehandligMenyValg.Behandlig,
-                                        },
-                                        {
-                                            label: 'Vedtak',
-                                            active: meny === SaksbehandligMenyValg.Vedtak,
-                                        },
-                                    ]}
-                                    onClick={(index) => {
-                                        switch (index) {
-                                            case 0:
-                                                history.push(`/saksoversikt`);
-                                                return;
-                                            case 1:
-                                                history.push(`/saksoversikt/soknad`);
-                                                return;
-                                            case 2:
-                                                history.push(`/saksoversikt/vilkar`);
-                                                return;
-                                            case 3:
-                                                history.push(`/saksoversikt/behandling`);
-                                                return;
-                                            case 4:
-                                                history.push(`/saksoversikt/vedtak`);
-                                                return;
-                                        }
-                                    }}
-                                />
-                                <div className={styles.mainContent}>
-                                    <Switch>
-                                        <Route path={`${path}/${SaksbehandligMenyValg.Søknad}`}>soknad</Route>
-                                        <Route path={`${path}/${SaksbehandligMenyValg.Vilkår}`}>
-                                            <Vilkår />
-                                        </Route>
-                                        <Route path={`${path}/${SaksbehandligMenyValg.Behandlig}`}>behandling</Route>
-                                        <Route path={`${path}/${SaksbehandligMenyValg.Vedtak}`}>vedtak</Route>
-                                        <Route path={path}>
-                                            <Sakintro sak={sak} />
-                                        </Route>
-                                    </Switch>
-                                </div>
+                            <Søkefelt />
+                        </div>
+                        <div className={styles.container}>
+                            <SideMenu
+                                links={[
+                                    {
+                                        label: 'Sak',
+                                        active: meny === undefined,
+                                    },
+                                    {
+                                        label: 'Søknad',
+                                        active: meny === SaksbehandligMenyValg.Søknad,
+                                    },
+                                    {
+                                        label: 'Vilkår',
+                                        active: meny === SaksbehandligMenyValg.Vilkår,
+                                    },
+                                    {
+                                        label: 'Behandlig',
+                                        active: meny === SaksbehandligMenyValg.Behandlig,
+                                    },
+                                    {
+                                        label: 'Vedtak',
+                                        active: meny === SaksbehandligMenyValg.Vedtak,
+                                    },
+                                ]}
+                                onClick={(index) => {
+                                    switch (index) {
+                                        case 0:
+                                            history.push(`/saksoversikt`);
+                                            return;
+                                        case 1:
+                                            history.push(`/saksoversikt/soknad`);
+                                            return;
+                                        case 2:
+                                            history.push(`/saksoversikt/vilkar`);
+                                            return;
+                                        case 3:
+                                            history.push(`/saksoversikt/behandling`);
+                                            return;
+                                        case 4:
+                                            history.push(`/saksoversikt/vedtak`);
+                                            return;
+                                    }
+                                }}
+                            />
+                            <div className={styles.mainContent}>
+                                <Switch>
+                                    <Route path={`${path}/${SaksbehandligMenyValg.Søknad}`}>heisann</Route>
+                                    <Route path={`${path}/${SaksbehandligMenyValg.Vilkår}`}>
+                                        <Vilkår sak={sak} />
+                                    </Route>
+                                    <Route path={`${path}/${SaksbehandligMenyValg.Behandlig}`}>behandling</Route>
+                                    <Route path={`${path}/${SaksbehandligMenyValg.Vedtak}`}>vedtak</Route>
+                                    <Route path={path}>
+                                        <Sakintro sak={sak} />
+                                    </Route>
+                                </Switch>
                             </div>
-                        </>
-                    )
-                )
+                        </div>
+                    </>
+                )),
+                RemoteData.getOrElse(() => <span />)
             )}
         </IntlProvider>
     );
