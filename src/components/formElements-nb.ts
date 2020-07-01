597f8223b8d01aff78937810670d434756a1d460
Ingar Almklov <ingara@gmail.com>
2020-07-01 12:52:34 +0200

    JaNeiSpørsmål: fiks tekst-ID-er

--- src/components/formElements-nb.ts
+++ src/components/formElements-nb.ts
@@ -1,5 +1,5 @@
 export default {
-    'janeisporsmal.label.ja': 'Ja',
-    'janeisporsmal.label.nei': 'Nei',
+    'jaNeiSpørsmal.label.ja': 'Ja',
+    'jaNeiSpørsmal.label.nei': 'Nei',
     'anbefalerIkkeSøke.message': 'Du kan fremdeles søke, men du vil sannsynligvis få avslag.',
 };
