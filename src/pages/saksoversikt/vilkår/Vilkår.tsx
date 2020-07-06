ba128050acc9099f0e33275635a00681629de2a3
Ingar Almklov <ingara@gmail.com>
2020-07-06 15:22:00 +0200

    Vilkårsvurdering: legg til visning for alle vilkårene
    En god del TODOs og ting som må fylles inn, men dette er å anse som en start og
    forslag til kodestruktur.

--- src/pages/saksoversikt/vilkår/Vilkår.tsx
+++ src/pages/saksoversikt/vilkår/Vilkår.tsx
@@ -2,11 +2,32 @@ import React from 'react';
 import { Undertittel } from 'nav-frontend-typografi';
 
 import Vilkårsvurdering from '../vilkårsvurdering/Vilkårsvurdering';
-import WarningIcon from '../icons/WarningIcon';
 import styles from './vilkår.module.less';
 import { Behandling, Vilkårtype } from '~api/behandlingApi';
-import { Sak } from '~api/sakApi';
 import { Søknad } from '~api/søknadApi';
+import { Nullable } from '~lib/types';
+import { Sak } from '~api/sakApi';
+
+const boolTilJaNei = (val: Nullable<boolean>) => {
+    if (val === null) {
+        return '-';
+    }
+    return val ? 'Ja' : 'Nei';
+};
+
+const showNumber = (val: Nullable<number>) => {
+    if (val === null) {
+        return '-';
+    }
+    return val.toString();
+};
+
+const Infolinje = (props: { tittel: string; verdi: React.ReactNode }) => (
+    <div className={styles.infolinje}>
+        <span className={styles.infotittel}>{props.tittel}:</span>
+        <span>{props.verdi}</span>
+    </div>
+);
 
 const VilkårInnhold = (props: { behandling: Behandling; søknad: Søknad }) => {
     const { vilkårsvurderinger } = props.behandling;
@@ -15,15 +36,123 @@ const VilkårInnhold = (props: { behandling: Behandling; søknad: Søknad }) => 
             <Vilkårsvurdering
                 title="Uførhet"
                 paragraph="§ 12-4 - § 12-8"
-                icon={<WarningIcon />}
                 legend="Har søker fått vedtak om uføretrygd der vilkårene i §12-4 til §12-8 i folketrygdloven er oppfylt?"
-                status={vilkårsvurderinger[Vilkårtype.Uførhet].status}
-                begrunnelse={vilkårsvurderinger[Vilkårtype.Uførhet].begrunnelse}
+                vilkårsvurdering={vilkårsvurderinger[Vilkårtype.Uførhet]}
                 onSaveClick={console.log}
+                className={styles.vilkarsvurdering}
             >
                 <div>
                     <Undertittel>Fra søknad</Undertittel>
-                    <p>Har uførevedtak: {props.søknad.uførevedtak.harUførevedtak.toString()}</p>
+                    <Infolinje tittel="Har uførevedtak" verdi={boolTilJaNei(props.søknad.uførevedtak.harUførevedtak)} />
+                </div>
+            </Vilkårsvurdering>
+            <Vilkårsvurdering
+                title="Flyktning"
+                paragraph="§ 28"
+                legend="Er søker registrert flyktning?"
+                vilkårsvurdering={vilkårsvurderinger[Vilkårtype.Flyktning]}
+                onSaveClick={console.log}
+                className={styles.vilkarsvurdering}
+            >
+                <div>
+                    <Undertittel>Fra søknad</Undertittel>
+                    <Infolinje
+                        tittel="Registrert flyktning"
+                        verdi={boolTilJaNei(props.søknad.flyktningsstatus.registrertFlyktning)}
+                    />
+                </div>
+            </Vilkårsvurdering>
+            <Vilkårsvurdering
+                title="Opphold"
+                paragraph="§ 3"
+                legend="Er søker norsk statsborger?"
+                vilkårsvurdering={vilkårsvurderinger[Vilkårtype.Oppholdstillatelse]}
+                onSaveClick={console.log}
+                className={styles.vilkarsvurdering}
+            >
+                <div>
+                    <Undertittel>Fra søknad</Undertittel>
+                    <Infolinje
+                        tittel="Norsk statsborger"
+                        verdi={boolTilJaNei(props.søknad.oppholdstillatelse.erNorskStatsborger)}
+                    />
+                    <Infolinje
+                        tittel="Har oppholdstillatelse"
+                        verdi={
+                            <span>
+                                {boolTilJaNei(props.søknad.oppholdstillatelse.harOppholdstillatelse)}.{' '}
+                                {props.søknad.oppholdstillatelse.typeOppholdstillatelse}
+                            </span>
+                        }
+                    />
+                    {props.søknad.oppholdstillatelse.statsborgerskapAndreLand && (
+                        <Infolinje
+                            tittel="Statsborgerskap i andre land"
+                            verdi={props.søknad.oppholdstillatelse.statsborgerskapAndreLandFritekst ?? ''}
+                        />
+                    )}
+                    <Infolinje
+                        tittel="Oppholdstillatelse forelengelse"
+                        verdi={boolTilJaNei(props.søknad.oppholdstillatelse.oppholdstillatelseForlengelse)}
+                    />
+                    <Infolinje
+                        tittel="Oppholdstillatelse mindre enn tre måneder"
+                        verdi={boolTilJaNei(props.søknad.oppholdstillatelse.oppholdstillatelseMindreEnnTreMåneder)}
+                    />
+                </div>
+            </Vilkårsvurdering>
+            <Vilkårsvurdering
+                title="Personlig oppmøte"
+                paragraph="§ 17"
+                legend="TODO"
+                vilkårsvurdering={vilkårsvurderinger[Vilkårtype.PersonligOppmøte]}
+                onSaveClick={console.log}
+                className={styles.vilkarsvurdering}
+            >
+                <div>
+                    <Undertittel>Fra søknad</Undertittel>
+                    <p>TODO</p>
+                </div>
+            </Vilkårsvurdering>
+            <Vilkårsvurdering
+                title="Formue"
+                paragraph="§ 8"
+                legend="TODO"
+                vilkårsvurdering={vilkårsvurderinger[Vilkårtype.Formue]}
+                onSaveClick={console.log}
+                className={styles.vilkarsvurdering}
+            >
+                <div>
+                    <Undertittel>Fra søknad</Undertittel>
+                    <Infolinje tittel="Bor i bolig" verdi={boolTilJaNei(props.søknad.formue.borIBolig)} />
+                    <Infolinje tittel="Verdi på bolig" verdi={showNumber(props.søknad.formue.verdiPåBolig)} />
+                    <Infolinje tittel="Bolig brukes til" verdi={props.søknad.formue.boligBrukesTil ?? ''} />
+                    <Infolinje tittel="Depositumsbeløp" verdi={showNumber(props.søknad.formue.depositumsBeløp)} />
+                    <Infolinje tittel="Kontonummer" verdi={props.søknad.formue.kontonummer ?? ''} />
+                    <Infolinje tittel="Verdi på eiendom" verdi={showNumber(props.søknad.formue.verdiPåEiendom)} />
+                    <Infolinje tittel="Eiendom brukes til" verdi={props.søknad.formue.eiendomBrukesTil ?? ''} />
+                    <Infolinje tittel="Kjøretøy" verdi={props.søknad.formue.kjøretøyDeEier ?? ''} />
+                    <Infolinje tittel="Verdi på kjøretøy" verdi={showNumber(props.søknad.formue.verdiPåKjøretøy)} />
+                    <Infolinje tittel="Innskuddsbeløp" verdi={showNumber(props.søknad.formue.innskuddsBeløp)} />
+                    <Infolinje tittel="Verdipapirbeløp" verdi={showNumber(props.søknad.formue.verdipapirBeløp)} />
+                    <Infolinje
+                        tittel="Penger folk skylder søker"
+                        verdi={showNumber(props.søknad.formue.skylderNoenMegPengerBeløp)}
+                    />
+                    <Infolinje tittel="Kontanter" verdi={showNumber(props.søknad.formue.kontanterBeløp)} />
+                </div>
+            </Vilkårsvurdering>
+            <Vilkårsvurdering
+                title="Boforhold/sivilstatus"
+                paragraph="§ 5"
+                legend="TODO"
+                vilkårsvurdering={vilkårsvurderinger[Vilkårtype.BorOgOppholderSegINorge]}
+                onSaveClick={console.log}
+                className={styles.vilkarsvurdering}
+            >
+                <div>
+                    <Undertittel>Fra søknad</Undertittel>
+                    <p>{props.søknad.boforhold.borOgOppholderSegINorge}</p>
                 </div>
             </Vilkårsvurdering>
         </div>
--- src/pages/saksoversikt/vilkårsvurdering/Vilkårsvurdering.tsx
+++ src/pages/saksoversikt/vilkårsvurdering/Vilkårsvurdering.tsx
@@ -1,63 +1,83 @@
 import React from 'react';
 import { useFormik } from 'formik';
 import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
-import { Undertittel } from 'nav-frontend-typografi';
+import { Undertittel, EtikettLiten } from 'nav-frontend-typografi';
 import { Hovedknapp } from 'nav-frontend-knapper';
 import { guid } from 'nav-frontend-js-utils';
 import { Textarea } from 'nav-frontend-skjema';
-import Etikett from 'nav-frontend-etiketter';
 
 import { JaNeiSpørsmål } from '~components/FormElements';
-import * as behandlingApi from '~api/behandlingApi';
 import { Nullable } from '~lib/types';
 
 import styles from './vilkårsvurdering.module.less';
+import { VilkårVurderingStatus, Vilkårsvurdering } from '~api/behandlingApi';
+import Ikon from 'nav-frontend-ikoner-assets';
+
 interface FormData {
     vurdering: Nullable<boolean>;
     begrunnelse: Nullable<string>;
 }
 
-const vilkårVurderingStatusTilJaNeiSpørsmål = (status: behandlingApi.VilkårVurderingStatus): Nullable<boolean> => {
+const vilkårVurderingStatusTilJaNeiSpørsmål = (status: VilkårVurderingStatus): Nullable<boolean> => {
     switch (status) {
-        case behandlingApi.VilkårVurderingStatus.Ok:
+        case VilkårVurderingStatus.Ok:
             return true;
-        case behandlingApi.VilkårVurderingStatus.IkkeOk:
+        case VilkårVurderingStatus.IkkeOk:
             return false;
-        case behandlingApi.VilkårVurderingStatus.IkkeVurdert:
+        case VilkårVurderingStatus.IkkeVurdert:
             return null;
+    }
+};
+
+const boolTilVilkårvurderingStatus = (val: Nullable<boolean>): VilkårVurderingStatus => {
+    if (val === null) {
+        return VilkårVurderingStatus.IkkeVurdert;
+    }
+    return val ? VilkårVurderingStatus.Ok : VilkårVurderingStatus.IkkeOk;
+};
+
+const statusIcon = (status: VilkårVurderingStatus) => {
+    switch (status) {
+        case VilkårVurderingStatus.IkkeVurdert:
+            return <Ikon kind="advarsel-sirkel-fyll" />;
+        case VilkårVurderingStatus.IkkeOk:
+            return <Ikon kind="feil-sirkel-fyll" />;
+        case VilkårVurderingStatus.Ok:
+            return <Ikon kind="ok-sirkel-fyll" />;
     }
 };
 
 const Vilkårsvurdering: React.FC<{
     title: string | React.ReactNode;
     paragraph: string;
-    icon: JSX.Element;
+    vilkårsvurdering: Vilkårsvurdering;
     legend: string | React.ReactNode;
     children?: React.ReactChild;
-    status: behandlingApi.VilkårVurderingStatus;
-    begrunnelse: Nullable<string>;
-    onSaveClick: (svar: { status: behandlingApi.VilkårVurderingStatus; begrunnelse: Nullable<string> }) => void;
+    className?: string;
+    onSaveClick: (svar: { status: VilkårVurderingStatus; begrunnelse: Nullable<string> }) => void;
 }> = (props) => {
     const formik = useFormik<FormData>({
         initialValues: {
-            vurdering: vilkårVurderingStatusTilJaNeiSpørsmål(props.status),
-            begrunnelse: props.begrunnelse,
+            vurdering: vilkårVurderingStatusTilJaNeiSpørsmål(props.vilkårsvurdering.status),
+            begrunnelse: props.vilkårsvurdering.begrunnelse,
         },
         onSubmit: (values) => {
-            console.log(values); // TODO send inn vurdering
+            props.onSaveClick({
+                status: boolTilVilkårvurderingStatus(values.vurdering),
+                begrunnelse: values.begrunnelse,
+            });
         },
     });
 
     return (
         <Ekspanderbartpanel
+            className={props.className}
             tittel={
-                <div className={styles.tittel}>
-                    <span className={styles.tittelikon}>{props.icon}</span>
+                <div className={styles.tittelContainer}>
+                    <span className={styles.tittelikon}>{statusIcon(props.vilkårsvurdering.status)}</span>
                     <div className={styles.titteltekster}>
-                        <Undertittel>{props.title}</Undertittel>
-                        <Etikett type="info" mini>
-                            {props.paragraph}
-                        </Etikett>
+                        <Undertittel className={styles.tittel}>{props.title}</Undertittel>
+                        <EtikettLiten className={styles.paragraf}>{props.paragraph}</EtikettLiten>
                     </div>
                 </div>
             }
@@ -70,13 +90,16 @@ const Vilkårsvurdering: React.FC<{
                         feil={null}
                         state={formik.values.vurdering}
                         onChange={(val) => formik.setValues({ ...formik.values, vurdering: val })}
+                        className={styles.vurdering}
                     />
-                    <Textarea
-                        label="Begrunnelse"
-                        name="begrunnelse"
-                        value={formik.values.begrunnelse ?? ''}
-                        onChange={formik.handleChange}
-                    />
+                    <div className={styles.begrunnelse}>
+                        <Textarea
+                            label="Begrunnelse"
+                            name="begrunnelse"
+                            value={formik.values.begrunnelse ?? ''}
+                            onChange={formik.handleChange}
+                        />
+                    </div>
                     <Hovedknapp>Lagre</Hovedknapp>
                 </form>
                 <div className={styles.grunnlag}>{props.children}</div>
