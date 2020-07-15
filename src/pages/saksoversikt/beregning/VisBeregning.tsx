6f0b95bb8abf22e5346cf31cf47431eabed9d107
Ingar Almklov <ingara@gmail.com>
2020-07-15 08:22:18 +0200

    Beregning: vis totalbeløp

--- src/pages/saksoversikt/beregning/VisBeregning.tsx
+++ src/pages/saksoversikt/beregning/VisBeregning.tsx
@@ -20,14 +20,16 @@ const InfoLinje = (props: { tittel: string; value: string | number }) => (
 const VisBeregning = (props: Props) => {
     const intl = useI18n({ messages });
     const { beregning } = props;
+    const totalbeløp = beregning.månedsberegninger.reduce((acc, val) => acc + val.beløp, 0);
     return (
         <div className={styles.visBeregning}>
             <Innholdstittel>Beregning:</Innholdstittel>
             <InfoLinje tittel={'id:'} value={beregning.id} />
             <InfoLinje tittel={'opprettet:'} value={formatDateTime(beregning.opprettet, intl)} />
             <InfoLinje tittel={'sats:'} value={beregning.sats} />
-            <InfoLinje tittel={'Start dato:'} value={intl.formatDate(beregning.fom)} />
-            <InfoLinje tittel={'Slutt dato:'} value={intl.formatDate(beregning.tom)} />
+            <InfoLinje tittel={'Startdato:'} value={intl.formatDate(beregning.fom)} />
+            <InfoLinje tittel={'Sluttdato:'} value={intl.formatDate(beregning.tom)} />
+            <InfoLinje tittel="Totalbeløp:" value={intl.formatNumber(totalbeløp, { currency: 'NOK' })} />
             <table className="tabell">
                 <thead>
                     <th></th>
