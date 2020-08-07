cdc13bc86efb47873487e43558c1a7fa77a653f7
Ingar Almklov <ingara@gmail.com>
2020-08-07 10:52:35 +0200

    Søknad: forbedre søking på person i inngangen

--- src/pages/søknad/steg/inngang/Inngang.tsx
+++ src/pages/søknad/steg/inngang/Inngang.tsx
@@ -1,7 +1,9 @@
 import * as RemoteData from '@devexperts/remote-data-ts';
-import { useFormik } from 'formik';
+import fnrValidator from '@navikt/fnrvalidator';
+import AlertStripe from 'nav-frontend-alertstriper';
+import { Søkeknapp } from 'nav-frontend-ikonknapper';
 import { Hovedknapp } from 'nav-frontend-knapper';
-import { Input, Feiloppsummering } from 'nav-frontend-skjema';
+import { Input, Label, SkjemaelementFeilmelding } from 'nav-frontend-skjema';
 import NavFrontendSpinner from 'nav-frontend-spinner';
 import * as React from 'react';
 import { FormattedMessage, RawIntlProvider } from 'react-intl';
@@ -9,19 +11,15 @@ import { useHistory } from 'react-router-dom';
 
 import { Personkort } from '~components/Personkort';
 import * as personSlice from '~features/person/person.slice';
-import yup, { formikErrorsTilFeiloppsummering, formikErrorsHarFeil } from '~lib/validering';
+import { pipe } from '~lib/fp';
+import { useI18n } from '~lib/hooks';
 import { useAppDispatch, useAppSelector } from '~redux/Store';
 
-import { useI18n } from '../../../../lib/hooks';
 import sharedStyles from '../../steg-shared.module.less';
 import sharedI18n from '../steg-shared-i18n';
 
 import nb from './inngang-nb';
 import styles from './inngang.module.less';
-
-interface FormData {
-    fnr: string;
-}
 
 const index = (props: { nesteUrl: string }) => {
     const { søker } = useAppSelector((s) => s.søker);
@@ -31,79 +29,98 @@ const index = (props: { nesteUrl: string }) => {
 
     const intl = useI18n({ messages: { ...sharedI18n, ...nb } });
 
-    const schema = yup.object<FormData>({
-        fnr: yup
-            .string()
-            .length(11)
-            .label(intl.formatMessage({ id: 'input.fnr.label' }))
-            .required(),
-    });
+    const [fnr, setFnr] = React.useState('');
 
-    const formik = useFormik<FormData>({
-        initialValues: {
-            fnr: '',
-        },
-        onSubmit: async () => {
-            history.push(props.nesteUrl);
-        },
-        validationSchema: schema,
-        validateOnChange: hasSubmitted,
-    });
-
-    const feiloppsummeringref = React.useRef<HTMLDivElement>(null);
+    const [fnrValidation, setFnrValidation] = React.useState<ValidationResult | null>(null);
 
     React.useEffect(() => {
-        if (formik.values.fnr.length === 11) {
-            dispatch(personSlice.fetchPerson({ fnr: formik.values.fnr }));
-        } else if (formik.values.fnr.length !== 11 && søker) {
+        const validation = fnrValidator.fnr(fnr);
+        setFnrValidation(validation);
+        if (fnr.length !== 11) {
             dispatch(personSlice.default.actions.resetSøker());
         }
-    }, [formik.values.fnr]);
+    }, [fnr]);
 
+    const handleSubmit = () => {
+        setHasSubmitted(true);
+        const validation = fnrValidator.fnr(fnr);
+        setFnrValidation(validation);
+
+        if (validation.status === 'valid') {
+            dispatch(personSlice.fetchPerson({ fnr }));
+        }
+    };
+
+    const handleStartSøknadClick = () => {
+        if (RemoteData.isSuccess(søker)) {
+            history.push(props.nesteUrl);
+        }
+    };
+
+    const lagFeilmelding = (error: ErrorReason) => {
+        switch (error) {
+            case 'fnr or dnr must consist of 11 digits':
+                return intl.formatMessage({ id: 'feilmelding.lengde' });
+            case "checksums don't match":
+            case 'invalid date':
+                return intl.formatMessage({ id: 'feilmelding.ugyldig' });
+        }
+    };
     return (
         <RawIntlProvider value={intl}>
             <div className={sharedStyles.container}>
                 <form
+                    className={styles.formContainer}
                     onSubmit={(e) => {
-                        setHasSubmitted(true);
-                        formik.handleSubmit(e);
-                        setTimeout(() => {
-                            if (feiloppsummeringref.current) {
-                                feiloppsummeringref.current.focus();
-                            }
-                        }, 0);
+                        e.stopPropagation();
+                        e.preventDefault();
+                        handleSubmit();
                     }}
                 >
                     <div className={styles.inputContainer}>
-                        <Input
-                            id="fnr"
-                            name="fnr"
-                            label={<FormattedMessage id={'input.fnr.label'} />}
-                            onChange={formik.handleChange}
-                            value={formik.values.fnr}
-                            feil={formik.errors.fnr}
-                        />
-                        {RemoteData.isPending(søker) && <NavFrontendSpinner />}
-                        {RemoteData.isSuccess(søker) && (
-                            <div className={styles.personkortWrapper}>
-                                <Personkort person={søker.value} />
-                            </div>
+                        <Label htmlFor="fnr">
+                            <FormattedMessage id={'input.fnr.label'} />
+                        </Label>
+                        <div className={styles.inputfeltcontainer}>
+                            <Input
+                                id="fnr"
+                                name="fnr"
+                                className={styles.inputfelt}
+                                onChange={(e) => {
+                                    setFnr(e.target.value);
+                                }}
+                                value={fnr}
+                            />
+                            <Søkeknapp htmlType="submit">
+                                <FormattedMessage id="knapp.søk" />
+                            </Søkeknapp>
+                        </div>
+                        <SkjemaelementFeilmelding>
+                            {hasSubmitted && fnrValidation?.status === 'invalid'
+                                ? lagFeilmelding(fnrValidation.reasons[0])
+                                : undefined}{' '}
+                        </SkjemaelementFeilmelding>
+                    </div>
+                    <div className={styles.personkortWrapper}>
+                        {pipe(
+                            søker,
+                            RemoteData.fold(
+                                () => null,
+                                () => <NavFrontendSpinner />,
+                                (err) => <AlertStripe type="feil">{err.message}</AlertStripe>,
+                                (s) => <Personkort person={s} />
+                            )
                         )}
                     </div>
+                </form>
 
-                    <Feiloppsummering
-                        className={styles.feiloppsummering}
-                        tittel={intl.formatMessage({ id: 'feiloppsummering.title' })}
-                        feil={formikErrorsTilFeiloppsummering(formik.errors)}
-                        hidden={!formikErrorsHarFeil(formik.errors)}
-                        innerRef={feiloppsummeringref}
-                    />
+                {RemoteData.isSuccess(søker) && (
                     <div className={styles.successknapper}>
-                        <Hovedknapp>
+                        <Hovedknapp htmlType="button" onClick={handleStartSøknadClick}>
                             <FormattedMessage id="knapp.startSøknad" />
                         </Hovedknapp>
                     </div>
-                </form>
+                )}
             </div>
         </RawIntlProvider>
     );
