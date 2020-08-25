adee75dc6faf858a190ba5bd6e610e717e2f7d5c
Ingar Almklov <ingara@gmail.com>
2020-08-25 10:10:37 +0200

    Add Amplitude client

---
+++ src/lib/tracking/amplitude.ts
@@ -0,0 +1,29 @@
+import amplitude from 'amplitude-js';
+
+let amplitudeClient: amplitude.AmplitudeClient | null = null;
+
+export function init() {
+    if (amplitudeClient !== null) {
+        throw new Error('Amplitude has already been initialized');
+    }
+
+    amplitudeClient = amplitude.getInstance();
+    amplitudeClient.init(window.AMPLITUDE_API_KEY, undefined, {
+        apiEndpoint: 'amplitude.nav.no/collect',
+        saveEvents: true,
+        includeUtm: true,
+        includeReferrer: true,
+        trackingOptions: {
+            city: false,
+            ip_address: false,
+            region: false,
+        },
+    });
+}
+
+export const logEvent: amplitude.AmplitudeClient['logEvent'] = (...args) => {
+    if (amplitudeClient === null) {
+        throw new Error('Not init');
+    }
+    return amplitudeClient.logEvent(...args);
+};
