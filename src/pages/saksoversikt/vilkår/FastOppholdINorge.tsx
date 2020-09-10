2cedabf227ae01556cf6cb5a44da3395200aa4f6
Ingar Almklov <ingara@gmail.com>
2020-09-10 13:16:38 +0200

    Saksbehandling v2: implement fast opphold i norge

--- src/pages/saksoversikt/vilkår/FastOppholdINorge.tsx
+++ src/pages/saksoversikt/vilkår/FastOppholdINorge.tsx
@@ -3,30 +3,42 @@ import { Radio, RadioGruppe, Textarea } from 'nav-frontend-skjema';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
+import { lagreBehandlingsinformasjon } from '~features/saksoversikt/sak.slice';
 import { Nullable } from '~lib/types';
 import yup from '~lib/validering';
+import { useAppDispatch } from '~redux/Store';
+import { FastOppholdINorgeStatus } from '~types/Behandlingsinformasjon';
 
 import Faktablokk from './Faktablokk';
 import { VilkårsvurderingBaseProps } from './types';
 import { Vurdering, Vurderingknapper } from './Vurdering';
 
-type FastOppholdINorgeType = Nullable<boolean> | 'uavklart';
-
 interface FormData {
-    fastOppholdINorge: FastOppholdINorgeType;
-    fastOppholdINorgeBegrunnelse: Nullable<string>;
+    status: Nullable<FastOppholdINorgeStatus>;
+    begrunnelse: Nullable<string>;
 }
 
 const schema = yup.object<FormData>({
-    fastOppholdINorge: yup.mixed().defined().oneOf([true, false, 'uavklart'], 'Vennligst velg et alternativ '),
-    fastOppholdINorgeBegrunnelse: yup.string().required().typeError('Feltet kan ikke være tomt'),
+    status: yup
+        .mixed<FastOppholdINorgeStatus>()
+        .defined()
+        .oneOf(
+            [
+                FastOppholdINorgeStatus.Uavklart,
+                FastOppholdINorgeStatus.VilkårIkkeOppfylt,
+                FastOppholdINorgeStatus.VilkårOppfylt,
+            ],
+            'Vennligst velg et alternativ '
+        ),
+    begrunnelse: yup.string().nullable().defined().typeError('Feltet kan ikke være tomt'),
 });
 
 const FastOppholdINorge = (props: VilkårsvurderingBaseProps) => {
+    const dispatch = useAppDispatch();
     const formik = useFormik<FormData>({
         initialValues: {
-            fastOppholdINorge: null,
-            fastOppholdINorgeBegrunnelse: null,
+            status: null,
+            begrunnelse: null,
         },
         onSubmit(values) {
             console.log({ values });
@@ -41,28 +53,40 @@ const FastOppholdINorge = (props: VilkårsvurderingBaseProps) => {
             {{
                 left: (
                     <form onSubmit={formik.handleSubmit}>
-                        <RadioGruppe legend="Oppholder søker sig fast i Norge" feil={formik.errors.fastOppholdINorge}>
+                        <RadioGruppe legend="Oppholder søker sig fast i Norge" feil={formik.errors.status}>
                             <Radio
                                 label="Ja"
                                 name="fastOppholdINorge"
-                                onChange={() => formik.setValues({ ...formik.values, fastOppholdINorge: true })}
+                                onChange={() =>
+                                    formik.setValues({
+                                        ...formik.values,
+                                        status: FastOppholdINorgeStatus.VilkårOppfylt,
+                                    })
+                                }
                             />
                             <Radio
                                 label="Nei"
                                 name="fastOppholdINorge"
-                                onChange={() => formik.setValues({ ...formik.values, fastOppholdINorge: false })}
+                                onChange={() =>
+                                    formik.setValues({
+                                        ...formik.values,
+                                        status: FastOppholdINorgeStatus.VilkårIkkeOppfylt,
+                                    })
+                                }
                             />
                             <Radio
                                 label="Uavklart"
                                 name="fastOppholdINorge"
-                                onChange={() => formik.setValues({ ...formik.values, fastOppholdINorge: 'uavklart' })}
+                                onChange={() =>
+                                    formik.setValues({ ...formik.values, status: FastOppholdINorgeStatus.Uavklart })
+                                }
                             />
                         </RadioGruppe>
                         <Textarea
                             label="Begrunnelse"
                             name="fastOppholdINorgeBegrunnelse"
-                            feil={formik.errors.fastOppholdINorgeBegrunnelse}
-                            value={formik.values.fastOppholdINorgeBegrunnelse || ''}
+                            feil={formik.errors.begrunnelse}
+                            value={formik.values.begrunnelse ?? ''}
                             onChange={formik.handleChange}
                         />
                         <Vurderingknapper
@@ -71,7 +95,20 @@ const FastOppholdINorge = (props: VilkårsvurderingBaseProps) => {
                                 history.push(props.forrigeUrl);
                             }}
                             onLagreOgFortsettSenereClick={() => {
-                                console.log('lagre og fortsett senere');
+                                if (!formik.values.status) return;
+
+                                dispatch(
+                                    lagreBehandlingsinformasjon({
+                                        sakId: props.sakId,
+                                        behandlingId: props.behandling.id,
+                                        behandlingsinformasjon: {
+                                            fastOppholdINorge: {
+                                                status: formik.values.status,
+                                                begrunnelse: formik.values.begrunnelse,
+                                            },
+                                        },
+                                    })
+                                );
                             }}
                         />
                     </form>
