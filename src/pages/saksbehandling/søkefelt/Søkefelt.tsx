7dabeaad501bee55d6e7ea1af02c5cd441d29887
Aryan Iranzamini <aryaniranzamini@gmail.com>
2020-09-21 14:59:41 +0200

    Show error message and prevent infinite spinner in some rare edge cases

--- src/pages/saksbehandling/søkefelt/Søkefelt.tsx
+++ src/pages/saksbehandling/søkefelt/Søkefelt.tsx
@@ -1,11 +1,10 @@
 import { Input } from 'nav-frontend-skjema';
 import React from 'react';
 
-import * as personSlice from '~features/person/person.slice';
 import * as sakSlice from '~features/saksoversikt/sak.slice';
 import { useAppDispatch } from '~redux/Store';
 
-const Søkefelt = (props: { onSakFetchSuccess: (id: string) => void }) => {
+const Søkefelt = () => {
     const dispatch = useAppDispatch();
     const [fnr, setFnr] = React.useState('');
 
@@ -17,12 +16,7 @@ const Søkefelt = (props: { onSakFetchSuccess: (id: string) => void }) => {
             onChange={(e) => setFnr(e.target.value)}
             onKeyDown={async (e) => {
                 if (e.keyCode === 13) {
-                    await dispatch(personSlice.fetchPerson({ fnr }));
-                    const sakAction = await dispatch(sakSlice.fetchSak({ fnr }));
-                    if (sakSlice.fetchSak.fulfilled.match(sakAction)) {
-                        setFnr('');
-                        props.onSakFetchSuccess(sakAction.payload.id);
-                    }
+                    dispatch(sakSlice.fetchSak({ fnr }));
                 }
             }}
             value={fnr}
