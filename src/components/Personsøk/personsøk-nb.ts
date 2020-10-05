64729e57538fa4fbf99087470283ebd768e89ee4
Ingar Almklov <ingara@gmail.com>
2020-10-05 15:03:12 +0200

    Extract Personsøk to shared component

---
+++ src/components/Personsøk/personsøk-nb.ts
@@ -0,0 +1,7 @@
+export default {
+    'input.fnr.label': 'Fødselsnummer',
+    'knapp.søk': 'Søk',
+    'feilmelding.lengde': 'Fødselsnummeret må inneholde 11 siffer',
+    'feilmelding.ugyldig': 'Fødselsnummeret er ikke gyldig',
+    'feilmelding.ikkeTilgang': 'Du har ikke tilgang til å se informasjon om denne brukeren',
+};
--- src/pages/søknad/steg/inngang/inngang-nb.ts
+++ src/pages/søknad/steg/inngang/inngang-nb.ts
@@ -1,15 +1,3 @@
 export default {
-    tittel: 'Søknad om supplerende stønad ufør',
-    'input.fnr.label': 'Fødselsnummer',
-    'input.navn.label': 'Navn',
-    'knapp.søk': 'Søk',
-    'knapp.nyttSøk': 'Nytt søk',
     'knapp.startSøknad': 'Start søknad',
-    'knapp.hentSøker': 'Hent søker',
-    'sokerinfo.fornavn.label': 'Fornavn',
-    'sokerinfo.etternavn.label': 'Etternavn',
-    'sokerinfo.fnr.label': 'Fødselsnummer',
-    'feilmelding.lengde': 'Fødselsnummeret må inneholde 11 siffer',
-    'feilmelding.ugyldig': 'Fødselsnummeret er ikke gyldig',
-    'feilmelding.ikkeTilgang': 'Du har ikke tilgang til å se informasjon om denne brukeren',
 };
