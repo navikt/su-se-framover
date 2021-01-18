08e39170a2fb46d5d3162299cc00f3da0a4f610d
Ingar Almklov <ingara@gmail.com>
2021-01-18 10:52:03 +0100

    Add config vars for auth

--- server/config.ts
+++ server/config.ts
@@ -18,8 +18,28 @@ export const server = {
 
     frontendDir: envVar({ name: 'FRONTEND_DIR', defaultValue: path.join(__dirname, 'frontend') }),
 
+    sessionKey: envVar({ name: 'SESSION_KEY' }),
+    sessionCookieName: envVar({ name: 'SESSION_COOKIE_NAME', defaultValue: 'supstonad-login-cookie' }),
+
     isDev: envVar({ name: 'NODE_ENV' }) === 'development',
     isProd: envVar({ name: 'NODE_ENV' }) === 'production',
+
+    mockOauthServerPort: Number.parseInt(envVar({ name: 'LOCAL_AUTH_SERVER_PORT', defaultValue: '' })),
+};
+
+// For auth
+export const auth = {
+    discoverUrl: envVar({ name: 'AZURE_APP_WELL_KNOWN_URL', defaultValue: server.isDev ? 'x' : '' }),
+    clientId: envVar({ name: 'AZURE_APP_CLIENT_ID', defaultValue: server.isDev ? 'supstonad' : '' }),
+    clientSecret: envVar({ name: 'AZURE_APP_CLIENT_SECRET', defaultValue: server.isDev ? 'superhemmelig' : '' }),
+    jwks: JSON.parse(envVar({ name: 'AZURE_APP_JWKS', defaultValue: server.isDev ? '{}' : '' })),
+
+    redirectUri: envVar({ name: 'AUTH_REDIRECT_URI' }),
+
+    tokenEndpointAuthMethod: 'private_key_jwt',
+    responseType: 'code',
+    responseMode: 'query',
+    tokenEndpointAuthSigningAlg: 'RS256',
 };
 
 // Config that is exposed to the frontend
