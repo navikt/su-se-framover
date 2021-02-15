a16b839d308f801f5549e7489b5a962b9cc36179
Ingar Almklov <ingara@gmail.com>
2021-02-15 09:25:08 +0100

    Show uføregrad/forventet inntekt in summary

--- src/pages/saksbehandling/steg/faktablokk/faktablokker/UførhetFaktablokk.tsx
+++ src/pages/saksbehandling/steg/faktablokk/faktablokker/UførhetFaktablokk.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 
 import { vilkårTittelFormatted } from '~features/saksoversikt/utils';
 import { useI18n } from '~lib/hooks';
-import { keyOf } from '~lib/types';
+import { UførhetStatus } from '~types/Behandlingsinformasjon';
 
 import Vilkårsblokk from '../../../vilkårsOppsummering/VilkårsBlokk';
 import saksbehandlingMessages from '../../uførhet/uførhet-nb';
-import Faktablokk from '../Faktablokk';
+import Faktablokk, { FaktaSpacing } from '../Faktablokk';
 
 import messages from './faktablokker-nb';
 import { FaktablokkProps, VilkårsblokkProps } from './faktablokkUtils';
@@ -39,6 +39,10 @@ export const UførhetVilkårsblokk = (props: VilkårsblokkProps<'uførhet'>) => 
             ...saksbehandlingMessages,
         },
     });
+    const saksbehandlingMessage = (s: keyof typeof saksbehandlingMessages) =>
+        intl.formatMessage({
+            id: s,
+        });
     return (
         <Vilkårsblokk
             tittel={vilkårTittelFormatted(props.info.vilkårtype)}
@@ -52,9 +56,7 @@ export const UførhetVilkårsblokk = (props: VilkårsblokkProps<'uførhet'>) => 
                         tittel={intl.formatMessage({ id: 'display.fraSaksbehandling' })}
                         fakta={[
                             {
-                                tittel: intl.formatMessage({
-                                    id: keyOf<typeof saksbehandlingMessages>('radio.uførhet.legend'),
-                                }),
+                                tittel: saksbehandlingMessage('radio.uførhet.legend'),
                                 verdi:
                                     props.behandlingsinformasjon.status === 'VilkårOppfylt'
                                         ? intl.formatMessage({ id: 'fraSøknad.ja' })
@@ -62,6 +64,19 @@ export const UførhetVilkårsblokk = (props: VilkårsblokkProps<'uførhet'>) => 
                                         ? intl.formatMessage({ id: 'fraSøknad.nei' })
                                         : intl.formatMessage({ id: 'radio.label.uføresakTilBehandling' }),
                             },
+                            ...(props.behandlingsinformasjon.status === UførhetStatus.VilkårOppfylt
+                                ? [
+                                      FaktaSpacing,
+                                      {
+                                          tittel: saksbehandlingMessage('input.label.uføregrad'),
+                                          verdi: props.behandlingsinformasjon.uføregrad?.toString() ?? '-',
+                                      },
+                                      {
+                                          tittel: saksbehandlingMessage('input.label.forventetInntekt'),
+                                          verdi: props.behandlingsinformasjon.forventetInntekt?.toString() ?? '-',
+                                      },
+                                  ]
+                                : []),
                         ]}
                     />
                 )
