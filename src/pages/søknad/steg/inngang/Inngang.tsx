7b1aa4f1405d61963fc79d1de3a9a84cbef35e06
ramziabuqassim <ramzi.abu.qassim@nav.no>
2021-03-01 16:14:11 +0100

    moved some info from infoside to inngang. Added confirmationbox.

--- src/pages/søknad/steg/inngang/Inngang.tsx
+++ src/pages/søknad/steg/inngang/Inngang.tsx
@@ -1,18 +1,18 @@
 import * as RemoteData from '@devexperts/remote-data-ts';
 import { Hovedknapp } from 'nav-frontend-knapper';
-import { Sidetittel } from 'nav-frontend-typografi';
+import { BekreftCheckboksPanel } from 'nav-frontend-skjema';
+import { Sidetittel, Ingress, Feilmelding } from 'nav-frontend-typografi';
 import * as React from 'react';
 import { FormattedMessage, RawIntlProvider } from 'react-intl';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 
 import Personsøk from '~components/Personsøk/Personsøk';
 import * as personSlice from '~features/person/person.slice';
 import søknadSlice from '~features/søknad/søknad.slice';
 import { useI18n } from '~lib/hooks';
+import * as Routes from '~lib/routes';
 import { useAppDispatch, useAppSelector } from '~redux/Store';
 import { Søknadstype } from '~types/Søknad';
-
-import sharedStyles from '../../steg-shared.module.less';
 
 import nb from './inngang-nb';
 import styles from './inngang.module.less';
@@ -22,6 +22,8 @@ const index = (props: { nesteUrl: string }) => {
     const dispatch = useAppDispatch();
     const history = useHistory();
     const isPapirsøknad = history.location.search.includes('papirsoknad');
+    const [hasSubmitted, setHasSubmitted] = React.useState<boolean>(false);
+    const [erBekreftet, setErBekreftet] = React.useState<boolean>(false);
 
     const intl = useI18n({ messages: nb });
 
@@ -29,40 +31,107 @@ const index = (props: { nesteUrl: string }) => {
         dispatch(søknadSlice.actions.resetSøknad());
     }, [søker]);
 
-    const handleStartSøknadClick = () => {
-        if (RemoteData.isSuccess(søker)) {
+    const isFormValid = () => {
+        if (hasSubmitted && erBekreftet && RemoteData.isSuccess(søker)) {
+            return true;
+        }
+        return false;
+    };
+
+    const handleStartSøknadKlikk = () => {
+        setHasSubmitted(true);
+        console.log(søker);
+
+        if (isFormValid()) {
             dispatch(
                 søknadSlice.actions.startSøknad(isPapirsøknad ? Søknadstype.Papirsøknad : Søknadstype.DigitalSøknad)
             );
             history.push(props.nesteUrl);
         }
     };
-
     return (
         <RawIntlProvider value={intl}>
-            <div className={sharedStyles.container}>
+            <div className={styles.pageContainer}>
                 <Sidetittel className={styles.tittel}>
                     {intl.formatMessage(
                         isPapirsøknad ? { id: 'page.tittel.papirSøknad' } : { id: 'page.tittel.digitalSøknad' }
                     )}
                 </Sidetittel>
-                <div className={styles.search}>
-                    <Personsøk
-                        onReset={() => {
-                            dispatch(personSlice.default.actions.resetSøker());
-                        }}
-                        onSubmit={(fnr) => {
-                            dispatch(personSlice.fetchPerson({ fnr }));
-                        }}
-                        person={søker}
-                    />
-                    {RemoteData.isSuccess(søker) && (
-                        <div className={styles.successknapper}>
-                            <Hovedknapp htmlType="button" onClick={handleStartSøknadClick}>
-                                <FormattedMessage id="knapp.startSøknad" />
-                            </Hovedknapp>
-                        </div>
-                    )}
+
+                <section className={styles.section}>
+                    <Ingress>
+                        <FormattedMessage id="sendeInnDokumentasjon.måSendeInnDok" />
+                    </Ingress>
+
+                    <p className={styles.paragraphSpacing}>
+                        <FormattedMessage id="sendeInnDokumentasjon.dokGjelder" />
+                    </p>
+
+                    <p className={styles.paragraphSpacing}>
+                        <FormattedMessage id="sendeInnDokumentasjon.måLeggesVed" />
+                    </p>
+                    <ul className={styles.list}>
+                        <li className={styles.listItem}>
+                            <FormattedMessage id="sendeInnDokumentasjon.måLeggesVed.punkt1" />
+                        </li>
+                        <li className={styles.listItem}>
+                            <FormattedMessage id="sendeInnDokumentasjon.måLeggesVed.punkt2" />
+                        </li>
+                    </ul>
+
+                    <p className={styles.paragraphSpacing}>
+                        <FormattedMessage id="sendeInnDokumentasjon.kanskjeLeggesVed" />
+                    </p>
+                    <ul className={styles.list}>
+                        <li className={styles.listItem}>
+                            <FormattedMessage id="sendeInnDokumentasjon.kanskjeLeggesVed.punkt1" />
+                        </li>
+                        <li className={styles.listItem}>
+                            <FormattedMessage id="sendeInnDokumentasjon.kanskjeLeggesVed.punkt2" />
+                        </li>
+                    </ul>
+                </section>
+
+                <div className={styles.checkboksPanelContainer}>
+                    <BekreftCheckboksPanel
+                        checked={erBekreftet}
+                        label={intl.formatMessage({ id: 'bekreftelsesboks.tekst.p2' })}
+                        onChange={() => setErBekreftet(!erBekreftet)}
+                        feil={hasSubmitted && !erBekreftet ? intl.formatMessage({ id: 'feil.påkrevdFelt' }) : undefined}
+                    >
+                        {intl.formatMessage({ id: 'bekreftelsesboks.tekst.p1' })}
+                    </BekreftCheckboksPanel>
+                </div>
+
+                <div className={styles.searchContainer}>
+                    <Ingress>
+                        <FormattedMessage id="finnSøker.tittel" />
+                    </Ingress>
+                    <p className={styles.finnSøkerTekst}>
+                        <FormattedMessage id="finnSøker.tekst" />
+                    </p>
+                    <div className={styles.searchboxContainer}>
+                        <Personsøk
+                            onReset={() => {
+                                dispatch(personSlice.default.actions.resetSøker());
+                            }}
+                            onSubmit={(fnr) => {
+                                dispatch(personSlice.fetchPerson({ fnr }));
+                            }}
+                            person={søker}
+                        />
+                        {hasSubmitted && RemoteData.isInitial(søker) && (
+                            <Feilmelding>{intl.formatMessage({ id: 'feil.måSøkePerson' })}</Feilmelding>
+                        )}
+                    </div>
+                </div>
+                <div className={styles.knapperContainer}>
+                    <Link className="knapp" to={Routes.soknad.createURL()}>
+                        <FormattedMessage id="knapp.forrige" />
+                    </Link>
+                    <Hovedknapp htmlType="button" onClick={handleStartSøknadKlikk}>
+                        <FormattedMessage id="knapp.startSøknad" />
+                    </Hovedknapp>
                 </div>
             </div>
         </RawIntlProvider>
--- src/pages/søknad/steg/inngang/inngang-nb.ts
+++ src/pages/søknad/steg/inngang/inngang-nb.ts
@@ -1,5 +1,27 @@
 export default {
+    'bekreftelsesboks.tekst.p1':
+        'Veileder har informert søker om hvilken informasjon NAV henter og hvilken informasjon som søker må dokumentere.',
+    'bekreftelsesboks.tekst.p2':
+        'Søker er klar over at hen kan miste retten til supplerende stønad dersom søker ikke har gitt riktige opplysninger.',
+
+    'feil.måSøkePerson': 'Du må søke opp person før du kan fortsette',
+    'feil.påkrevdFelt': 'Feltet er påkrevd',
+
+    'finnSøker.tittel': 'Finn søker',
+    'finnSøker.tekst': 'For å starte søknaden, må du skrive inn fødselsnummeret til søkeren',
+
+    'knapp.forrige': 'Forrige',
     'knapp.startSøknad': 'Start søknad',
+
+    'sendeInnDokumentasjon.måSendeInnDok': 'Det kan hende du må sende inn dokumentasjon',
+    'sendeInnDokumentasjon.dokGjelder':
+        'Dokumentasjonen du må legge ved gjelder både søker og eventuelt ektefelle, samboer eller registrert partner.',
+    'sendeInnDokumentasjon.måLeggesVed': 'Dette må du uansett legge ved:',
+    'sendeInnDokumentasjon.måLeggesVed.punkt1': 'Kopi av pass',
+    'sendeInnDokumentasjon.måLeggesVed.punkt2': 'Kopi av grunnlaget for skatt og siste skattemelding',
+    'sendeInnDokumentasjon.kanskjeLeggesVed': 'Dette kan det hende du må legge ved: ',
+    'sendeInnDokumentasjon.kanskjeLeggesVed.punkt1': 'Dokumentering av formue i utlandet',
+    'sendeInnDokumentasjon.kanskjeLeggesVed.punkt2': 'Dokumentering av pensjon fra utlandet',
 
     'page.tittel.digitalSøknad': 'Søknad om supplerende stønad for uføre flyktninger',
     'page.tittel.papirSøknad': 'Registrering av papirsøknad',
