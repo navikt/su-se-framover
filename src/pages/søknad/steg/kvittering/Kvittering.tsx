0d1419c83a7b292562352a92aac36dc0ba697988
ramziabuqassim <ramzi.abu.qassim@nav.no>
2021-03-02 13:57:09 +0100

    WIP: added initial info and styling to kvittering

--- src/pages/søknad/steg/kvittering/Kvittering.tsx
+++ src/pages/søknad/steg/kvittering/Kvittering.tsx
@@ -1,10 +1,11 @@
 import * as RemoteData from '@devexperts/remote-data-ts';
 import { pipe } from 'fp-ts/lib/function';
-import AlertStripe from 'nav-frontend-alertstriper';
+import AlertStripe, { AlertStripeInfo } from 'nav-frontend-alertstriper';
 import { Hovedknapp, Knapp } from 'nav-frontend-knapper';
 import NavFrontendSpinner from 'nav-frontend-spinner';
-import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
+import { Normaltekst, Undertittel, Element } from 'nav-frontend-typografi';
 import * as React from 'react';
+import { IntlProvider, FormattedMessage } from 'react-intl';
 import { useHistory } from 'react-router-dom';
 
 import { fetchSøknad } from '~api/pdfApi';
@@ -22,6 +23,11 @@ const Kvittering = () => {
     const dispatch = useAppDispatch();
     const history = useHistory();
     const søknad = useAppSelector((state) => state.innsending.søknad);
+
+    /* const søknad = RemoteData.success({
+        saksnummer: 100,
+        søknad: {},
+    });*/
 
     return (
         <div>
@@ -63,46 +69,87 @@ const Kvittering = () => {
                     },
                     (s) => {
                         return (
-                            <>
-                                <AlertStripe type="suksess">
-                                    <Normaltekst>{intl.formatMessage({ id: 'kvittering.søknadMottatt' })}</Normaltekst>
-                                    <Normaltekst>Saksnummer: {s.saksnummer}</Normaltekst>
-                                </AlertStripe>
+                            <IntlProvider locale="nb" messages={messages}>
+                                <div className={styles.suksessContainer}>
+                                    <div>
+                                        <AlertStripe type="suksess">
+                                            <Normaltekst>
+                                                <FormattedMessage id="kvittering.søknadMottatt" />
+                                            </Normaltekst>
+                                            <Normaltekst>
+                                                <FormattedMessage
+                                                    id="kvittering.saksnummer"
+                                                    values={{
+                                                        saksnummer: s.saksnummer,
+                                                    }}
+                                                />
+                                            </Normaltekst>
+                                        </AlertStripe>
 
-                                <div className={styles.tilVeileder}>
-                                    <Undertittel tag="h3">
-                                        {intl.formatMessage({ id: 'kvittering.tilVeileder.heading' })}
-                                    </Undertittel>
-                                    <ol>
-                                        <li>{intl.formatMessage({ id: 'kvittering.tilVeileder.punkt1' })}</li>
-                                        <li>{intl.formatMessage({ id: 'kvittering.tilVeileder.punkt2' })}</li>
-                                        <li>{intl.formatMessage({ id: 'kvittering.tilVeileder.punkt3' })}</li>
-                                    </ol>
+                                        <div className={styles.tilVeileder}>
+                                            <Undertittel tag="h3">
+                                                <FormattedMessage id="kvittering.tilVeileder.heading" />
+                                            </Undertittel>
+                                            <ol>
+                                                <li>
+                                                    <FormattedMessage id="kvittering.tilVeileder.punkt1" />
+                                                </li>
+                                                <li>
+                                                    <FormattedMessage id="kvittering.tilVeileder.punkt2" />
+                                                </li>
+                                                <li>
+                                                    <FormattedMessage id="kvittering.tilVeileder.punkt3" />
+                                                </li>
+                                            </ol>
+                                        </div>
+
+                                        <div className={styles.nySøknadKnapp}>
+                                            <Knapp
+                                                onClick={() => {
+                                                    dispatch(personSlice.default.actions.resetSøker());
+                                                    dispatch(søknadslice.default.actions.resetSøknad());
+                                                    history.push(Routes.soknadPersonSøk.createURL({}));
+                                                }}
+                                            >
+                                                <FormattedMessage id="kvittering.avslutt" />
+                                            </Knapp>
+
+                                            <Hovedknapp
+                                                onClick={() =>
+                                                    fetchSøknad(s.søknad.id).then((res) => {
+                                                        if (res.status === 'ok')
+                                                            window.open(URL.createObjectURL(res.data));
+                                                    })
+                                                }
+                                            >
+                                                <FormattedMessage id="kvittering.skrivUtSøknad" />
+                                            </Hovedknapp>
+                                        </div>
+                                    </div>
+                                    <div className={styles.infoContainer}>
+                                        <AlertStripeInfo>
+                                            <p>
+                                                <FormattedMessage
+                                                    id="vedlegg.huskVedlegg"
+                                                    values={{
+                                                        //eslint-disable-next-line react/display-name
+                                                        b: (text: string) => <Element>{text}</Element>,
+                                                    }}
+                                                />
+                                            </p>
+                                            <p>
+                                                <FormattedMessage
+                                                    id="vedlegg.måLeggesMed"
+                                                    values={{
+                                                        //eslint-disable-next-line react/display-name
+                                                        b: (text: string) => <Element>{text}</Element>,
+                                                    }}
+                                                />
+                                            </p>
+                                        </AlertStripeInfo>
+                                    </div>
                                 </div>
-
-                                <div className={styles.nySøknadKnapp}>
-                                    <Hovedknapp
-                                        className={styles.marginRight}
-                                        onClick={() =>
-                                            fetchSøknad(s.søknad.id).then((res) => {
-                                                if (res.status === 'ok') window.open(URL.createObjectURL(res.data));
-                                            })
-                                        }
-                                    >
-                                        {intl.formatMessage({ id: 'kvittering.skrivUtSøknad' })}
-                                    </Hovedknapp>
-
-                                    <Knapp
-                                        onClick={() => {
-                                            dispatch(personSlice.default.actions.resetSøker());
-                                            dispatch(søknadslice.default.actions.resetSøknad());
-                                            history.push(Routes.soknadPersonSøk.createURL({}));
-                                        }}
-                                    >
-                                        {intl.formatMessage({ id: 'kvittering.avslutt' })}
-                                    </Knapp>
-                                </div>
-                            </>
+                            </IntlProvider>
                         );
                     }
                 )
--- src/pages/søknad/steg/kvittering/kvittering-nb.ts
+++ src/pages/søknad/steg/kvittering/kvittering-nb.ts
@@ -3,8 +3,21 @@ export default {
     'kvittering.skrivUtSøknad': 'Skriv ut søknad',
     'kvittering.avslutt': 'Avslutt',
     'knapp.tilbake': 'Tilbake',
+
+    'kvittering.saksnummer': 'Saksnummer: {saksnummer}',
+
     'kvittering.tilVeileder.heading': 'Til veileder',
     'kvittering.tilVeileder.punkt1': 'Skriv ut to eksempler av søknaden',
     'kvittering.tilVeileder.punkt2': 'Få underskrift av søker på den ene kopien og gi den andre kopien til søker',
     'kvittering.tilVeileder.punkt3': 'Send kopien med underskrift og vedlegg til scanning',
+
+    'vedlegg.huskVedlegg': 'Husk vedlegg som skal sendes med søknaden',
+    'vedlegg.måLeggesMed': 'Vedlegg som må legges med fra søker og eventuelt søkers ektefelle/samboer:',
+    'vedlegg.måLeggesMed.puntk1': '<b>Kopi av pass</b>',
+    'vedlegg.måLeggesMed.puntk2': '<b>Kopi av grunnlaget for skatt og siste skattemelding</b>',
+    'vedlegg.formueIUtlandet':
+        'Hvis søker og eventuelt søkers ektefelle/samboer har formue i utlandet eller mottar pensjon fra utlandet, må dette dokumenters:',
+    'vedlegg.formueIUtlandet.punkt1': '<b>Dokumentering av formue i utlandet</b>',
+    'vedlegg.formueIUtlandet.punkt2': '<b>Dokumentering av pensjon fra utlandet</b>',
+    'vedlegg.søkerManglerDok': '<b>Hvis søker mangler dokumentasjon kan søker ettersende dette.</b>',
 };
