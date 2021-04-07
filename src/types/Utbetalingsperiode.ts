34c2b2c033172ce3066e54f69bcac108723c34e3
Aryan Iranzamini <aryaniranzamini@gmail.com>
2021-04-07 11:16:43 +0200

    Update view for utbetalingsperioder
    Co-authored-by: Ingar Almklov <ingara@gmail.com>
    Co-authored-by: Christian H. Mosveen <christian.mosveen@nav.no>

--- src/types/Utbetalingsperiode.ts
+++ src/types/Utbetalingsperiode.ts
@@ -1,11 +1,16 @@
 import * as DateFns from 'date-fns';
 
+export const enum Utbetalingstype {
+    NY = 'NY',
+    OPPHØR = 'OPPHØR',
+}
+
 export interface Utbetalingsperiode {
     id: string;
     fraOgMed: string;
     tilOgMed: string;
     beløp: number;
-    type: string;
+    type: Utbetalingstype;
 }
 
 export const compareUtbetalingsperiode = (u1: Utbetalingsperiode, u2: Utbetalingsperiode) =>
