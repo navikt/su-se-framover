cb4e6b811221b8653da3d33f8451c380ddebc8b2
ramziabuqassim <ramzi.abu.qassim@nav.no>
2021-04-19 10:54:28 +0200

    handling of revurdering-api calls

---
+++ src/pages/saksbehandling/revurdering/revurderingskallFeilet/RevurderingskallFeilet.tsx
@@ -0,0 +1,50 @@
+import * as RemoteData from '@devexperts/remote-data-ts';
+import { AlertStripeFeil } from 'nav-frontend-alertstriper';
+import React from 'react';
+import { IntlShape } from 'react-intl';
+
+import { ApiError, ErrorMessage } from '~api/apiClient';
+import { useI18n } from '~lib/hooks';
+import { Nullable } from '~lib/types';
+import { RevurderingErrorCodes } from '~types/Revurdering';
+
+import messages from './revurderingskallFeilet-nb';
+import styles from './revurderingskallFeilet.module.less';
+
+const feilkodeTilFeilmelding = (feil: Nullable<ErrorMessage>, intl: IntlShape) => {
+    switch (feil?.code) {
+        case RevurderingErrorCodes.UGYLDIG_TILSTAND:
+            return intl.formatMessage({ id: 'feil.ugyldig.tilstand' });
+        case RevurderingErrorCodes.UGYLDIG_PERIODE:
+            return intl.formatMessage({ id: 'feil.ugyldig.periode' });
+        case RevurderingErrorCodes.FANT_IKKE_REVURDERING:
+            return intl.formatMessage({ id: 'feil.fant.ikke.revurdering' });
+        case RevurderingErrorCodes.FANT_IKKE_AKTØR_ID:
+            return intl.formatMessage({ id: 'feil.fant.ikke.aktør.id' });
+        case RevurderingErrorCodes.FANT_IKKE_SAK:
+            return intl.formatMessage({ id: 'feil.fant.ikke.sak' });
+        case RevurderingErrorCodes.FANT_IKKE_PERSON:
+            return intl.formatMessage({ id: 'feil.kunne.ikke.person' });
+        case RevurderingErrorCodes.KUNNE_IKKE_OPPRETTE_OPPGAVE:
+            return intl.formatMessage({ id: 'feil.kunne.ikke.opprette.oppgave' });
+        case RevurderingErrorCodes.KUNNE_IKKE_JOURNALFØRE_BREV:
+            return intl.formatMessage({ id: 'feil.kunne.ikke.journalføre.brev' });
+        case RevurderingErrorCodes.KUNNE_IKKE_DISTRIBUERE_BREV:
+            return intl.formatMessage({ id: 'feil.kunne.ikke.distribuere.brev' });
+        case RevurderingErrorCodes.ALLEREDE_FORHÅNDSVARSLET:
+            return intl.formatMessage({ id: 'feil.allerede.forhåndsvarslet' });
+        default:
+            return intl.formatMessage({ id: 'feil.ukjentFeil' });
+    }
+};
+const RevurderingskallFeilet = (props: { error: ApiError }) => {
+    const intl = useI18n({ messages });
+
+    return (
+        <AlertStripeFeil className={styles.alertstripe}>
+            {feilkodeTilFeilmelding(props.error.body, intl)}
+        </AlertStripeFeil>
+    );
+};
+
+export default RevurderingskallFeilet;
---
+++ src/pages/saksbehandling/revurdering/revurderingskallFeilet/revurderingskallFeilet-nb.ts
@@ -0,0 +1,10 @@
+export default {
+    'feil.ukjentFeil': 'Ukjent feil',
+    'feil.fant.ikke.sak': 'Fant ikke sak',
+    'feil.fant.ikke.aktør.id': 'Fant ikke aktør id',
+    'feil.kunne.ikke.opprette.oppgave': 'Kunne ikke opprette oppgave',
+    'feil.fant.ikke.revurdering': 'Fant ikke revurdering',
+    'feil.ugyldig.periode': 'Ugyldig periode',
+    'feil.ugyldig.tilstand': 'Ugyldig tilstand',
+    'feil.allerede.forhåndsvarslet': 'Revurderingen er allerede blitt forhåndsvarslet',
+};
