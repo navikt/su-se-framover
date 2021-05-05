da2a49a1b142b446f8bf93d915cd1513b2665946
Ingar Almklov <ingara@gmail.com>
2021-05-05 10:58:50 +0200

    Allow custom element for tittel in ToKolonner

--- src/components/toKolonner/ToKolonner.tsx
+++ src/components/toKolonner/ToKolonner.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import styles from './toKolonner.module.less';
 
 const ToKolonner = (props: {
-    tittel: string;
+    tittel: string | JSX.Element;
     children: {
         left: JSX.Element;
         right: JSX.Element;
@@ -12,9 +12,13 @@ const ToKolonner = (props: {
 }) => (
     <div className={styles.contentContainer}>
         <div className={styles.left}>
-            <Systemtittel tag="h1" className={styles.tittel}>
-                {props.tittel}
-            </Systemtittel>
+            {typeof props.tittel === 'string' ? (
+                <Systemtittel tag="h1" className={styles.tittel}>
+                    {props.tittel}
+                </Systemtittel>
+            ) : (
+                props.tittel
+            )}
             {props.children.left}
         </div>
         <div className={styles.rightContainer}>
