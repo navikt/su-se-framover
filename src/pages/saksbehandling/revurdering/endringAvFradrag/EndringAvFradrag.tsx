72b6432c20a44f37b7585ea1f3d43961b8825b76
Jacob Meidell <jacob.meidell@nav.no>
2021-05-27 08:41:42 +0200

    Update src/pages/saksbehandling/revurdering/endringAvFradrag/EndringAvFradrag.tsx
    Co-authored-by: Ingar Almklov <ingara@gmail.com>

--- src/pages/saksbehandling/revurdering/endringAvFradrag/EndringAvFradrag.tsx
+++ src/pages/saksbehandling/revurdering/endringAvFradrag/EndringAvFradrag.tsx
@@ -161,11 +161,11 @@ const EndringAvFradrag = (props: {
                     periode: {
                         /* eslint-disable @typescript-eslint/no-non-null-assertion */
                         fraOgMed: DateUtils.toIsoDateOnlyString(
-                            f.periode?.fraOgMed ?? new Date(props.revurdering.periode.fraOgMed)
+                            f.periode?.fraOgMed ?? DateUtils.parseIsoDateOnly(props.revurdering.periode.fraOgMed)
                         ),
                         tilOgMed: DateUtils.toIsoDateOnlyString(
                             DateUtils.sluttenAvMåneden(
-                                f.periode?.tilOgMed ?? new Date(props.revurdering.periode.tilOgMed)
+                                f.periode?.tilOgMed ?? DateUtils.parseIsoDateOnly(props.revurdering.periode.tilOgMed)
                             )
                         ),
                         /* eslint-enable @typescript-eslint/no-non-null-assertion */
