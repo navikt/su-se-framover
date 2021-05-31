e00e0b08da7db33e6212fe2dd80a8afad8b9d694
ramziabuqassim <ramzi.abu.qassim@nav.no>
2021-05-31 15:58:29 +0200

    temp commit

---
+++ src/pages/saksbehandling/revurdering/bosituasjon/bosituasjon-nb.ts
@@ -0,0 +1,6 @@
+export default {
+    'form.begrunnelse': 'Begrunnelse',
+    'form.delerSøkerBolig': 'Deler søker bolig med noen over 18 år?',
+    'form.epsFnr': 'Ektefelle/samboers fødelsnummer',
+    'form.harSøkerEPS': 'Har søker ektefelle eller samboer?',
+};
