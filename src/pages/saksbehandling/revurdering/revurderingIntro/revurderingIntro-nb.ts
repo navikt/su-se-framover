415cb14ac352b8b918a1132cfbf0fb8712e5daa3
ramziabuqassim <ramzi.abu.qassim@nav.no>
2021-06-03 12:52:17 +0200

    info - relevant å endre formue/inntekt ved bosituasjon

--- src/pages/saksbehandling/revurdering/revurderingIntro/revurderingIntro-nb.ts
+++ src/pages/saksbehandling/revurdering/revurderingIntro/revurderingIntro-nb.ts
@@ -10,4 +10,6 @@ export default {
     'informasjonSomRevurderes.uførhet': 'Uførhet',
     'informasjonSomRevurderes.inntekt': 'Inntekt',
     'informasjonSomRevurderes.bosituasjon': 'Bosituasjon',
+
+    'info.bosituasjon': 'Ved boforhold kan det være relevant å endre inntekt og formue også',
 };
