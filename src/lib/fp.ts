17988ecb804be4f0f2b3ffee5ff0e223e370f3b6
Ingar Almklov <ingara@gmail.com>
2021-06-16 11:50:35 +0200

    Fix usage of deprecated fp-ts functions

--- src/lib/fp.ts
+++ src/lib/fp.ts
@@ -1,8 +1,7 @@
-import * as Apply from 'fp-ts/Apply';
+import * as F from 'fp-ts/function';
 import * as Option from 'fp-ts/Option';
-import * as pipeable from 'fp-ts/pipeable';
 
-export const pipe = pipeable.pipe;
-export const combineOptions = Apply.sequenceT(Option.option);
+export const pipe = F.pipe;
+export const combineOptions = Option.sequenceArray;
 
 export const nullableMap = <T, U>(arg: T | null, f: (x: T) => U) => (arg === null ? null : f(arg));
