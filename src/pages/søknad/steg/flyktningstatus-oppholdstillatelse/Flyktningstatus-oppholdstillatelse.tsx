1951fcb5e58a325622f5a773e60b6c79a21cfac6
ramziabuqassim <ramzi.abu.qassim@nav.no>
2021-07-08 15:06:07 +0200

    fjern unødvendig wrapper & bruk formatMessage()

--- src/pages/søknad/steg/flyktningstatus-oppholdstillatelse/Flyktningstatus-oppholdstillatelse.tsx
+++ src/pages/søknad/steg/flyktningstatus-oppholdstillatelse/Flyktningstatus-oppholdstillatelse.tsx
@@ -3,7 +3,6 @@ import AlertStripe from 'nav-frontend-alertstriper';
 import { Feiloppsummering, RadioPanelGruppe } from 'nav-frontend-skjema';
 import Input from 'nav-frontend-skjema/lib/input';
 import * as React from 'react';
-import { FormattedMessage, RawIntlProvider } from 'react-intl';
 import { useHistory } from 'react-router-dom';
 
 import { JaNeiSpørsmål } from '~/components/FormElements';
@@ -92,162 +91,160 @@ const FlyktningstatusOppholdstillatelse = (props: { forrigeUrl: string; nesteUrl
     });
     const feiloppsummeringref = React.useRef<HTMLDivElement>(null);
 
-    const { intl } = useI18n({ messages: { ...sharedI18n, ...messages } });
+    const { formatMessage } = useI18n({ messages: { ...sharedI18n, ...messages } });
 
     return (
-        <RawIntlProvider value={intl}>
-            <form
-                onSubmit={(e) => {
-                    setHasSubmitted(true);
-                    formik.handleSubmit(e);
-                    setTimeout(() => {
-                        if (feiloppsummeringref.current) {
-                            feiloppsummeringref.current.focus();
-                        }
-                    }, 0);
-                }}
-                className={sharedStyles.container}
-            >
-                <div className={sharedStyles.formContainer}>
+        <form
+            onSubmit={(e) => {
+                setHasSubmitted(true);
+                formik.handleSubmit(e);
+                setTimeout(() => {
+                    if (feiloppsummeringref.current) {
+                        feiloppsummeringref.current.focus();
+                    }
+                }, 0);
+            }}
+            className={sharedStyles.container}
+        >
+            <div className={sharedStyles.formContainer}>
+                <JaNeiSpørsmål
+                    id={keyOf<FormData>('erFlyktning')}
+                    className={sharedStyles.sporsmal}
+                    legend={formatMessage('flyktning.label')}
+                    hjelpetekstTittel={formatMessage('flyktning.hjelpetekst.tittel')}
+                    hjelpetekstBody={formatMessage('flyktning.hjelpetekst.body')}
+                    feil={formik.errors.erFlyktning}
+                    state={formik.values.erFlyktning}
+                    onChange={(val) =>
+                        formik.setValues({
+                            ...formik.values,
+                            erFlyktning: val,
+                        })
+                    }
+                />
+                {formik.values.erFlyktning === false && (
+                    <AlertStripe type="advarsel" className={sharedStyles.marginBottom}>
+                        {formatMessage('flyktning.måVæreFlyktning')}
+                    </AlertStripe>
+                )}
+                <JaNeiSpørsmål
+                    id={keyOf<FormData>('erNorskStatsborger')}
+                    className={sharedStyles.sporsmal}
+                    legend={formatMessage('norsk.statsborger.label')}
+                    feil={formik.errors.erNorskStatsborger}
+                    state={formik.values.erNorskStatsborger}
+                    onChange={(val) =>
+                        formik.setValues({
+                            ...formik.values,
+                            erNorskStatsborger: val,
+                            harOppholdstillatelse: null,
+                            typeOppholdstillatelse: null,
+                        })
+                    }
+                />
+                {formik.values.erNorskStatsborger === false && (
                     <JaNeiSpørsmål
-                        id={keyOf<FormData>('erFlyktning')}
+                        id={keyOf<FormData>('harOppholdstillatelse')}
                         className={sharedStyles.sporsmal}
-                        legend={<FormattedMessage id="flyktning.label" />}
-                        hjelpetekstTittel={intl.formatMessage({ id: 'flyktning.hjelpetekst.tittel' })}
-                        hjelpetekstBody={intl.formatMessage({ id: 'flyktning.hjelpetekst.body' })}
-                        feil={formik.errors.erFlyktning}
-                        state={formik.values.erFlyktning}
+                        legend={formatMessage('oppholdstillatelse.label')}
+                        feil={formik.errors.harOppholdstillatelse}
+                        state={formik.values.harOppholdstillatelse}
                         onChange={(val) =>
                             formik.setValues({
                                 ...formik.values,
-                                erFlyktning: val,
-                            })
-                        }
-                    />
-                    {formik.values.erFlyktning === false && (
-                        <AlertStripe type="advarsel" className={sharedStyles.marginBottom}>
-                            {intl.formatMessage({ id: 'flyktning.måVæreFlyktning' })}
-                        </AlertStripe>
-                    )}
-                    <JaNeiSpørsmål
-                        id={keyOf<FormData>('erNorskStatsborger')}
-                        className={sharedStyles.sporsmal}
-                        legend={<FormattedMessage id="norsk.statsborger.label" />}
-                        feil={formik.errors.erNorskStatsborger}
-                        state={formik.values.erNorskStatsborger}
-                        onChange={(val) =>
-                            formik.setValues({
-                                ...formik.values,
-                                erNorskStatsborger: val,
-                                harOppholdstillatelse: null,
+                                harOppholdstillatelse: val,
                                 typeOppholdstillatelse: null,
                             })
                         }
                     />
-                    {formik.values.erNorskStatsborger === false && (
-                        <JaNeiSpørsmål
-                            id={keyOf<FormData>('harOppholdstillatelse')}
-                            className={sharedStyles.sporsmal}
-                            legend={<FormattedMessage id="oppholdstillatelse.label" />}
-                            feil={formik.errors.harOppholdstillatelse}
-                            state={formik.values.harOppholdstillatelse}
-                            onChange={(val) =>
-                                formik.setValues({
-                                    ...formik.values,
-                                    harOppholdstillatelse: val,
-                                    typeOppholdstillatelse: null,
-                                })
-                            }
-                        />
-                    )}
-                    {formik.values.harOppholdstillatelse === true && (
-                        <RadioPanelGruppe
-                            className={sharedStyles.sporsmal}
-                            feil={formik.errors.typeOppholdstillatelse}
-                            legend={<FormattedMessage id={'oppholdstillatelse.type'} />}
-                            name={keyOf<FormData>('typeOppholdstillatelse')}
-                            radios={[
-                                {
-                                    id: keyOf<FormData>('typeOppholdstillatelse'),
-                                    label: <FormattedMessage id={'oppholdstillatelse.permanent'} />,
-                                    value: TypeOppholdstillatelse.Permanent,
-                                },
-                                {
-                                    label: <FormattedMessage id={'oppholdstillatelse.midlertidig'} />,
-                                    value: TypeOppholdstillatelse.Midlertidig,
-                                },
-                            ]}
-                            onChange={(_, value) => {
-                                formik.setValues({
-                                    ...formik.values,
-                                    typeOppholdstillatelse: value,
-                                });
-                            }}
-                            checked={formik.values.typeOppholdstillatelse?.toString()}
-                        />
-                    )}
-                    {formik.values.harOppholdstillatelse === false && (
-                        <AlertStripe type="advarsel" className={sharedStyles.marginBottom}>
-                            {intl.formatMessage({ id: 'oppholdstillatelse.ikkeLovligOpphold' })}
-                        </AlertStripe>
-                    )}
-
-                    {formik.values.typeOppholdstillatelse === 'midlertidig' && (
-                        <AlertStripe type="advarsel" className={sharedStyles.marginBottom}>
-                            {intl.formatMessage({ id: 'oppholdstillatelse.midlertidig.info' })}
-                        </AlertStripe>
-                    )}
-
-                    <JaNeiSpørsmål
-                        id={keyOf<FormData>('statsborgerskapAndreLand')}
+                )}
+                {formik.values.harOppholdstillatelse === true && (
+                    <RadioPanelGruppe
                         className={sharedStyles.sporsmal}
-                        legend={<FormattedMessage id="statsborger.andre.land.label" />}
-                        feil={formik.errors.statsborgerskapAndreLand}
-                        state={formik.values.statsborgerskapAndreLand}
-                        onChange={(val) =>
+                        feil={formik.errors.typeOppholdstillatelse}
+                        legend={formatMessage('oppholdstillatelse.type')}
+                        name={keyOf<FormData>('typeOppholdstillatelse')}
+                        radios={[
+                            {
+                                id: keyOf<FormData>('typeOppholdstillatelse'),
+                                label: formatMessage('oppholdstillatelse.permanent'),
+                                value: TypeOppholdstillatelse.Permanent,
+                            },
+                            {
+                                label: formatMessage('oppholdstillatelse.midlertidig'),
+                                value: TypeOppholdstillatelse.Midlertidig,
+                            },
+                        ]}
+                        onChange={(_, value) => {
                             formik.setValues({
                                 ...formik.values,
-                                statsborgerskapAndreLand: val,
-                                statsborgerskapAndreLandFritekst: null,
-                            })
-                        }
+                                typeOppholdstillatelse: value,
+                            });
+                        }}
+                        checked={formik.values.typeOppholdstillatelse?.toString()}
                     />
-                    {formik.values.statsborgerskapAndreLand && (
-                        <Input
-                            id={keyOf<FormData>('statsborgerskapAndreLandFritekst')}
-                            name={keyOf<FormData>('statsborgerskapAndreLandFritekst')}
-                            label={<FormattedMessage id="statsborger.andre.land.fritekst" />}
-                            feil={formik.errors.statsborgerskapAndreLandFritekst}
-                            value={formik.values.statsborgerskapAndreLandFritekst || ''}
-                            onChange={formik.handleChange}
-                            autoComplete="off"
-                            // Dette elementet vises ikke ved sidelast
-                            // eslint-disable-next-line jsx-a11y/no-autofocus
-                            autoFocus
-                        />
-                    )}
-                </div>
-                <Feiloppsummering
-                    className={sharedStyles.marginBottom}
-                    tittel={intl.formatMessage({ id: 'feiloppsummering.title' })}
-                    feil={formikErrorsTilFeiloppsummering(formik.errors)}
-                    hidden={!formikErrorsHarFeil(formik.errors)}
-                    innerRef={feiloppsummeringref}
-                />
+                )}
+                {formik.values.harOppholdstillatelse === false && (
+                    <AlertStripe type="advarsel" className={sharedStyles.marginBottom}>
+                        {formatMessage('oppholdstillatelse.ikkeLovligOpphold')}
+                    </AlertStripe>
+                )}
 
-                <Bunnknapper
-                    previous={{
-                        onClick: () => {
-                            save(formik.values);
-                            history.push(props.forrigeUrl);
-                        },
-                    }}
-                    avbryt={{
-                        toRoute: props.avbrytUrl,
-                    }}
+                {formik.values.typeOppholdstillatelse === 'midlertidig' && (
+                    <AlertStripe type="advarsel" className={sharedStyles.marginBottom}>
+                        {formatMessage('oppholdstillatelse.midlertidig.info')}
+                    </AlertStripe>
+                )}
+
+                <JaNeiSpørsmål
+                    id={keyOf<FormData>('statsborgerskapAndreLand')}
+                    className={sharedStyles.sporsmal}
+                    legend={formatMessage('statsborger.andre.land.label')}
+                    feil={formik.errors.statsborgerskapAndreLand}
+                    state={formik.values.statsborgerskapAndreLand}
+                    onChange={(val) =>
+                        formik.setValues({
+                            ...formik.values,
+                            statsborgerskapAndreLand: val,
+                            statsborgerskapAndreLandFritekst: null,
+                        })
+                    }
                 />
-            </form>
-        </RawIntlProvider>
+                {formik.values.statsborgerskapAndreLand && (
+                    <Input
+                        id={keyOf<FormData>('statsborgerskapAndreLandFritekst')}
+                        name={keyOf<FormData>('statsborgerskapAndreLandFritekst')}
+                        label={formatMessage('statsborger.andre.land.fritekst')}
+                        feil={formik.errors.statsborgerskapAndreLandFritekst}
+                        value={formik.values.statsborgerskapAndreLandFritekst || ''}
+                        onChange={formik.handleChange}
+                        autoComplete="off"
+                        // Dette elementet vises ikke ved sidelast
+                        // eslint-disable-next-line jsx-a11y/no-autofocus
+                        autoFocus
+                    />
+                )}
+            </div>
+            <Feiloppsummering
+                className={sharedStyles.marginBottom}
+                tittel={formatMessage('feiloppsummering.title')}
+                feil={formikErrorsTilFeiloppsummering(formik.errors)}
+                hidden={!formikErrorsHarFeil(formik.errors)}
+                innerRef={feiloppsummeringref}
+            />
+
+            <Bunnknapper
+                previous={{
+                    onClick: () => {
+                        save(formik.values);
+                        history.push(props.forrigeUrl);
+                    },
+                }}
+                avbryt={{
+                    toRoute: props.avbrytUrl,
+                }}
+            />
+        </form>
     );
 };
 
