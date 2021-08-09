c3fee4ed9089c41f47186cb89ca83ef46750abd4
Christian H. Mosveen <mosveen@gmail.com>
2021-08-09 10:46:57 +0200

    Improve validation message

--- src/pages/saksbehandling/søknadsbehandling/uførhet/Uførhet.tsx
+++ src/pages/saksbehandling/søknadsbehandling/uførhet/Uførhet.tsx
@@ -49,7 +49,7 @@ const schema = yup.object<FormData>({
         .defined()
         .oneOf(
             [UføreResultat.VilkårOppfylt, UføreResultat.VilkårIkkeOppfylt, UføreResultat.HarUføresakTilBehandling],
-            'Du må velge om bruker har uføresak til behandling'
+            'Du må velge om bruker har vedtak om uføretrygd eller uføresak til behandling'
         ),
     uføregrad: yup
         .number()
