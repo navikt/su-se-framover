4272a6ebb13459b66d4975f89a13ed7fbc6d0a16
Ingar Almklov <ingara@gmail.com>
2021-08-31 12:11:59 +0200

    Fix clear-button disappearing from datepicker

--- src/components/datePicker/DatePicker.tsx
+++ src/components/datePicker/DatePicker.tsx
@@ -1,8 +1,11 @@
 import type { StrictModifiers } from '@popperjs/core';
+import classNames from 'classnames';
 import { Label, SkjemaelementFeilmelding } from 'nav-frontend-skjema';
 import * as React from 'react';
 import ReactDatePicker, { ReactDatePickerProps } from 'react-datepicker';
 import { v4 as uuid } from 'uuid';
+
+import styles from './datePicker.module.less';
 
 interface Props extends Omit<ReactDatePickerProps<StrictModifiers>, 'selected' | 'value'> {
     label: string;
@@ -16,7 +19,7 @@ const DatePicker = (
     ref: React.ForwardedRef<any>
 ) => {
     return (
-        <div className={className}>
+        <div className={classNames(styles.container, className)}>
             <Label htmlFor={id}>{label}</Label>
             <ReactDatePicker<StrictModifiers> id={id} selected={value} {...datePickerProps} ref={ref} />
             {feil && <SkjemaelementFeilmelding>{feil}</SkjemaelementFeilmelding>}
