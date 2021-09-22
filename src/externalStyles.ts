4949c1a0237904b9341bdb69984afb832e30354b
Ingar Almklov <ingara@gmail.com>
2021-09-22 12:29:16 +0200

    Specify base font-size

--- src/externalStyles.ts
+++ src/externalStyles.ts
@@ -3,5 +3,5 @@ import 'nav-frontend-popover-style';
 import 'nav-frontend-tabell-style';
 import 'nav-frontend-lesmerpanel-style';
 import 'react-datepicker/dist/react-datepicker.css';
-import './Root.less';
 import '@navikt/ds-css';
+import './Root.less';
