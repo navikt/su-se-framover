4441062e3b60f9bccadae64133cd158a4d8bee99
Ingar Almklov <ingara@gmail.com>
2021-10-28 11:55:40 +0200

    Improve spacings in for-veileder step in søknad

--- src/pages/søknad/steg/for-veileder/ForVeileder.tsx
+++ src/pages/søknad/steg/for-veileder/ForVeileder.tsx
@@ -1,4 +1,4 @@
-import { Alert, Panel, RadioGroup, Radio, Label, BodyShort } from '@navikt/ds-react';
+import { Alert, RadioGroup, Radio, Label, BodyShort } from '@navikt/ds-react';
 import { useFormik } from 'formik';
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
@@ -9,6 +9,7 @@ import søknadSlice, { ForVeilederDigitalSøknad } from '~/features/søknad/søk
 import { Person } from '~api/personApi';
 import Feiloppsummering from '~components/feiloppsummering/Feiloppsummering';
 import TextProvider from '~components/TextProvider';
+import SøknadSpørsmålsgruppe from '~features/søknad/søknadSpørsmålsgruppe/SøknadSpørsmålsgruppe';
 import { Vergemål } from '~features/søknad/types';
 import { focusAfterTimeout } from '~lib/formUtils';
 import { useI18n, Languages } from '~lib/i18n';
@@ -17,7 +18,6 @@ import yup, { formikErrorsHarFeil, formikErrorsTilFeiloppsummering } from '~lib/
 import { useAppDispatch, useAppSelector } from '~redux/Store';
 import { Søknadstype } from '~types/Søknad';
 
-import SøknadSpørsmålsgruppe from '../../../../features/søknad/søknadSpørsmålsgruppe/SøknadSpørsmålsgruppe';
 import Bunnknapper from '../../bunnknapper/Bunnknapper';
 import sharedStyles from '../../steg-shared.module.less';
 import sharedI18n from '../steg-shared-i18n';
@@ -77,111 +77,108 @@ const ForVeileder = (props: { forrigeUrl: string; nesteUrl: string; avbrytUrl: s
     const { intl } = useI18n({ messages: { ...sharedI18n, ...messages } });
     return (
         <TextProvider messages={{ [Languages.nb]: messages }}>
-            <div className={sharedStyles.container}>
-                <form
-                    onSubmit={(e) => {
-                        setHasSubmitted(true);
-                        formik.handleSubmit(e);
-                        focusAfterTimeout(feiloppsummeringref)();
-                    }}
-                >
-                    <Panel border className={styles.panelMargin}>
-                        <div className={styles.infoboks}>
-                            <Label spacing>{intl.formatMessage({ id: 'info.kontaktinfo.tittel' })}</Label>
-                            {kontaktinfo ? (
-                                <div>
-                                    <BodyShort>{telefonnummerKrr}</BodyShort>
-                                    <BodyShort>{epostKrr}</BodyShort>
-                                </div>
-                            ) : (
-                                <BodyShort>{intl.formatMessage({ id: 'info.kontaktinfo.mangler' })}</BodyShort>
-                            )}
-                        </div>
-                        <div className={styles.infoboks}>
-                            <Label spacing>{intl.formatMessage({ id: 'info.telefon.tittel' })}</Label>
-                            <BodyShort>{telefonnummerPdl}</BodyShort>
-                        </div>
-                        <Alert variant="info" className={styles.marginTopXSS}>
-                            {intl.formatMessage({ id: 'info.telefon.body' })}
-                        </Alert>
-                    </Panel>
+            <form
+                className={sharedStyles.container}
+                onSubmit={(e) => {
+                    setHasSubmitted(true);
+                    formik.handleSubmit(e);
+                    focusAfterTimeout(feiloppsummeringref)();
+                }}
+            >
+                <div className={sharedStyles.marginBottom}>
+                    <div className={styles.infoboks}>
+                        <Label spacing>{intl.formatMessage({ id: 'info.kontaktinfo.tittel' })}</Label>
+                        {kontaktinfo ? (
+                            <div>
+                                <BodyShort>{telefonnummerKrr}</BodyShort>
+                                <BodyShort>{epostKrr}</BodyShort>
+                            </div>
+                        ) : (
+                            <BodyShort>{intl.formatMessage({ id: 'info.kontaktinfo.mangler' })}</BodyShort>
+                        )}
+                    </div>
+                    <div className={styles.infoboks}>
+                        <Label spacing>{intl.formatMessage({ id: 'info.telefon.tittel' })}</Label>
+                        <BodyShort>{telefonnummerPdl}</BodyShort>
+                    </div>
+                    <Alert variant="info" className={styles.marginTopXSS}>
+                        {intl.formatMessage({ id: 'info.telefon.body' })}
+                    </Alert>
+                </div>
 
-                    <Panel border className={styles.panelMargin}>
-                        <div className={styles.infoboks}>
-                            <Label spacing>{intl.formatMessage({ id: 'info.kontaktform.tittel' })}</Label>
-                            {kontaktinfo ? (
-                                <BodyShort>
-                                    {digitalBruker ? 'Digital' : 'Reservert mot digital kommunikasjon'}
-                                </BodyShort>
-                            ) : (
-                                <BodyShort>{intl.formatMessage({ id: 'info.kontaktinfo.mangler' })}</BodyShort>
-                            )}
-                        </div>
-                        <Alert variant="info" className={styles.marginTopXSS}>
-                            {intl.formatMessage({ id: 'info.kontaktform.body' })}
-                        </Alert>
-                    </Panel>
+                <div className={sharedStyles.marginBottom}>
+                    <div className={styles.infoboks}>
+                        <Label spacing>{intl.formatMessage({ id: 'info.kontaktform.tittel' })}</Label>
+                        {kontaktinfo ? (
+                            <BodyShort>{digitalBruker ? 'Digital' : 'Reservert mot digital kommunikasjon'}</BodyShort>
+                        ) : (
+                            <BodyShort>{intl.formatMessage({ id: 'info.kontaktinfo.mangler' })}</BodyShort>
+                        )}
+                    </div>
+                    <Alert variant="info" className={styles.marginTopXSS}>
+                        {intl.formatMessage({ id: 'info.kontaktform.body' })}
+                    </Alert>
+                </div>
 
-                    <SøknadSpørsmålsgruppe withoutLegend>
-                        <BooleanRadioGroup
-                            name="harSøkerMøttPersonlig"
-                            legend={<FormattedMessage id="input.harSøkerMøttPersonlig.label" />}
-                            error={formik.errors.harSøkerMøttPersonlig}
-                            value={formik.values.harSøkerMøttPersonlig}
-                            onChange={(val) => {
+                <SøknadSpørsmålsgruppe withoutLegend>
+                    <BooleanRadioGroup
+                        name="harSøkerMøttPersonlig"
+                        legend={<FormattedMessage id="input.harSøkerMøttPersonlig.label" />}
+                        error={formik.errors.harSøkerMøttPersonlig}
+                        value={formik.values.harSøkerMøttPersonlig}
+                        onChange={(val) => {
+                            formik.setValues((values) => ({
+                                ...values,
+                                harSøkerMøttPersonlig: val,
+                                harFullmektigEllerVerge: null,
+                            }));
+                        }}
+                    />
+
+                    {formik.values.harSøkerMøttPersonlig === false && (
+                        <RadioGroup
+                            error={formik.errors.harFullmektigEllerVerge}
+                            legend={<FormattedMessage id={'input.fullmektigEllerVerge.label'} />}
+                            name="harFullmektigEllerVerge"
+                            onChange={(value) => {
                                 formik.setValues((values) => ({
                                     ...values,
-                                    harSøkerMøttPersonlig: val,
-                                    harFullmektigEllerVerge: null,
+                                    harFullmektigEllerVerge: value as Vergemål,
                                 }));
                             }}
-                        />
+                            value={formik.values.harFullmektigEllerVerge?.toString()}
+                        >
+                            <Radio id="harFullmektigEllerVerge" value={Vergemål.Fullmektig}>
+                                <FormattedMessage id={'input.fullmektigEllerVerge.fullmektig.label'} />
+                            </Radio>
+                            <Radio value={Vergemål.Verge}>
+                                <FormattedMessage id={'input.fullmektigEllerVerge.verge.label'} />
+                            </Radio>
+                        </RadioGroup>
+                    )}
 
-                        {formik.values.harSøkerMøttPersonlig === false && (
-                            <RadioGroup
-                                error={formik.errors.harFullmektigEllerVerge}
-                                legend={<FormattedMessage id={'input.fullmektigEllerVerge.label'} />}
-                                name="harFullmektigEllerVerge"
-                                onChange={(value) => {
-                                    formik.setValues((values) => ({
-                                        ...values,
-                                        harFullmektigEllerVerge: value as Vergemål,
-                                    }));
-                                }}
-                                value={formik.values.harFullmektigEllerVerge?.toString()}
-                            >
-                                <Radio id="harFullmektigEllerVerge" value={Vergemål.Fullmektig}>
-                                    <FormattedMessage id={'input.fullmektigEllerVerge.fullmektig.label'} />
-                                </Radio>
-                                <Radio value={Vergemål.Verge}>
-                                    <FormattedMessage id={'input.fullmektigEllerVerge.verge.label'} />
-                                </Radio>
-                            </RadioGroup>
-                        )}
-
-                        {formik.values.harFullmektigEllerVerge === 'fullmektig' && (
-                            <Alert variant="warning">Husk å legge ved legeattest/legeerklæring</Alert>
-                        )}
-                    </SøknadSpørsmålsgruppe>
-                    <Feiloppsummering
-                        className={sharedStyles.marginBottom}
-                        tittel={intl.formatMessage({ id: 'feiloppsummering.title' })}
-                        feil={formikErrorsTilFeiloppsummering(formik.errors)}
-                        hidden={!formikErrorsHarFeil(formik.errors)}
-                        ref={feiloppsummeringref}
-                    />
-                    <Bunnknapper
-                        previous={{
-                            onClick: () => {
-                                history.push(props.forrigeUrl);
-                            },
-                        }}
-                        avbryt={{
-                            toRoute: props.avbrytUrl,
-                        }}
-                    />
-                </form>
-            </div>
+                    {formik.values.harFullmektigEllerVerge === 'fullmektig' && (
+                        <Alert variant="warning">Husk å legge ved legeattest/legeerklæring</Alert>
+                    )}
+                </SøknadSpørsmålsgruppe>
+                <Feiloppsummering
+                    className={sharedStyles.marginBottom}
+                    tittel={intl.formatMessage({ id: 'feiloppsummering.title' })}
+                    feil={formikErrorsTilFeiloppsummering(formik.errors)}
+                    hidden={!formikErrorsHarFeil(formik.errors)}
+                    ref={feiloppsummeringref}
+                />
+                <Bunnknapper
+                    previous={{
+                        onClick: () => {
+                            history.push(props.forrigeUrl);
+                        },
+                    }}
+                    avbryt={{
+                        toRoute: props.avbrytUrl,
+                    }}
+                />
+            </form>
         </TextProvider>
     );
 };
