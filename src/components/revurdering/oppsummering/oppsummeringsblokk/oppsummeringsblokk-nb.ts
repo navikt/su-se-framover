061bfc84f2cb31aedc4c2a3f0151338ab4052d25
Christian H. Mosveen <mosveen@gmail.com>
2022-01-11 13:46:45 +0100

    Vis resultatet i oppsummeringen av revurderinger

--- src/components/revurdering/oppsummering/oppsummeringsblokk/oppsummeringsblokk-nb.ts
+++ src/components/revurdering/oppsummering/oppsummeringsblokk/oppsummeringsblokk-nb.ts
@@ -1,10 +1,20 @@
 export default {
     heading: 'Oppsummering fra revurdering',
 
+    'label.resultat': 'Resultat',
     'label.saksbehandler': 'Saksbehandler',
     'label.attestant': 'Attestant',
     'label.periode': 'Revurderingsperiode',
     'label.startet': 'Revurdering startet',
     'label.årsak': 'Årsak for revurdering',
     'label.begrunnelse': 'Revurderingsbegrunnelse',
+
+    'vurdering.ingenEndring': 'Ingen endring',
+    'vurdering.opphør': 'Opphør',
+    'vurdering.endring': 'Endring',
+    'vurdering.ukjent': 'Ukjent',
+    'vurdering.opprettet': 'Ny',
+    'vurdering.avsluttet': 'Avsluttet',
+    'vurdering.stans': 'Stanset',
+    'vurdering.gjenopptak': 'Gjenopptak',
 };
