1fb6707da5522fc06480c358fe9b69f48fa1d55b
Simen Johnsrud <simen.johnsrud92@gmail.com>
2022-01-12 11:02:33 +0100

    Bedre tekster til innkalling av kontrollsamtale

--- src/pages/kompetansesamtale/message-nb.ts
+++ src/pages/kompetansesamtale/message-nb.ts
@@ -1,11 +1,11 @@
 export default {
     kontrollsamtale: 'Kontrollsamtale',
-    nestePlanlagt: 'Neste planlagte kontrollsamtale: ',
+    nestePlanlagt: 'Innkalling til neste planlagte kontrollsamtale: ',
     ingenPlanlagt: 'Ingen planlagte kontrollsamtaler',
     settNyDato: 'Sett ny dato',
-    velgDatoTittel: 'Velg dato for neste kontrollsamtale',
+    velgDatoTittel: 'Velg dato for innkalling til neste kontrollsamtale',
     datovalidering: 'Ingen dato er satt',
     kunneIkkeSetteDato: 'Kunne ikke sette ny dato',
-    nyDatoBekreftelse: 'Dato for neste kontrollsamtale er oppdatert',
+    nyDatoBekreftelse: 'Dato for innkalling til neste kontrollsamtale er oppdatert',
     tilbake: 'Tilbake',
 };
