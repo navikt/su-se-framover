0e7156ab4292ffbd06015eb348b41dc6d09338dd
ramziabuqassim <ramzi.abu.qassim@nav.no>
2022-01-19 14:25:51 +0100

    legg til klage for åpne behandlinger

--- src/types/Restans.ts
+++ src/types/Restans.ts
@@ -11,6 +11,7 @@ export interface Restans {
 export enum RestansType {
     SØKNADSBEHANDLING = 'SØKNADSBEHANDLING',
     REVURDERING = 'REVURDERING',
+    KLAGE = 'KLAGE',
 }
 
 export enum RestansStatus {
