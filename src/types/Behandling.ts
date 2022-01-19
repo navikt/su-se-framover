6f1dc42b400183a68fce1dae547c25491b98945e
ramziabuqassim <ramzi.abu.qassim@nav.no>
2022-01-19 12:38:30 +0100

    fiks skrivefeil

--- src/types/Behandling.ts
+++ src/types/Behandling.ts
@@ -87,6 +87,6 @@ export const underkjennelsesGrunnTextMapper: { [key in UnderkjennelseGrunn]: str
     [UnderkjennelseGrunn.ANDRE_FORHOLD]: 'Andre forhold',
     [UnderkjennelseGrunn.BEREGNINGEN_ER_FEIL]: 'Beregningen er feil',
     [UnderkjennelseGrunn.DOKUMENTASJON_MANGLER]: 'Dokumentasjon mangler',
-    [UnderkjennelseGrunn.INNGANGSVILKÅRENE_ER_FEILVURDERT]: 'Ingangsvilkårene er feilvurdert',
+    [UnderkjennelseGrunn.INNGANGSVILKÅRENE_ER_FEILVURDERT]: 'Inngangsvilkårene er feilvurdert',
     [UnderkjennelseGrunn.VEDTAKSBREVET_ER_FEIL]: 'Vedtaksbrev er feil',
 };
