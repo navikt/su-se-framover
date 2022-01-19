801d4795940ba0eb5a12c9297cfbab785efb4779
Simen Johnsrud <simen.johnsrud92@gmail.com>
2022-01-19 14:46:08 +0100

    Legg til apierror-koder

--- src/components/apiErrorAlert/ApiErrorAlert.tsx
+++ src/components/apiErrorAlert/ApiErrorAlert.tsx
@@ -1,4 +1,5 @@
 import { Alert } from '@navikt/ds-react';
+import classNames from 'classnames';
 import React from 'react';
 
 import { ApiError } from '~api/apiClient';
@@ -8,11 +9,16 @@ import messages from './ApiErrorAlert-nb';
 import styles from './apierroralert.module.less';
 import { ApiErrorCode } from './apiErrorCode';
 
-const ApiErrorAlert = ({ error }: { error?: ApiError }) => {
+interface Props {
+    error?: ApiError;
+    className?: string;
+}
+
+const ApiErrorAlert = ({ error, className = '' }: Props) => {
     const { formatMessage } = useI18n({ messages });
 
     return (
-        <Alert variant="error" className={styles.alertstripe}>
+        <Alert variant="error" className={classNames(className, styles.alertstripe)}>
             {formatMessage(error?.body?.code ?? ApiErrorCode.UKJENT_FEIL)}
         </Alert>
     );
