2e4a30e289d5c47eb28e311ea37a4246c4f2ca0e
Simen Johnsrud <simen.johnsrud92@gmail.com>
2022-02-11 14:33:53 +0100

    Fjerne feilaktig summering i nøkkeltall

--- src/utils/nøkkeltall/nøkkeltallUtils.ts
+++ src/utils/nøkkeltall/nøkkeltallUtils.ts
@@ -1,7 +1,6 @@
-import { Nøkkeltall, Søknader } from '~types/Nøkkeltall';
+import { Søknader } from '~types/Nøkkeltall';
 
 export const ferdigBehandlet = (søknader: Søknader) =>
     søknader.iverksatteInnvilget + søknader.iverksatteAvslag + søknader.lukket;
 export const ikkeFerdigbehandlet = (søknader: Søknader) => søknader.påbegynt + søknader.ikkePåbegynt;
 export const søknader = (søknader: Søknader) => søknader.digitalsøknader + søknader.papirsøknader;
-export const brukere = (nøkkeltall: Nøkkeltall) => nøkkeltall.antallUnikePersoner + nøkkeltall.løpendeSaker;
