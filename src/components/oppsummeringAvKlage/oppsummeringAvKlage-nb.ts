816ef99afd453424c973f9b4dc96535447ff0e3e
ramziabuqassim <ramzi.abu.qassim@nav.no>
2022-03-07 11:42:15 +0100

    fks små-ting ved klage

--- src/components/oppsummeringAvKlage/oppsummeringAvKlage-nb.ts
+++ src/components/oppsummeringAvKlage/oppsummeringAvKlage-nb.ts
@@ -6,7 +6,7 @@ export default {
     'label.klageMottatt': 'Klage opprettet',
     'label.årsaksutfall': 'Utfall',
 
-    'label.vurdering.tittel': 'vurdering',
+    'label.vurdering.tittel': 'VURDERING',
     'label.vurdering.avvist': 'Klagen avvist',
     'label.vurdering.opprettholdt': 'Oppretthold vedtaket',
     'label.vurdering.omgjort': 'Omgjøring av vedtak',
@@ -17,4 +17,6 @@ export default {
 
     'label.ja': 'Ja',
     'label.nei': 'Nei',
+
+    'knapp.seBrev': 'Se brev',
 };
