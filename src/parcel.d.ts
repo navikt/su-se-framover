c56c8d853d1b585dde7f21182188e89cd4212511
John Andre Hestad <john.andre.hestad@nav.no>
2022-03-30 13:25:28 +0200

    Fix typecheck + eslint errors

--- src/parcel.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '*.module.less' {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const styles: any;
-    export default styles;
-}
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,1 @@
-// declare module '*.module.less' {
-//     const styles: Record<string, string>;
-//     export default styles;
-// }
+declare module '*.module.less';
