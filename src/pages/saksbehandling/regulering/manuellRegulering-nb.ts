ec7c6ed938425669f153dc37fdb84503b8ef92be
Aryan Iranzamini <aryaniranzamini@gmail.com>
2022-04-29 10:32:15 +0200

    Endre tekst på varslinger i manuell regulering

--- src/pages/saksbehandling/regulering/manuellRegulering-nb.ts
+++ src/pages/saksbehandling/regulering/manuellRegulering-nb.ts
@@ -13,11 +13,12 @@ export default {
     'knapper.send': 'Gjennomfør regulering',
     notification: 'Regulering har blitt gjennomført',
 
-    'manuell.årsak.stans': 'Denne saken er stanset og må gjenopptas først.',
+    'manuell.årsak.stans': 'Denne saken må reguleres manuelt fordi den var stanset da vi kjørte automatisk regulering.',
     'manuell.årsak.hull':
         'Reguleringsperioden innehåller perioder med delvis opphør og/eller hull i tidslinjen. Denne saken må revurderes.',
     'manuell.årsak.avkorting': 'Denne brukern har måneder med avkorting. Saken må revurderes.',
-    'manuell.årsak.avventarKravgrunnlag': 'Denne brukern avventer kravgrunnlag.',
+    'manuell.årsak.avventarKravgrunnlag':
+        'Denne saken må reguleres manuelt fordi saken avventet kravgrunnlag da vi kjørte automatisk regulering.',
     'manuell.årsak.utbetalingFeilet':
-        'En feil skjedde under utbetaling ved automatisk regulering, og er derfor satt til manuell behandling.',
+        'En feil skjedde under utbetaling ved regulering, og er derfor satt til manuell behandling.',
 };
