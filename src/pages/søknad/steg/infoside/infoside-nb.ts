79cdf63352e2a2707d15252489d54d5d74cbf4f7
Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>
2022-05-23 10:25:35 +0200

    Infoside for SU alder (#1866)
    * Infoside for SU alder
    
    Gjenbruker i hovedsak infosiden for SU ufør, og bytter til
    spesialiserte tekster der det er forskjeller. Det er også en
    liten endring med på dokumentasjon-siden (Inngang), som skal
    være for både alder og ufør.
    
    * Legger til oppdatert tekst for henting info
    
    * Oppdatert tekst overskrift

--- src/pages/søknad/steg/infoside/infoside-nb.ts
+++ src/pages/søknad/steg/infoside/infoside-nb.ts
@@ -1,10 +1,14 @@
 export default {
-    'suppstønadInfo.kanFåSupp':
-        'Du kan få supplerende stønad hvis du er <b>flyktning</b>, <b>ufør</b> og har <b>bodd kort tid i Norge</b>.',
-    'suppstønadInfo.ingress':
+    'suppstønadInfo.kanFåSupp.ufør':
+        'Du kan få supplerende stønad hvis du er <strong>flyktning</strong>, <strong>ufør</strong> og har <strong>bodd kort tid i Norge</strong>.',
+    'suppstønadInfo.kanFåSupp.alder':
+        'Du kan få supplerende stønad hvis du har <strong>fylt 67 år og bodd kort tid i Norge</strong>.',
+    'suppstønadInfo.ingress.ufør':
         'Stønaden vil sikre deg en inntekt som er det minste du kan få i uføretrygd. Inntekten din og inntekten til samboeren eller ektefellen din, avgjør hvor mye penger du har rett til. Om du bor alene eller sammen med andre avgjør også hvor mye du har rett til.',
-
+    'suppstønadInfo.ingress.alder':
+        'Stønaden vil sikre deg en inntekt som svarer til folketrygdens garantipensjon for alderspensjonister. Inntekten din og inntekten til samboeren eller ektefellen din, avgjør hvor mye penger du har rett til. Om du bor alene eller sammen med andre avgjør også hvor mye du har rett til.',
     'suppstønad.merOmSuForUføre': 'Mer om supplerende stønad for uføre flyktninger',
+    'suppstønad.merOmSuAlder': 'Mer om supplerende stønad for personer som har fylt 67 år',
 
     'henterInnInfo.ingress': 'Vi vil hente informasjon om deg',
     'henterInnInfo.viHenterInfo':
@@ -13,6 +17,7 @@ export default {
     'henterInnInfo.viHenter.arbeidsforhold':
         'opplysninger om arbeidsforholdet ditt fra arbeidsgiver- og arbeidstakerregisteret',
     'henterInnInfo.viHenter.flyktningsstatus': 'flyktningstatus og oppholdstillatelse fra UDI',
+    'henterInnInfo.viHenter.oppholdstillatelse': 'oppholdstillatelse fra UDI',
     'henterInnInfo.brukerTidligereOpplysninger':
         'Hvis det er nødvendig kan vi bruke opplysninger du har gitt oss før, eller som du har gitt oss i andre sammenhenger.',
     'henterInnInfo.personvernLinkTekst': 'Mer om hvordan NAV håndterer dine personopplysninger',
@@ -26,5 +31,5 @@ export default {
         'Mangler du dokumentasjon, kan du sende den inn på et senere tidspunkt. Du finner mer om dette på <navLink>nav.no.</navLink>',
 
     'page.tittel.uføre': 'Start en ny søknad om supplerende stønad for uføre flyktninger',
-    'page.tittel.alder': 'Start en ny søknad om supplerende stønad for alder',
+    'page.tittel.alder': 'Start en ny søknad om supplerende stønad for personer som har fylt 67 år',
 };
--- src/pages/søknad/utils.ts
+++ src/pages/søknad/utils.ts
@@ -1,13 +1,13 @@
 import { Søknadstema } from '~src/types/Søknad';
 
-export const getSøknadstematekst = (
+export function getSøknadstematekst<AlderTekst extends string, UføreTekst extends string>(
     søknadstema: Søknadstema,
-    text: { [Søknadstema.Uføre]: string; [Søknadstema.Alder]: string }
-): string => {
+    text: { [Søknadstema.Uføre]: UføreTekst; [Søknadstema.Alder]: AlderTekst }
+): AlderTekst | UføreTekst {
     switch (søknadstema) {
         case Søknadstema.Alder:
             return text[Søknadstema.Alder];
         case Søknadstema.Uføre:
             return text[Søknadstema.Uføre];
     }
-};
+}
