661b8676cb50a700566f1543bd4123d50c58088f
Simen <simen.johnsrud92@gmail.com>
2022-06-13 15:58:01 +0200

    Skrive om 4 steg til å bruke en generisk søknadsbehandlingswrapper

---
+++ src/pages/saksbehandling/søknadsbehandling/SøknadsbehandlingWrapper.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { ReactElement } from 'react';
+import { UseFormReturn } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+
+import * as RemoteData from '~node_modules/@devexperts/remote-data-ts';
+import { FieldValues } from '~node_modules/react-hook-form/dist/types/fields';
+import ApiErrorAlert from '~src/components/apiErrorAlert/ApiErrorAlert';
+import Feiloppsummering from '~src/components/feiloppsummering/Feiloppsummering';
+import { ApiResult } from '~src/lib/hooks';
+import { useI18n } from '~src/lib/i18n';
+import { hookFormErrorsTilFeiloppsummering } from '~src/lib/validering';
+import { RevurderingBunnknapper } from '~src/pages/saksbehandling/revurdering/bunnknapper/RevurderingBunnknapper';
+import * as styles from '~src/pages/saksbehandling/steg/uføre/uførhet.module.less';
+import stegSharedI18n from '~src/pages/søknad/steg/steg-shared-i18n';
+import { Behandling } from '~src/types/Behandling';
+
+interface Props<T, U> {
+    form: UseFormReturn<T>;
+    save: (values: T, onSuccess: (res?: U) => void) => void;
+    savingState: ApiResult<unknown>;
+    onSuccess?: (res: U) => void;
+    avsluttUrl: string;
+    forrigeUrl: string;
+    nesteUrl: string;
+    visModal?: boolean;
+    children: ReactElement;
+}
+
+export const SøknadsbehandlingWrapper = <T extends FieldValues, U extends Behandling>({
+    form,
+    ...props
+}: Props<T, U>) => {
+    const { formatMessage } = useI18n({ messages: stegSharedI18n });
+    const feiloppsummeringRef = React.useRef<HTMLDivElement>(null);
+    const navigate = useNavigate();
+
+    return (
+        <form
+            onSubmit={form.handleSubmit<T>((values) =>
+                props.save(values, (res) => {
+                    props.onSuccess && res ? props.onSuccess(res) : navigate(props.nesteUrl);
+                })
+            )}
+        >
+            {props.children}
+            <Feiloppsummering
+                tittel={formatMessage('feiloppsummering.title')}
+                className={styles.feiloppsummering}
+                feil={hookFormErrorsTilFeiloppsummering(form.formState.errors)}
+                hidden={hookFormErrorsTilFeiloppsummering(form.formState.errors).length === 0}
+                ref={feiloppsummeringRef}
+            />
+            {RemoteData.isFailure(props.savingState) && <ApiErrorAlert error={props.savingState.error} />}
+            <RevurderingBunnknapper
+                tilbake={{ url: props.forrigeUrl, visModal: props.visModal ?? false }}
+                onLagreOgFortsettSenereClick={form.handleSubmit<T>((values) =>
+                    props.save(values, () => navigate(props.avsluttUrl))
+                )}
+                loading={RemoteData.isPending(props.savingState)}
+            />
+        </form>
+    );
+};
