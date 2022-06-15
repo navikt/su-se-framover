99b04fdf0f638886f0519476e68f0a6324a67105
Simen <simen.johnsrud92@gmail.com>
2022-06-15 11:39:45 +0200

    La til søknadsbehandlingswrapper på resten av vilkårene i ufør

--- src/pages/saksbehandling/søknadsbehandling/institusjonsopphold/Institusjonsopphold.tsx
+++ src/pages/saksbehandling/søknadsbehandling/institusjonsopphold/Institusjonsopphold.tsx
@@ -1,30 +1,25 @@
-import * as RemoteData from '@devexperts/remote-data-ts';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Radio, RadioGroup } from '@navikt/ds-react';
 import { struct } from 'fp-ts/Eq';
 import * as S from 'fp-ts/string';
-import React, { useRef } from 'react';
+import React from 'react';
 import { Controller, useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
 
-import ApiErrorAlert from '~src/components/apiErrorAlert/ApiErrorAlert';
-import Feiloppsummering from '~src/components/feiloppsummering/Feiloppsummering';
 import { InstitusjonsoppholdBlokk } from '~src/components/oppsummering/vilkårsOppsummering/faktablokk/faktablokker/InstitusjonsoppholdBlokk';
 import ToKolonner from '~src/components/toKolonner/ToKolonner';
 import { useSøknadsbehandlingDraftContextFor } from '~src/context/søknadsbehandlingDraftContext';
 import * as sakSlice from '~src/features/saksoversikt/sak.slice';
-import { focusAfterTimeout } from '~src/lib/formUtils';
 import { useAsyncActionCreator } from '~src/lib/hooks';
 import { useI18n } from '~src/lib/i18n';
 import * as Routes from '~src/lib/routes';
 import { eqNullable, Nullable } from '~src/lib/types';
-import yup, { hookFormErrorsTilFeiloppsummering } from '~src/lib/validering';
+import yup from '~src/lib/validering';
+import { SøknadsbehandlingWrapper } from '~src/pages/saksbehandling/søknadsbehandling/SøknadsbehandlingWrapper';
 import { Vilkårstatus } from '~src/types/Behandlingsinformasjon';
 import { Vilkårtype } from '~src/types/Vilkårsvurdering';
 
 import sharedI18n from '../sharedI18n-nb';
 import { VilkårsvurderingBaseProps } from '../types';
-import { Vurderingknapper } from '../vurderingknapper/Vurderingknapper';
 
 import messages from './institusjonsopphold-nb';
 
@@ -46,8 +41,6 @@ const schema = yup
     .required();
 
 const Institusjonsopphold = (props: VilkårsvurderingBaseProps) => {
-    const navigate = useNavigate();
-    const feiloppsummeringRef = useRef<HTMLDivElement>(null);
     const { formatMessage } = useI18n({ messages: { ...sharedI18n, ...messages } });
     const [status, lagreBehandlingsinformasjon] = useAsyncActionCreator(sakSlice.lagreBehandlingsinformasjon);
     const initialValues = {
@@ -59,38 +52,27 @@ const Institusjonsopphold = (props: VilkårsvurderingBaseProps) => {
             eqFormData.equals(values, initialValues)
         );
 
-    const {
-        formState: { isValid, isSubmitted, errors },
-        ...form
-    } = useForm({
+    const form = useForm({
         defaultValues: draft ?? initialValues,
         resolver: yupResolver(schema),
     });
 
     useDraftFormSubscribe(form.watch);
 
-    const handleSave = (nesteUrl: string) => async (values: InstitusjonsoppholdFormData) => {
-        if (!values.status) return;
-
-        if (eqFormData.equals(values, initialValues)) {
-            clearDraft();
-            navigate(nesteUrl);
-            return;
-        }
-
+    const handleSave = async (values: InstitusjonsoppholdFormData, onSuccess: () => void) => {
         await lagreBehandlingsinformasjon(
             {
                 sakId: props.sakId,
                 behandlingId: props.behandling.id,
                 behandlingsinformasjon: {
                     institusjonsopphold: {
-                        status: values.status,
+                        status: values.status!,
                     },
                 },
             },
             () => {
                 clearDraft();
-                navigate(nesteUrl);
+                onSuccess();
             }
         );
     };
@@ -99,8 +81,13 @@ const Institusjonsopphold = (props: VilkårsvurderingBaseProps) => {
         <ToKolonner tittel={formatMessage('page.tittel')}>
             {{
                 left: (
-                    <form
-                        onSubmit={form.handleSubmit(handleSave(props.nesteUrl), focusAfterTimeout(feiloppsummeringRef))}
+                    <SøknadsbehandlingWrapper
+                        form={form}
+                        save={handleSave}
+                        savingState={status}
+                        avsluttUrl={Routes.saksoversiktValgtSak.createURL({ sakId: props.sakId })}
+                        forrigeUrl={props.forrigeUrl}
+                        nesteUrl={props.nesteUrl}
                     >
                         <Controller
                             control={form.control}
@@ -122,25 +109,7 @@ const Institusjonsopphold = (props: VilkårsvurderingBaseProps) => {
                                 </RadioGroup>
                             )}
                         />
-
-                        {RemoteData.isFailure(status) && <ApiErrorAlert error={status.error} />}
-                        <Feiloppsummering
-                            tittel={formatMessage('feiloppsummering.title')}
-                            hidden={!isSubmitted || isValid}
-                            feil={hookFormErrorsTilFeiloppsummering(errors)}
-                            ref={feiloppsummeringRef}
-                        />
-                        <Vurderingknapper
-                            onTilbakeClick={() => {
-                                navigate(props.forrigeUrl);
-                            }}
-                            onLagreOgFortsettSenereClick={form.handleSubmit(
-                                handleSave(Routes.saksoversiktValgtSak.createURL({ sakId: props.sakId })),
-                                focusAfterTimeout(feiloppsummeringRef)
-                            )}
-                            loading={RemoteData.isPending(status)}
-                        />
-                    </form>
+                    </SøknadsbehandlingWrapper>
                 ),
                 right: <InstitusjonsoppholdBlokk søknadInnhold={props.behandling.søknad.søknadInnhold} />,
             }}
