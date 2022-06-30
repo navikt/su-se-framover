2bcd456f6ac7dcceb4a2970676a5ab5fce7c3c90
ramziabuqassim <ramzi.abu.qassim@nav.no>
2022-06-30 14:38:29 +0200

    vis gjeldende lovligopphold

--- src/pages/saksbehandling/revurdering/oppholdstillatelse/GjeldendeLovligOpphold.tsx
+++ src/pages/saksbehandling/revurdering/oppholdstillatelse/GjeldendeLovligOpphold.tsx
@@ -1,11 +1,45 @@
+import { Heading } from '@navikt/ds-react';
 import React from 'react';
 
+import { OppsummeringPar } from '~src/components/revurdering/oppsummering/oppsummeringspar/Oppsummeringsverdi';
+import { useI18n } from '~src/lib/i18n';
 import { Nullable } from '~src/lib/types';
 import { LovligOppholdVilkår } from '~src/types/grunnlagsdataOgVilkårsvurderinger/lovligOpphold/LovligOppholdVilkår';
 
+import messages from './gjeldendeLovligOpphold-nb';
+import styles from './gjeldendeLovligOpphold.module.less';
+
 const GjeldendeOppholdstillatelse = (props: { gjeldendeOppholdstillatelse: Nullable<LovligOppholdVilkår> }) => {
-    console.log(props.gjeldendeOppholdstillatelse);
-    return <div>hei hei</div>;
+    const { formatMessage } = useI18n({ messages });
+    return (
+        <div>
+            <Heading level="2" size="large" spacing>
+                {formatMessage('eksisterende.vedtakinfo.tittel')}
+            </Heading>
+
+            <ul className={styles.grunnlagsliste}>
+                {props.gjeldendeOppholdstillatelse?.vurderinger?.map((oppholdstillatelse) => (
+                    <li key={`${oppholdstillatelse.periode.fraOgMed} - ${oppholdstillatelse.periode.tilOgMed}`}>
+                        <OppsummeringPar
+                            className={styles.informasjonsbitContainer}
+                            label={formatMessage('datepicker.fom')}
+                            verdi={oppholdstillatelse.periode.fraOgMed}
+                        />
+                        <OppsummeringPar
+                            className={styles.informasjonsbitContainer}
+                            label={formatMessage('datepicker.tom')}
+                            verdi={oppholdstillatelse.periode.tilOgMed}
+                        />
+                        <OppsummeringPar
+                            className={styles.informasjonsbitContainer}
+                            label={formatMessage('resultat')}
+                            verdi={oppholdstillatelse.resultat}
+                        />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default GjeldendeOppholdstillatelse;
---
+++ src/pages/saksbehandling/revurdering/oppholdstillatelse/gjeldendeLovligOpphold-nb.ts
@@ -0,0 +1,7 @@
+export default {
+    'datepicker.fom': 'Fra og med',
+    'datepicker.tom': 'Til og med',
+
+    resultat: 'Resultat',
+    'eksisterende.vedtakinfo.tittel': 'Eksisterende vedtaksinformasjon',
+};
