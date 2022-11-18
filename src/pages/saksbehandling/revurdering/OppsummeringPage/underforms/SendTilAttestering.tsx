fdca671c684f1245d4ace249c7183bcf8bdeb3df
ramziabuqassim <ramzi.abu.qassim@nav.no>
2022-11-18 16:55:24 +0100

    første bit av vedtaksbrev ved attestering

--- src/pages/saksbehandling/revurdering/OppsummeringPage/underforms/SendTilAttestering.tsx
+++ src/pages/saksbehandling/revurdering/OppsummeringPage/underforms/SendTilAttestering.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import OppsummeringAvInformasjonsrevurdering from '~src/components/revurdering/oppsummering/OppsummeringAvInformasjonsrevurdering';
+import ToKolonner from '~src/components/toKolonner/ToKolonner';
 import { GrunnlagsdataOgVilkårsvurderinger } from '~src/types/grunnlagsdataOgVilkårsvurderinger/grunnlagsdataOgVilkårsvurderinger';
 import { InformasjonsRevurdering } from '~src/types/Revurdering';
+
+import { BrevvalgForm } from '../brevvalg/BrevvalgForm';
 
 const SendTilAttestering = (props: {
     sakId: string;
@@ -9,11 +13,20 @@ const SendTilAttestering = (props: {
     gjeldendeGrunnlagOgVilkår: GrunnlagsdataOgVilkårsvurderinger;
     forrigeUrl: string;
 }) => {
-    console.log(props.sakId, props.revurdering, props.gjeldendeGrunnlagOgVilkår, props.forrigeUrl);
     return (
-        <div>
-            <div>SendTilAttestering</div>
-        </div>
+        <ToKolonner tittel={'Vedtaksbrev'}>
+            {{
+                left: (
+                    <BrevvalgForm sakId={props.sakId} revurdering={props.revurdering} forrigeUrl={props.forrigeUrl} />
+                ),
+                right: (
+                    <OppsummeringAvInformasjonsrevurdering
+                        revurdering={props.revurdering}
+                        grunnlagsdataOgVilkårsvurderinger={props.gjeldendeGrunnlagOgVilkår}
+                    />
+                ),
+            }}
+        </ToKolonner>
     );
 };
 
