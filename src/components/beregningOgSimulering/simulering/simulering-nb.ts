a884e7326a6a37bd5408adc596452356781fd292
Jacob Meidell <jacob.meidell@nav.no>
2022-12-01 08:39:36 +0100

    Fjern tekst for slettede elementer

--- src/components/beregningOgSimulering/simulering/simulering-nb.ts
+++ src/components/beregningOgSimulering/simulering/simulering-nb.ts
@@ -1,15 +1,4 @@
-import { SimulertUtbetalingstype } from '~src/types/Simulering';
-
-export const simulertUtbetaling: { [key in SimulertUtbetalingstype]: string } = {
-    [SimulertUtbetalingstype.INGEN_UTBETALING]: 'Ingen utbetaling',
-    [SimulertUtbetalingstype.ORDINÆR]: 'Ordinær',
-    [SimulertUtbetalingstype.ETTERBETALING]: 'Etterbetaling',
-    [SimulertUtbetalingstype.FEILUTBETALING]: 'Feilutbetaling',
-    [SimulertUtbetalingstype.UENDRET]: 'Uendret',
-};
-
 export default {
-    ...simulertUtbetaling,
     totaltBeløp: 'Totalt beløp',
     iMnd: 'i mnd',
 
