6579f15acbfb9ea418aa00db50dd556052f21510
ramziabuqassim <ramzi.abu.qassim@nav.no>
2023-01-16 10:16:29 +0100

    refresh state ved lagrebevvalg

--- src/features/saksoversikt/sak.slice.ts
+++ src/features/saksoversikt/sak.slice.ts
@@ -226,6 +226,10 @@ export default createSlice({
             state.sak = oppdaterRevurderingISak(state.sak, action.payload);
         });
 
+        builder.addCase(revurderingActions.lagreBrevvalg.fulfilled, (state, action) => {
+            state.sak = oppdaterRevurderingISak(state.sak, action.payload);
+        });
+
         builder.addCase(revurderingActions.lagreTilbakekrevingsbehandling.fulfilled, (state, action) => {
             state.sak = oppdaterRevurderingISak(state.sak, action.payload);
         });
