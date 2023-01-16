5d09adf58972e5dcdcf04edb8e1dfe2fd9768ac7
John Andre Hestad <john.andre.hestad@nav.no>
2023-01-16 12:20:52 +0100

    Change ecmaVersion in .eslintrc.js to latest

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
         ecmaFeatures: {
             jsx: true,
         },
-        ecmaVersion: 2018,
+        ecmaVersion: 'latest',
         sourceType: 'module',
     },
     settings: {
