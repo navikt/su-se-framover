43eadec7bea037b4ca4a084db23dedd62cb1fb21
John Andre Hestad <john.andre.hestad@nav.no>
2023-01-18 09:01:52 +0100

    Legg til informasjon om at det er NØS man kan purre ved sent kravgrunnlag

--- src/components/apiErrorAlert/ApiErrorAlert-nb.ts
+++ src/components/apiErrorAlert/ApiErrorAlert-nb.ts
@@ -310,9 +310,9 @@ const messages: { [key in ApiErrorCode]: string } = {
         'Åpen revurdering for gjenopptak av ytelse eksisterer fra før',
     [ApiErrorCode.ÅPEN_REVURDERING_STANS_EKSISTERER]: 'Åpen revurdering for stans av ytelse eksisterer fra før',
     [ApiErrorCode.ÅPENT_KRAVGRUNNLAG_MÅ_HÅNDTERES_FØR_NY_REVURDERING]:
-        'Saken avventer kravgrunnlag (forrige iverksatte utbetaling førte til feilutbetaling). Vi kan ikke sende nye utbetalinslinjer før Oppdrag returnerer kravgrunnlaget. Dette utføres ofte innen 3 virkedager, men kan ta lengre tid. Hvis det tar lenger tid er det mulig å purre',
+        'Saken avventer kravgrunnlag (forrige iverksatte utbetaling førte til feilutbetaling). Vi kan ikke sende nye utbetalinslinjer før Oppdrag returnerer kravgrunnlaget. Dette utføres ofte innen 3 virkedager, men kan ta lengre tid. Hvis det tar lenger tid er det mulig å purre NØS',
     [ApiErrorCode.ÅPENT_KRAVGRUNNLAG_MÅ_HÅNDTERES_FØR_NY_SØKNADSBEHANDLING]:
-        'Saken avventer kravgrunnlag (forrige iverksatte utbetaling førte til feilutbetaling). Vi kan ikke sende nye utbetalinslinjer før Oppdrag returnerer kravgrunnlaget. Dette utføres ofte innen 3 virkedager, men kan ta lengre tid. Hvis det tar lenger tid er det mulig å purre',
+        'Saken avventer kravgrunnlag (forrige iverksatte utbetaling førte til feilutbetaling). Vi kan ikke sende nye utbetalinslinjer før Oppdrag returnerer kravgrunnlaget. Dette utføres ofte innen 3 virkedager, men kan ta lengre tid. Hvis det tar lenger tid er det mulig å purre NØS',
 };
 export default messages;
 
