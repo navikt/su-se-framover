5154a20815dd433de7615aba6c531654f3341226
ramziabuqassim <ramzi.abu.qassim@nav.no>
2023-06-19 08:47:03 +0200

    wrap registering av klage & journalposter i paneler

--- src/components/oppsummering/journalpost/OppsummeringAvJournalposter.tsx
+++ src/components/oppsummering/journalpost/OppsummeringAvJournalposter.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 
+import Oppsummeringspanel, {
+    Oppsummeringsfarge,
+    Oppsummeringsikon,
+} from '~src/components/oppsummeringspanel/Oppsummeringspanel';
+import { useI18n } from '~src/lib/i18n';
 import { Journalpost } from '~src/types/Journalpost';
 
 import OppsummeringAvJournalpost from './OppsummeringAvJournalpost';
 import styles from './OppsummeringAvJournalpost.module.less';
+import messages from './OppsummeringAvJournalposter-nb';
 
 const OppsummeringAvJournalposter = (props: { journalposter: Journalpost[] }) => {
+    const { formatMessage } = useI18n({ messages });
+
     return (
-        <ul className={styles.oppsummeringAvJournalposterContainer}>
-            {props.journalposter.map((j) => (
-                <li key={j.id}>
-                    <OppsummeringAvJournalpost journalpost={j} />
-                </li>
-            ))}
-        </ul>
+        <Oppsummeringspanel
+            ikon={Oppsummeringsikon.Liste}
+            farge={Oppsummeringsfarge.Lilla}
+            tittel={formatMessage('journalposter.tittel')}
+        >
+            <ul className={styles.oppsummeringAvJournalposterContainer}>
+                {props.journalposter.map((j) => (
+                    <li key={j.id}>
+                        <OppsummeringAvJournalpost journalpost={j} />
+                    </li>
+                ))}
+            </ul>
+        </Oppsummeringspanel>
     );
 };
 
--- src/pages/klage/opprettKlage/OpprettKlage.tsx
+++ src/pages/klage/opprettKlage/OpprettKlage.tsx
@@ -1,6 +1,6 @@
 import * as RemoteData from '@devexperts/remote-data-ts';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Button, Heading, HelpText, Loader, TextField } from '@navikt/ds-react';
+import { Button, HelpText, Loader, TextField } from '@navikt/ds-react';
 import * as DateFns from 'date-fns';
 import React from 'react';
 import { Controller, useForm } from 'react-hook-form';
@@ -10,6 +10,10 @@ import ApiErrorAlert from '~src/components/apiErrorAlert/ApiErrorAlert';
 import { DatePicker } from '~src/components/datePicker/DatePicker';
 import HentOgVisJournalposter from '~src/components/hentOgVisJournalposter/HentOgVisJournalposter';
 import LinkAsButton from '~src/components/linkAsButton/LinkAsButton';
+import Oppsummeringspanel, {
+    Oppsummeringsfarge,
+    Oppsummeringsikon,
+} from '~src/components/oppsummeringspanel/Oppsummeringspanel';
 import { SaksoversiktContext } from '~src/context/SaksoversiktContext';
 import * as klageActions from '~src/features/klage/klageActions';
 import { useAsyncActionCreator } from '~src/lib/hooks';
@@ -34,71 +38,80 @@ const OpprettKlage = () => {
     const { formatMessage } = useI18n({ messages });
 
     return (
-        <form
-            className={styles.form}
-            onSubmit={handleSubmit((values) =>
-                opprettKlage(
-                    {
-                        sakId: props.sak.id,
-                        journalpostId: values.journalpostId,
-                        datoKlageMottatt: DateFns.formatISO(values.datoKlageMottatt, { representation: 'date' }),
-                    },
-                    (klage) => {
-                        navigate(
-                            Routes.klage.createURL({ sakId: props.sak.id, klageId: klage.id, steg: KlageSteg.Formkrav })
-                        );
-                    }
-                )
-            )}
-        >
-            <div className={styles.opprettelseContainer}>
-                <Heading level="2" size="medium">
-                    {formatMessage('opprett.tittel')}
-                </Heading>
-                <TextField
-                    {...register('journalpostId')}
-                    error={formState.errors.journalpostId?.message}
-                    label={
-                        <div className={styles.journalpostIdLabel}>
-                            {formatMessage('opprett.journalpostId.label')}
-                            <HelpText>{formatMessage('opprett.journalpostId.hjelpetekst')}</HelpText>
-                        </div>
-                    }
-                />
-
-                <Controller
-                    control={control}
-                    name="datoKlageMottatt"
-                    render={({ field, fieldState }) => (
-                        <DatePicker
-                            label={formatMessage('opprett.klageMottatt.label')}
-                            value={field.value}
-                            onChange={field.onChange}
-                            error={fieldState.error?.message}
-                            toDate={DateFns.endOfDay(new Date())}
-                            hjelpetekst={formatMessage('opprett.klageMottatt.hjelpetekst')}
-                        />
+        <div className={styles.form}>
+            <Oppsummeringspanel
+                ikon={Oppsummeringsikon.Blyant}
+                farge={Oppsummeringsfarge.Grønn}
+                tittel={formatMessage('opprett.tittel')}
+            >
+                <form
+                    onSubmit={handleSubmit((values) =>
+                        opprettKlage(
+                            {
+                                sakId: props.sak.id,
+                                journalpostId: values.journalpostId,
+                                datoKlageMottatt: DateFns.formatISO(values.datoKlageMottatt, {
+                                    representation: 'date',
+                                }),
+                            },
+                            (klage) => {
+                                navigate(
+                                    Routes.klage.createURL({
+                                        sakId: props.sak.id,
+                                        klageId: klage.id,
+                                        steg: KlageSteg.Formkrav,
+                                    })
+                                );
+                            }
+                        )
                     )}
-                />
+                >
+                    <div className={styles.opprettelseContainer}>
+                        <TextField
+                            {...register('journalpostId')}
+                            error={formState.errors.journalpostId?.message}
+                            label={
+                                <div className={styles.journalpostIdLabel}>
+                                    {formatMessage('opprett.journalpostId.label')}
+                                    <HelpText>{formatMessage('opprett.journalpostId.hjelpetekst')}</HelpText>
+                                </div>
+                            }
+                        />
 
-                <div className={styles.buttons}>
-                    <LinkAsButton
-                        variant="secondary"
-                        href={Routes.saksoversiktValgtSak.createURL({ sakId: props.sak.id })}
-                    >
-                        {formatMessage('opprett.button.tilbake')}
-                    </LinkAsButton>
-                    <Button>
-                        {formatMessage('opprett.button.submit')}
-                        {RemoteData.isPending(opprettKlageStatus) && <Loader />}
-                    </Button>
-                </div>
-                {RemoteData.isFailure(opprettKlageStatus) && <ApiErrorAlert error={opprettKlageStatus.error} />}
-            </div>
-            <div>
-                <HentOgVisJournalposter sakId={props.sak.id} />
-            </div>
-        </form>
+                        <Controller
+                            control={control}
+                            name="datoKlageMottatt"
+                            render={({ field, fieldState }) => (
+                                <DatePicker
+                                    label={formatMessage('opprett.klageMottatt.label')}
+                                    value={field.value}
+                                    onChange={field.onChange}
+                                    error={fieldState.error?.message}
+                                    toDate={DateFns.endOfDay(new Date())}
+                                    hjelpetekst={formatMessage('opprett.klageMottatt.hjelpetekst')}
+                                />
+                            )}
+                        />
+
+                        <div className={styles.buttons}>
+                            <LinkAsButton
+                                variant="secondary"
+                                href={Routes.saksoversiktValgtSak.createURL({ sakId: props.sak.id })}
+                            >
+                                {formatMessage('opprett.button.tilbake')}
+                            </LinkAsButton>
+                            <Button>
+                                {formatMessage('opprett.button.submit')}
+                                {RemoteData.isPending(opprettKlageStatus) && <Loader />}
+                            </Button>
+                        </div>
+                        {RemoteData.isFailure(opprettKlageStatus) && <ApiErrorAlert error={opprettKlageStatus.error} />}
+                    </div>
+                </form>
+            </Oppsummeringspanel>
+
+            <HentOgVisJournalposter sakId={props.sak.id} />
+        </div>
     );
 };
 
