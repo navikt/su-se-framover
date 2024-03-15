315c5b254ccaabf09745e9385846c7b6c41f8a99
ramziabuqassim <ramzi.abu.qassim@nav.no>
2024-03-15 16:02:25 +0100

    kan sende inn reguleringssliste standalone

--- src/pages/drift/components/regulering/G-regulering.tsx
+++ src/pages/drift/components/regulering/G-regulering.tsx
@@ -10,11 +10,13 @@ import {
     RadioGroup,
     Radio,
     Textarea,
+    Label,
 } from '@navikt/ds-react';
 import { useState, useEffect } from 'react';
 
 import { dryRunRegulering, startRegulering } from '~src/api/reguleringApi';
 import * as reguleringApi from '~src/api/reguleringApi';
+import ApiErrorAlert from '~src/components/apiErrorAlert/ApiErrorAlert';
 import { MonthPicker } from '~src/components/inputs/datePicker/DatePicker';
 import { pipe } from '~src/lib/fp';
 import { useApiCall } from '~src/lib/hooks';
@@ -89,6 +91,7 @@ const GReguleringsModal = (props: { visModa: boolean; onClose: () => void }) => 
                     <Tabs.List>
                         <Tabs.Tab value="dry-run" label="Dry-run" />
                         <Tabs.Tab value="regulering" label="Regulering" />
+                        <Tabs.Tab value="supplement" label="Reguleringsupplement" />
                     </Tabs.List>
                     <Tabs.Panel value="dry-run" className={styles.tabPanel}>
                         <div className={styles.panelInnholdContainer}>
@@ -156,6 +159,9 @@ const GReguleringsModal = (props: { visModa: boolean; onClose: () => void }) => 
                             )}
                         </div>
                     </Tabs.Panel>
+                    <Tabs.Panel value="supplement" className={styles.tabPanel}>
+                        <ReguleringsSupplementStandAlone />
+                    </Tabs.Panel>
                 </Tabs>
             </Modal.Body>
         </Modal>
@@ -202,6 +208,50 @@ const ReguleringsSupplement = (props: { onSupplementChange: (i: Nullable<string 
                     onChange={(e) => props.onSupplementChange(e.target.value)}
                 />
             )}
+        </div>
+    );
+};
+
+const ReguleringsSupplementStandAlone = () => {
+    const [supplement, setSupplement] = useState<Nullable<'fil' | 'text'>>(null);
+
+    const [supplementValue, setSupplementValue] = useState<Nullable<string | File>>(null);
+    const [status, sendSupplement] = useApiCall(reguleringApi.reguleringssupplement);
+
+    const onClick = () => {
+        if (!supplementValue) {
+            console.log('du hakke valgt hva du skal sende inn');
+            return;
+        }
+        sendSupplement({ innhold: supplementValue });
+    };
+
+    return (
+        <div className={styles.supplementContainer}>
+            <Label>
+                Dersom regulering er blitt kjørt, og reguleringsbehandlinger er blitt opprettet, kan du legge til et
+                supplement som kjører en del av disse behandlingenene automatisk
+            </Label>
+            <RadioGroup legend="Velg supplement">
+                <Radio value={'fil'} onClick={() => setSupplement('fil')}>
+                    Fil
+                </Radio>
+
+                <Radio value={'text'} onClick={() => setSupplement('text')}>
+                    Text
+                </Radio>
+            </RadioGroup>
+
+            {supplement === 'fil' && (
+                <input type="file" onChange={(e) => (e.target.files ? setSupplementValue(e.target.files[0]) : null)} />
+            )}
+            {supplement === 'text' && (
+                <Textarea label={'CSV'} minRows={5} maxRows={10} onChange={(e) => setSupplementValue(e.target.value)} />
+            )}
+
+            {RemoteData.isFailure(status) && <ApiErrorAlert error={status.error} />}
+
+            <Button onClick={onClick}>Oppdater regulering med supplement</Button>
         </div>
     );
 };
