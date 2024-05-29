cc25e29ba107e6f98d271111662b0157004438e4
ramziabuqassim <ramzi.abu.qassim@nav.no>
2024-05-29 10:48:02 +0200

    chore: Update button label for journaling

--- src/components/hentOgVisSkattegrunnlag/HentOgVisSkattegrunnlag.tsx
+++ src/components/hentOgVisSkattegrunnlag/HentOgVisSkattegrunnlag.tsx
@@ -348,7 +348,7 @@ export const HentOfVisSkattegrunnlagForFrioppslag = () => {
                                     });
                                 }}
                             >
-                                Journalfør likevel
+                                Journalfør
                             </Button>
                         </div>
                     )}
