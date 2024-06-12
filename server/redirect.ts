35dfb04afb0d9ef1e3f56dcf19ddb9e02c86ace6
John Andre Hestad <john.andre.hestad@nav.no>
2024-06-12 14:45:23 +0200

    Fix dev ingress

--- server/redirect.ts
+++ server/redirect.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 const redirectMiddleware = (request: Request, res: Response, next: NextFunction) => {
     const forwardedHostHeader: string | undefined = request.header('x-forwarded-host');
     if (forwardedHostHeader && forwardedHostHeader?.includes('dev.adeo.no')) {
-        res.redirect('https://supplerendestonad.intern.dev.adeo.no');
+        res.redirect('https://supplerendestonad.intern.dev.nav.no');
     } else if (forwardedHostHeader && forwardedHostHeader?.includes('nais.adeo.no')) {
         res.redirect('https://supplerendestonad.intern.nav.no');
     } else {
