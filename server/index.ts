07deba031d4658c50fd2bcf5d6bf441fff0446b2
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-04-28 09:35:55 +0200

    Fjern hotjar og forbedre oppsett

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,44 @@
-async function setupEnv(): Promise<void> {
-    if (process.env.NODE_ENV === 'development') {
-        console.log('Detected development environment, loading .env file');
-        const dotenv = await import('dotenv');
-        dotenv.config();
-    }
-}
+import compression from 'compression';
+import cors from 'cors';
+import express from 'express';
 
-setupEnv().then(async () => {
-    const startServer = await import('./start.js');
-    startServer.default();
+import setupAuth from './auth/index.js';
+import * as AuthUtils from './auth/utils.js';
+import * as Config from './config.js';
+import { httpLogger, logger } from './logger.js';
+import setupProxy from './proxy.js';
+import redirectMiddleware from './redirect.js';
+import routes from './routes.js';
+
+const app = express();
+logger.info(`Using log level: ${Config.server.logLevel}`);
+
+app.use(httpLogger);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('views', Config.server.frontendDir);
+app.set('view engine', '.html');
+
+app.use(redirectMiddleware);
+
+app.use(
+    cors({
+        origin: Config.server.host,
+        allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'X-Requested-With'],
+    }),
+);
+
+app.use(compression());
+const authClient = await AuthUtils.getOpenIdClient(Config.auth.discoverUrl);
+await setupAuth(app, authClient);
+
+app.use(setupProxy(authClient));
+
+app.use(await routes());
+
+const port = Config.server.port;
+app.listen(port, () => {
+    logger.info(`Listening on http://:${port}`);
 });
