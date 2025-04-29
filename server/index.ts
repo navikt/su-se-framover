954a7cd8f8333c6329c51618d0071a365a777228
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2025-04-29 12:56:34 +0200

    Fiks lokal kjøring, må se mer på det (#4053)
    * Fiks lokal kjøring, må se mer på det
    
    * Fiks lint mas
    
    * Fikser import av express start
    
    ---------
    
    Co-authored-by: Bjorn Golberg <bjorn.golberg@gmail.com>

--- server/index.ts
+++ server/index.ts
@@ -1,44 +1,14 @@
-import compression from 'compression';
-import cors from 'cors';
-import express from 'express';
+import 'dotenv/config';
+import expressStart from './express.js';
 
-import setupAuth from './auth/index.js';
-import * as AuthUtils from './auth/utils.js';
-import * as Config from './config.js';
-import { httpLogger, logger } from './logger.js';
-import setupProxy from './proxy.js';
-import redirectMiddleware from './redirect.js';
-import routes from './routes.js';
+async function setupEnv(): Promise<void> {
+    if (process.env.NODE_ENV === 'development') {
+        console.log('Detected development environment, loading .env file');
+        const dotenv = await import('dotenv');
+        dotenv.config();
+    }
+}
 
-const app = express();
-logger.info(`Using log level: ${Config.server.logLevel}`);
-
-app.use(httpLogger);
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.set('views', Config.server.frontendDir);
-app.set('view engine', '.html');
-
-app.use(redirectMiddleware);
-
-app.use(
-    cors({
-        origin: Config.server.host,
-        allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'X-Requested-With'],
-    }),
-);
-
-app.use(compression());
-const authClient = await AuthUtils.getOpenIdClient(Config.auth.discoverUrl);
-await setupAuth(app, authClient);
-
-app.use(setupProxy(authClient));
-
-app.use(await routes());
-
-const port = Config.server.port;
-app.listen(port, () => {
-    logger.info(`Listening on http://:${port}`);
+setupEnv().then(async () => {
+    await expressStart();
 });
