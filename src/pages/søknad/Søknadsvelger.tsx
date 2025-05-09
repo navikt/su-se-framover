80be5796d8e8d3c3eb3f23047a8045e9b7905ac0
Bjørn Golberg <bjorn.golberg.tools@gmail.com>
2025-05-09 15:22:39 +0200

    Tilgjengeligjør søknad alder i prod men ikke for veileder (#4081)
    * Tilgjengeligjør søknad alder i prod men ikke for veileder
    
    * Snur if
    
    ---------
    
    Co-authored-by: Bjorn Golberg <bjorn.golberg@gmail.com>

--- src/pages/søknad/Søknadsvelger.tsx
+++ src/pages/søknad/Søknadsvelger.tsx
@@ -5,6 +5,7 @@ import LinkAsButton from '~src/components/linkAsButton/LinkAsButton';
 import { useUserContext } from '~src/context/userContext';
 import { useI18n } from '~src/lib/i18n';
 import * as Routes from '~src/lib/routes';
+import { Rolle } from '~src/types/LoggedInUser.ts';
 
 import messages from './nb';
 import styles from './søknadsvelger.module.less';
@@ -14,7 +15,61 @@ const Søknadsvelger = () => {
     const { formatMessage } = useI18n({ messages });
     const isPapirsøknad = location.search.includes('papirsoknad');
 
-    if (user.isProd) {
+    if (user.roller.includes(Rolle.Saksbehandler)) {
+        return (
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    <GuidePanel>
+                        <BodyLong>
+                            {formatMessage('velg-grupper', {
+                                strong: (text) => <strong>{text}</strong>,
+                                br: () => <br />,
+                            })}
+                        </BodyLong>
+                    </GuidePanel>
+
+                    <Heading className={styles.tittel} level="1" size="xlarge" spacing>
+                        {formatMessage('velg-tittel')}
+                    </Heading>
+                    <Ingress spacing>{formatMessage('velg-undertittel')}</Ingress>
+
+                    <div className={styles.linkgruppe}>
+                        <Panel border>
+                            <Heading level="2" size="medium">
+                                {formatMessage('velg-alder-tittel')}
+                            </Heading>
+                            <Ingress>{formatMessage('alder-beskrivelse')}</Ingress>
+
+                            <LinkAsButton
+                                variant="secondary"
+                                href={Routes.soknadtema.createURL({
+                                    papirsøknad: isPapirsøknad,
+                                    soknadstema: Routes.URL_TEMA_ALDER,
+                                })}
+                            >
+                                {formatMessage('alder-lenke')}
+                            </LinkAsButton>
+                        </Panel>
+                        <Panel border>
+                            <Heading level="2" size="medium">
+                                {formatMessage('velg-ufør-tittel')}
+                            </Heading>
+                            <Ingress>{formatMessage('ufør-beskrivelse')}</Ingress>
+                            <LinkAsButton
+                                variant="secondary"
+                                href={Routes.soknadtema.createURL({
+                                    papirsøknad: isPapirsøknad,
+                                    soknadstema: Routes.URL_TEMA_UFØRE,
+                                })}
+                            >
+                                {formatMessage('ufør-lenke')}
+                            </LinkAsButton>
+                        </Panel>
+                    </div>
+                </div>
+            </div>
+        );
+    } else {
         return (
             <Navigate
                 replace
@@ -22,60 +77,6 @@ const Søknadsvelger = () => {
             />
         );
     }
-
-    return (
-        <div className={styles.container}>
-            <div className={styles.content}>
-                <GuidePanel>
-                    <BodyLong>
-                        {formatMessage('velg-grupper', {
-                            strong: (text) => <strong>{text}</strong>,
-                            br: () => <br />,
-                        })}
-                    </BodyLong>
-                </GuidePanel>
-
-                <Heading className={styles.tittel} level="1" size="xlarge" spacing>
-                    {formatMessage('velg-tittel')}
-                </Heading>
-                <Ingress spacing>{formatMessage('velg-undertittel')}</Ingress>
-
-                <div className={styles.linkgruppe}>
-                    <Panel border>
-                        <Heading level="2" size="medium">
-                            {formatMessage('velg-alder-tittel')}
-                        </Heading>
-                        <Ingress>{formatMessage('alder-beskrivelse')}</Ingress>
-
-                        <LinkAsButton
-                            variant="secondary"
-                            href={Routes.soknadtema.createURL({
-                                papirsøknad: isPapirsøknad,
-                                soknadstema: Routes.URL_TEMA_ALDER,
-                            })}
-                        >
-                            {formatMessage('alder-lenke')}
-                        </LinkAsButton>
-                    </Panel>
-                    <Panel border>
-                        <Heading level="2" size="medium">
-                            {formatMessage('velg-ufør-tittel')}
-                        </Heading>
-                        <Ingress>{formatMessage('ufør-beskrivelse')}</Ingress>
-                        <LinkAsButton
-                            variant="secondary"
-                            href={Routes.soknadtema.createURL({
-                                papirsøknad: isPapirsøknad,
-                                soknadstema: Routes.URL_TEMA_UFØRE,
-                            })}
-                        >
-                            {formatMessage('ufør-lenke')}
-                        </LinkAsButton>
-                    </Panel>
-                </div>
-            </div>
-        </div>
-    );
 };
 
 export default Søknadsvelger;
