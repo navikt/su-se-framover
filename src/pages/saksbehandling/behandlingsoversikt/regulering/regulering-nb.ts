c2602cb0490bcab99030d1cab821da9117eaaeac
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-06-06 15:24:14 +0200

    Typesikker og knytning mot tekstreferansene fra enumen for årsakene til manuell regulering

--- src/pages/saksbehandling/behandlingsoversikt/regulering/regulering-nb.ts
+++ src/pages/saksbehandling/behandlingsoversikt/regulering/regulering-nb.ts
@@ -1,3 +1,30 @@
+import { ÅrsakTilManuellReguleringKategori } from '~src/types/Regulering.ts';
+
+const manuelleÅrsakerForRegulering: { [key in ÅrsakTilManuellReguleringKategori]: string } = {
+    [ÅrsakTilManuellReguleringKategori.FradragMåHåndteresManuelt]:
+        'Fradraget må håndteres manuelt av historiske grunner',
+    [ÅrsakTilManuellReguleringKategori.UtbetalingFeilet]: 'Utbetaling feilet for behandlingen',
+    [ÅrsakTilManuellReguleringKategori.BrukerManglerSupplement]: 'Bruker har ikke data fra ekstern kilde for fradraget',
+    [ÅrsakTilManuellReguleringKategori.SupplementInneholderIkkeFradraget]:
+        'Data fra ekstern kilde inneholder ikke fradraget',
+    [ÅrsakTilManuellReguleringKategori.FinnesFlerePerioderAvFradrag]: 'Funnet flere perioder av samme fradrag',
+    [ÅrsakTilManuellReguleringKategori.FradragErUtenlandsinntekt]: 'Fradrag er markert som utenlandsk',
+    [ÅrsakTilManuellReguleringKategori.SupplementHarFlereVedtaksperioderForFradrag]:
+        'Data fra ekstern kilde inneholdt flere vedtaksperioder for fradrag som kan reguleresf',
+    [ÅrsakTilManuellReguleringKategori.DifferanseFørRegulering]:
+        'Differanse i beløp mellom eksterne kilde, og beløpet som er i fradraget før regulering',
+    [ÅrsakTilManuellReguleringKategori.DifferanseEtterRegulering]:
+        'Differanse i beløp mellom eksterne kilde, og beløpet som er i fradraget etter regulering',
+    [ÅrsakTilManuellReguleringKategori.YtelseErMidlertidigStanset]: 'Ytelsen er midlertidig stanset',
+    [ÅrsakTilManuellReguleringKategori.ForventetInntektErStørreEnn0]: 'Forventet inntekt må justeres',
+    [ÅrsakTilManuellReguleringKategori.AutomatiskSendingTilUtbetalingFeilet]:
+        'Automatisk behandling av reguleringen feilet fordi utbetaling feilet',
+    [ÅrsakTilManuellReguleringKategori.VedtakstidslinjeErIkkeSammenhengende]: 'Vedtakstidslinjen inneholder hull',
+    [ÅrsakTilManuellReguleringKategori.DelvisOpphør]: 'Saken er delvis opphørt',
+    [ÅrsakTilManuellReguleringKategori.FantIkkeVedtakForApril]: 'Fant ikke eksterne vedtaket for april',
+    [ÅrsakTilManuellReguleringKategori.MerEnn1Eps]: 'Data fra ekstern kilde inneholder flere EPS',
+};
+
 export default {
     resultat: '{antallManuelle} saker trenger manuell behandling. ',
     'resultat.startManuell': 'Start behandling av saker til manuell G-regulering',
@@ -9,22 +36,5 @@ export default {
     'tabell.ekstraInformasjon': 'Ekstra informasjon',
     'tabell.årsakTilManuellRegulering': 'Årsak til manuell G-regulering',
     'tabell.lenke.knapp': 'Se sak',
-    FradragMåHåndteresManuelt: 'Fradraget må håndteres manuelt av historiske grunner',
-    UtbetalingFeilet: 'Utbetaling feilet for behandlingen',
-    BrukerManglerSupplement: 'Bruker har ikke data fra ekstern kilde for fradraget',
-    SupplementInneholderIkkeFradraget: 'Data fra ekstern kilde inneholder ikke fradraget',
-    FinnesFlerePerioderAvFradrag: 'Funnet flere perioder av samme fradrag',
-    FradragErUtenlandsinntekt: 'Fradrag er markert som utenlandsk',
-    SupplementHarFlereVedtaksperioderForFradrag:
-        'Data fra ekstern kilde inneholdt flere vedtaksperioder for fradrag som kan reguleresf',
-    DifferanseFørRegulering: 'Differanse i beløp mellom eksterne kilde, og beløpet som er i fradraget før regulering',
-    DifferanseEtterRegulering:
-        'Differanse i beløp mellom eksterne kilde, og beløpet som er i fradraget etter regulering',
-    YtelseErMidlertidigStanset: 'Ytelsen er midlertidig stanset',
-    ForventetInntektErStørreEnn0: 'Forventet inntekt må justeres',
-    AutomatiskSendingTilUtbetalingFeilet: 'Automatisk behandling av reguleringen feilet fordi utbetaling feilet',
-    VedtakstidslinjeErIkkeSammenhengende: 'Vedtakstidslinjen inneholder hull',
-    DelvisOpphør: 'Saken er delvis opphørt',
-    FantIkkeVedtakForApril: 'Fant ikke eksterne vedtaket for april',
-    MerEnn1Eps: 'Data fra ekstern kilde inneholder flere EPS',
+    ...manuelleÅrsakerForRegulering,
 };
