ec0a93a5c4d06bbbb6964dd42d8d16b908f51095
sevdeoguz <sevde.oguz@nav.no>
2025-09-24 12:24:40 +0200

    Endret standardtekst alderspensjon (#4260)

--- src/components/forms/vilkårOgGrunnlagForms/alderspensjon/alderspensjon-nb.ts
+++ src/components/forms/vilkårOgGrunnlagForms/alderspensjon/alderspensjon-nb.ts
@@ -3,7 +3,7 @@ import sharedMessages from '~src/pages/saksbehandling/søknadsbehandling/sharedI
 export default {
     ...sharedMessages,
     'page.tittel': 'Alder',
-    'label.folketrygd': 'Har søker søkt om alderspensjon og fått svar på søknaden?',
+    'label.folketrygd': 'Har søker søkt om 100 % alderspensjon og fått svar på søknaden?',
     'label.andreNorske': 'Har søker søkt om andre norske pensjonsordninger og fått svar på søknaden?',
     'label.utenlandske': 'Har søker søkt om pensjon i utlandet og fått svar på søknaden?',
 };
