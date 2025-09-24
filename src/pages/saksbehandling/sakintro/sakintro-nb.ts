5710dd911573e3f434a22bf33a999fbccbd4665b
sevdeoguz <sevde.oguz@nav.no>
2025-09-24 09:37:09 +0200

    small fix

--- src/pages/saksbehandling/sakintro/sakintro-nb.ts
+++ src/pages/saksbehandling/sakintro/sakintro-nb.ts
@@ -10,7 +10,6 @@ const utbetalingsTypeTekstMapper: { [key in Utbetalingstype]: string } = {
 
 export default {
     'attestering.attester': 'Attester',
-    'dataCell.info.knapp.attestering.modal.tittel': 'Er du sikker på at du ønsker å returnere saken?',
 
     'saksoversikt.tittel': 'Saksnummer',
 
@@ -24,7 +23,6 @@ export default {
     'link.utenlandsopphold': 'Utenlandsopphold',
     'link.brev': 'Opprett nytt fritekstbrev',
     'link.retur': 'Returner behandling',
-
 
     'bekreft.tittel': 'Bekreft',
 
@@ -96,7 +94,6 @@ export default {
     'datacell.info.knapp.regulering.start': 'Start regulering',
     'dataCell.info.knapp.regulering.modal.tittel': 'Er du sikker på at du ønsker å lukke reguleringen?',
     'dataCell.info.knapp.attestering.modal.tittel': 'Er du sikker på at du ønsker å returnere behandlingen?',
-
 
     'dataCell.seOppsummering': 'Se oppsummering',
     'dataCell.startNyBehandling': 'Start ny behandling',
