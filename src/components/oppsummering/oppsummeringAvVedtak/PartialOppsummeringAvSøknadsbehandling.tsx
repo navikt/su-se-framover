8775a6aaba169dfe4ed7fb303b7d615618c3767c
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-10-10 11:43:59 +0200

    Splitt ut oppsummering av vedtak

---
+++ src/components/oppsummering/oppsummeringAvVedtak/PartialOppsummeringAvSøknadsbehandling.tsx
@@ -0,0 +1,56 @@
+import messages from '~src/components/oppsummering/oppsummeringAvVedtak/OppsummeringAvVedtak-nb';
+import styles from '~src/components/oppsummering/oppsummeringAvVedtak/OppsummeringAvVedtak.module.less';
+import { OppsummeringPar } from '~src/components/oppsummering/oppsummeringpar/OppsummeringPar';
+import SidestiltOppsummeringAvVilkårOgGrunnlag from '~src/components/oppsummering/sidestiltOppsummeringAvVilkårOgGrunnlag/SidestiltOppsummeringAvVilkårOgGrunnlag';
+import { useI18n } from '~src/lib/i18n';
+import { opprettOmgjøringÅrsakTekstMapper } from '~src/pages/saksbehandling/sakintro/Vedtakstabell/omgjøringsmodal-nb';
+import { Sakstype } from '~src/types/Sak';
+import { Søknadsbehandling } from '~src/types/Søknadsbehandling';
+import { formatDateTime } from '~src/utils/date/dateUtils';
+import { søknadMottatt } from '~src/utils/søknad/søknadUtils';
+import { splitStatusOgResultatFraSøkandsbehandling } from '~src/utils/SøknadsbehandlingUtils';
+
+export const PartialOppsummeringAvSøknadsbehandling = (props: { s: Søknadsbehandling; sakstype: Sakstype }) => {
+    const { formatMessage } = useI18n({ messages: { ...messages, ...opprettOmgjøringÅrsakTekstMapper } });
+
+    return (
+        <div>
+            <div className={styles.vedtakOgBehandlingInfoContainer}>
+                <OppsummeringPar
+                    label={formatMessage('behandling.resultat')}
+                    verdi={splitStatusOgResultatFraSøkandsbehandling(props.s).resultat}
+                    retning={'vertikal'}
+                />
+                <OppsummeringPar
+                    label={formatMessage('søknadsbehandling.startet')}
+                    verdi={formatDateTime(props.s.opprettet)}
+                    retning={'vertikal'}
+                />
+                <OppsummeringPar
+                    label={formatMessage('søknadsbehandling.søknadsdato')}
+                    verdi={søknadMottatt(props.s.søknad)}
+                    retning={'vertikal'}
+                />
+                {props.s.omgjøringsårsak && props.s.omgjøringsgrunn && (
+                    <>
+                        <OppsummeringPar
+                            label={formatMessage('label.årsak')}
+                            verdi={formatMessage(props.s.omgjøringsårsak)}
+                            retning={'vertikal'}
+                        />
+                        <OppsummeringPar
+                            label={formatMessage('label.omgjøring')}
+                            verdi={formatMessage(props.s.omgjøringsgrunn)}
+                            retning={'vertikal'}
+                        />
+                    </>
+                )}
+            </div>
+            <SidestiltOppsummeringAvVilkårOgGrunnlag
+                grunnlagsdataOgVilkårsvurderinger={props.s.grunnlagsdataOgVilkårsvurderinger}
+                visesSidestiltMed={props.s.søknad.søknadInnhold}
+                sakstype={props.sakstype}
+            />
+        </div>
+    );
+};
