bcc71f28e6d4c9fd91bf88319589a2ec6eb7c882
sevdeoguz <sevde.oguz@nav.no>
2025-11-03 12:56:31 +0100

    fjernet consoe-logs og imports

--- src/pages/saksbehandling/avsluttBehandling/lukkSøknad/Avvist.tsx
+++ src/pages/saksbehandling/avsluttBehandling/lukkSøknad/Avvist.tsx
@@ -1,14 +1,11 @@
 import { FieldError } from 'react-hook-form';
 
 import * as søknadApi from '~src/api/søknadApi.ts';
-//import * as pdfApi from "~src/api/pdfApi.ts";
 import { BrevInput } from '~src/components/inputs/brevInput/BrevInput.tsx';
 import { AvvistBrevtyper } from '~src/pages/saksbehandling/avsluttBehandling/lukkSøknad/lukkSøknadUtils.ts';
 import { LukkSøknadBegrunnelse } from '~src/types/Søknad.ts';
 
 import styles from './lukkSøknad.module.less';
-
-//import {LukkSøknadBegrunnelse} from "~src/types/Søknad.ts";
 
 interface AvvistProps {
     søknadId: string;
@@ -18,8 +15,6 @@ interface AvvistProps {
 }
 
 const Avvist = (props: AvvistProps) => {
-    console.log(props.fritekstValue);
-
     return (
         <div className={styles.container}>
             <BrevInput
