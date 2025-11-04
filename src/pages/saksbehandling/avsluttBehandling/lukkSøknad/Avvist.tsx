ba5a2bfd9637e1bc77849e328572cb26d61a4f78
sevdeoguz <sevde.oguz@nav.no>
2025-11-04 09:57:06 +0100

    Lagt til valg avslag-innvilget stønad (#4306)
    * Lagt til valg avslag-innvilget stønad
    
    * endre til vedtaksbrev
    
    * fjernet consoe-logs og imports
    
    * fix imports
    
    * fjerner nullable
    
    * Fiks nullable

--- src/pages/saksbehandling/avsluttBehandling/lukkSøknad/Avvist.tsx
+++ src/pages/saksbehandling/avsluttBehandling/lukkSøknad/Avvist.tsx
@@ -1,113 +1,39 @@
-import * as RemoteData from '@devexperts/remote-data-ts';
-import { Button, Loader, RadioGroup, Radio, Textarea } from '@navikt/ds-react';
-import { FieldErrorsImpl } from 'react-hook-form';
+import { FieldError } from 'react-hook-form';
 
-import * as søknadApi from '~src/api/søknadApi';
-import ApiErrorAlert from '~src/components/apiErrorAlert/ApiErrorAlert';
-import { BooleanRadioGroup } from '~src/components/formElements/FormElements';
-import { useBrevForhåndsvisning } from '~src/lib/hooks';
-import { useI18n } from '~src/lib/i18n';
-import { Nullable } from '~src/lib/types';
-import { LukkSøknadBegrunnelse } from '~src/types/Søknad';
+import * as søknadApi from '~src/api/søknadApi.ts';
+import { BrevInput } from '~src/components/inputs/brevInput/BrevInput.tsx';
+import { AvvistBrevtyper } from '~src/pages/saksbehandling/avsluttBehandling/lukkSøknad/lukkSøknadUtils.ts';
+import { LukkSøknadBegrunnelse } from '~src/types/Søknad.ts';
 
-import nb from './lukkSøknad-nb';
 import styles from './lukkSøknad.module.less';
-import { AvvistBrevtyper } from './lukkSøknadUtils';
-
-export interface AvvistFormData {
-    skalSendesBrev: Nullable<boolean>;
-    typeBrev: Nullable<AvvistBrevtyper>;
-    fritekst: Nullable<string>;
-}
 
 interface AvvistProps {
     søknadId: string;
-    avvistFormData: AvvistFormData;
-    feilmeldinger?: FieldErrorsImpl<AvvistFormData>;
-    onValueChange: (value: AvvistFormData) => void;
-    onRequestValidate: (onSuccess: () => void) => void;
+    fritekstValue: string;
+    fritekstError?: FieldError;
+    onFritekstChange: (value: string) => void;
 }
 
 const Avvist = (props: AvvistProps) => {
-    const { formatMessage } = useI18n({ messages: nb });
-
-    const [brevStatus, hentBrev] = useBrevForhåndsvisning(søknadApi.hentLukketSøknadsBrevutkast);
-
-    const handleHentBrevClick = () => {
-        props.onRequestValidate(() => {
-            hentBrev({
-                søknadId: props.søknadId,
-                body: {
-                    type: LukkSøknadBegrunnelse.Avvist,
-                    brevConfig: props.avvistFormData.typeBrev
-                        ? {
-                              brevtype: props.avvistFormData.typeBrev,
-                              fritekst: props.avvistFormData.fritekst,
-                          }
-                        : null,
-                },
-            });
-        });
-    };
-
     return (
-        <div className={styles.avvistContainer}>
-            <div className={styles.radioContainer}>
-                <BooleanRadioGroup
-                    name="skalSendesBrev"
-                    legend={formatMessage('avvist.skalSendesBrevTilSøker')}
-                    error={props.feilmeldinger?.skalSendesBrev?.message}
-                    value={props.avvistFormData.skalSendesBrev}
-                    onChange={(val) => {
-                        props.onValueChange({ skalSendesBrev: val, typeBrev: null, fritekst: null });
-                    }}
-                />
-            </div>
-            {props.avvistFormData.skalSendesBrev && (
-                <div className={styles.radioContainer}>
-                    <RadioGroup
-                        name="typeBrev"
-                        legend={formatMessage('avvist.typeBrev')}
-                        value={props.avvistFormData.typeBrev?.toString()}
-                        onChange={(val) => {
-                            props.onValueChange({
-                                ...props.avvistFormData,
-                                typeBrev: val as AvvistBrevtyper,
-                            });
-                        }}
-                        error={props.feilmeldinger?.typeBrev?.message}
-                    >
-                        <Radio id="typeBrev" value={AvvistBrevtyper.Vedtaksbrev}>
-                            {formatMessage('avvist.brevType.vedtaksbrev')}
-                        </Radio>
-                        <Radio value={AvvistBrevtyper.Fritekstsbrev}>
-                            {formatMessage('avvist.brevType.fritekstbrev')}
-                        </Radio>
-                    </RadioGroup>
-                </div>
-            )}
-            {props.avvistFormData.typeBrev && (
-                <div className={styles.textAreaContainer}>
-                    <Textarea
-                        label={formatMessage('avvist.fritekst')}
-                        name="fritekst"
-                        value={props.avvistFormData.fritekst ?? ''}
-                        error={props.feilmeldinger?.fritekst?.message}
-                        onChange={(e) => props.onValueChange({ ...props.avvistFormData, fritekst: e.target.value })}
-                    />
-                </div>
-            )}
-            {props.avvistFormData.skalSendesBrev !== null && (
-                <div>
-                    {props.avvistFormData.skalSendesBrev && (
-                        <Button variant="secondary" type="button" onClick={handleHentBrevClick}>
-                            {formatMessage('knapp.seBrev')}
-                            {RemoteData.isPending(brevStatus) && <Loader />}
-                        </Button>
-                    )}
-                </div>
-            )}
-            <div>{RemoteData.isFailure(brevStatus) && <ApiErrorAlert error={brevStatus.error} />}</div>
+        <div className={styles.container}>
+            <BrevInput
+                tekst={props.fritekstValue}
+                onVisBrevClick={() =>
+                    søknadApi.hentLukketSøknadsBrevutkast({
+                        søknadId: props.søknadId,
+                        body: {
+                            type: LukkSøknadBegrunnelse.Avvist,
+                            brevConfig: {
+                                brevtype: AvvistBrevtyper.Vedtaksbrev,
+                                fritekst: props.fritekstValue,
+                            },
+                        },
+                    })
+                }
+                onChange={(e) => props.onFritekstChange(e.target.value)}
+                feil={props.fritekstError}
+            />
         </div>
     );
 };
