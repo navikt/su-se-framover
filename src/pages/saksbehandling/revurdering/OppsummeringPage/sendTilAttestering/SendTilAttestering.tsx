ab80c4feb124ba9516e6879d068faf47aaebeda3
sevdeoguz <sevde.oguz@nav.no>
2026-01-20 18:16:58 +0100

    fjerner fritekst

--- src/pages/saksbehandling/revurdering/OppsummeringPage/sendTilAttestering/SendTilAttestering.tsx
+++ src/pages/saksbehandling/revurdering/OppsummeringPage/sendTilAttestering/SendTilAttestering.tsx
@@ -223,8 +223,6 @@ const SendTilAttestering = (props: {
                                                 seBrev({
                                                     sakId: props.sakId,
                                                     revurderingId: props.revurdering.id,
-                                                    fritekst: form.getValues().fritekst ?? '',
-                                                    underAttestering: false,
                                                 }),
                                             status: seBrevStatus,
                                         }}
