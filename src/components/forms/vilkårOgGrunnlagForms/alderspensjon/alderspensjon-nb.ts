fbefd5c0c252e5ea243fc09804ee982881d852df
sevdeoguz <sevde.oguz@nav.no>
2026-01-23 15:14:23 +0100

    Hjelpetekst alderpensjon (#4383)
    * lag til hjelpetekst for alderpensjon
    
    * lag til info icon med tekst

--- src/components/forms/vilkårOgGrunnlagForms/alderspensjon/alderspensjon-nb.ts
+++ src/components/forms/vilkårOgGrunnlagForms/alderspensjon/alderspensjon-nb.ts
@@ -6,4 +6,7 @@ export default {
     'label.folketrygd': 'Har søker søkt om 100 % alderspensjon og fått svar på søknaden?',
     'label.andreNorske': 'Har søker søkt om andre norske pensjonsordninger og fått svar på søknaden?',
     'label.utenlandske': 'Har søker søkt om pensjon i utlandet og fått svar på søknaden?',
+
+    'alderspensjon.hjelpetekst':
+        'For å ha rett til supplerende stønad må du ha søkt om full alderspensjon. Lov om supplerende stønad setter krav til at den som mottar supplerende stønad fullt ut utnytter retten sin til pensjon. Dette er fordi stønaden skal supplere det du mangler for å komme opp til en inntekt tilsvarende garantipensjonen.',
 };
