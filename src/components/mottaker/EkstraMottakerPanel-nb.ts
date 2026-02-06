9c7a1b9b6703e6fe4213ec02f2a864e32dff4f8b
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2026-02-06 17:11:32 +0100

    Vis ekstra mottaker i attesteringsbildet fix

---
+++ src/components/mottaker/EkstraMottakerPanel-nb.ts
@@ -0,0 +1,8 @@
+export default {
+    'ekstramottaker.tittel': 'Ekstra mottaker',
+    'ekstramottaker.navn': 'Navn',
+    'ekstramottaker.fnr': 'Fødselsnummer',
+    'ekstramottaker.orgnr': 'Organisasjonsnummer',
+    'ekstramottaker.adresse': 'Adresse',
+    'feilmelding.kanIkkeHenteMottaker': 'Kan ikke hente mottaker',
+};
