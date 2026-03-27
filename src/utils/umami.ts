ff2419f47a725721e2d27cf929ce0230b0c71832
sevdeoguz <sevde.oguz@nav.no>
2026-03-27 14:54:28 +0100

    fjern det gamle

--- src/utils/umami.ts
+++ src/utils/umami.ts
@@ -4,6 +4,5 @@ type UmamiData = Record<string, UmamiValue>;
 export const loggUmamiEvent = (eventName: string, data: UmamiData) => {
     if (typeof window === 'undefined') return;
 
-    const umamiWindow = window as unknown as { umami?: { track: (eventName: string, data: object) => void } };
-    umamiWindow.umami?.track(eventName, data);
+    window.umami?.track(eventName, data);
 };
