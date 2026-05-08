af06977cb355d157245c21fd08fe81899e706467
sevdeoguz <sevde.oguz@nav.no>
2026-05-08 16:08:50 +0200

    legger til vedtaksbrev oppsummering (#4472)

--- src/pages/saksbehandling/attestering/attesterSøknadsbehandling/attesterSøknadsbehandling-nb.ts
+++ src/pages/saksbehandling/attestering/attesterSøknadsbehandling/attesterSøknadsbehandling-nb.ts
@@ -1,4 +1,13 @@
+import { Valg } from '~src/types/Søknadsbehandling.ts';
+
+const brevvalgTekstMapper: { [key in Valg]: string } = {
+    SEND: 'Ja',
+    IKKE_SEND: 'Nei',
+    IKKE_VALGT: 'Ikke valgt',
+};
+
 export default {
+    ...brevvalgTekstMapper,
     'feil.ikkeKlarForAttestering': 'Behandlingen er ikke klar for attestering',
 
     'status.sendtTilbake': 'Saken er sendt tilbake til vurdering',
@@ -6,4 +15,8 @@ export default {
     'status.underkjent': 'Vedtaket er underkjent',
 
     'lenke.saksoversikt': 'Til saksoversikt',
+
+    'oppsummeringspanel.Vedtaksbrev': 'Vedtaksbrev',
+
+    'brevvalg.skalSendeBrev': 'Skal det sendes vedtaksbrev?',
 };
