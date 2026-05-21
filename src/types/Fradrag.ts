c6dd7f7a9cf0f59e509d264c4fd6900ce16919b7
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2026-05-21 14:16:25 +0200

    Legg til omsorgslønn

--- src/types/Fradrag.ts
+++ src/types/Fradrag.ts
@@ -71,6 +71,7 @@ export enum VelgbareFradragskategorier {
     Kapitalinntekt = 'Kapitalinntekt',
     Fosterhjemsgodtgjørelse = 'Fosterhjemsgodtgjørelse',
     Annet = 'Annet',
+    Omsorgslønn = 'Omsorgslønn',
     Omstillingsstønad = 'Omstillingsstønad',
     Overgangsstønad = 'Overgangsstønad',
 }
@@ -104,6 +105,7 @@ type tagVarianter =
 export const fradragTilLabelTag: Record<Fradragskategori, tagVarianter> = {
     Alderspensjon: 'info',
     Annet: 'info-filled',
+    Omsorgslønn: 'info',
     Omstillingsstønad: 'info',
     Overgangsstønad: 'info',
     Arbeidsavklaringspenger: 'error-filled',
@@ -163,6 +165,7 @@ export const måReguleresManuelt = (fradrag: Fradragskategori): boolean => {
         case IkkeVelgbareFradragskategorier.NAVytelserTilLivsopphold:
             return true;
         case VelgbareFradragskategorier.Annet:
+        case VelgbareFradragskategorier.Omsorgslønn:
         case VelgbareFradragskategorier.Omstillingsstønad:
         case VelgbareFradragskategorier.Overgangsstønad:
         case VelgbareFradragskategorier.Sykepenger:
