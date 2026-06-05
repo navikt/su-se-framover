679d79352c3c8683d64f9a6517ff39f521797629
Bjorn Golberg <bjorn.golberg@gmail.com>
2026-06-05 10:38:28 +0200

    Legger tilbake tekst

--- src/pages/saksbehandling/behandlingsoversikt/regulering/regulering-nb.ts
+++ src/pages/saksbehandling/behandlingsoversikt/regulering/regulering-nb.ts
@@ -2,6 +2,18 @@ import {
     ikkeVelgbareFradragskategoriMessages,
     velgbareFradragskategoriMessages,
 } from '~src/components/forms/vilkårOgGrunnlagForms/VilkårOgGrunnlagForms-nb.ts';
+import { ÅrsakTilManuellReguleringKategori } from '~src/types/Regulering.ts';
+
+const manuelleÅrsakerForRegulering: { [key in ÅrsakTilManuellReguleringKategori]: string } = {
+    [ÅrsakTilManuellReguleringKategori.ManglerRegulertBeløpForFradrag]: 'Mangler regulert beløp for fradrag',
+    [ÅrsakTilManuellReguleringKategori.ManglerIeuFraPesys]: 'Mangler IEU fra Pesys',
+    [ÅrsakTilManuellReguleringKategori.YtelseErMidlertidigStanset]: 'Ytelsen er midlertidig stanset',
+    [ÅrsakTilManuellReguleringKategori.EtAutomatiskFradragHarFremtidigPeriode]:
+        'Et automatisk fradrag har fremtidig periode',
+    [ÅrsakTilManuellReguleringKategori.UgyldigePerioderForAutomatiskRegulering]:
+        'Ugyldige perioder for automatisk regulering',
+    [ÅrsakTilManuellReguleringKategori.AapManglerGyldigPeriode]: 'AAP mangler gyldig periode',
+};
 
 export default {
     resultat: '{antallManuelle} saker trenger manuell behandling. ',
@@ -14,6 +26,7 @@ export default {
     'tabell.ekstraInformasjon': 'Ekstra informasjon',
     'tabell.årsakTilManuellRegulering': 'Årsak til manuell G-regulering',
     'tabell.lenke.knapp': 'Se sak',
+    ...manuelleÅrsakerForRegulering,
     ...velgbareFradragskategoriMessages,
     ...ikkeVelgbareFradragskategoriMessages,
 };
