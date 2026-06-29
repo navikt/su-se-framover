8dd22ee20438405e8bb12b4ce37233ea45b8911c
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2026-06-29 15:37:20 +0200

    Kopier over

--- src/pages/saksbehandling/mottaker/Dødsbo.tsx
+++ src/pages/saksbehandling/mottaker/Dødsbo.tsx
@@ -1,5 +1,5 @@
 import * as RemoteData from '@devexperts/remote-data-ts';
-import { BodyShort, Box, Heading, VStack } from '@navikt/ds-react';
+import { BodyShort, Box, Button, Heading, VStack } from '@navikt/ds-react';
 import { useLocation, useOutletContext } from 'react-router-dom';
 import LinkAsButton from '~src/components/linkAsButton/LinkAsButton.tsx';
 import { OppsummeringPar } from '~src/components/oppsummering/oppsummeringpar/OppsummeringPar';
@@ -9,7 +9,29 @@ import { useAppSelector } from '~src/redux/Store.ts';
 import { KontaktInfoDødsbo, Kontaktinformasjon } from '~src/types/Person';
 import styles from './Mottaker.module.less';
 
-const DødsboPage = () => {
+interface DødsboPageProps {
+    onVelgKontakt?: (kontakt: Kontaktinformasjon, adresse: KontaktInfoDødsbo) => void;
+}
+
+const hentKontaktForDødsbo = (
+    kontaktinfo: KontaktInfoDødsbo,
+): { label: string; kontakt: Kontaktinformasjon } | null => {
+    if (kontaktinfo.kontaktPerson) {
+        return { label: 'Kontaktperson', kontakt: kontaktinfo.kontaktPerson };
+    }
+
+    if (kontaktinfo.kontaktAdvokat) {
+        return { label: 'Kontaktadvokat', kontakt: kontaktinfo.kontaktAdvokat };
+    }
+
+    if (kontaktinfo.kontaktOrganisasjon) {
+        return { label: 'Kontaktorganisasjon', kontakt: kontaktinfo.kontaktOrganisasjon };
+    }
+
+    return null;
+};
+
+const DødsboPage = ({ onVelgKontakt }: DødsboPageProps) => {
     const context = useOutletContext<SaksoversiktContext>();
     const { pathname } = useLocation();
     const { søker } = useAppSelector((s) => ({ søker: s.personopplysninger.søker }));
@@ -33,53 +55,47 @@ const DødsboPage = () => {
 
                         {dødsbo.length > 0 ? (
                             <VStack gap="6" className={styles.box}>
-                                {dødsbo.map((kontaktinfo: KontaktInfoDødsbo, index: number) => (
-                                    <VStack key={index} gap="4">
-                                        {kontaktinfo.kontaktPerson && (
-                                            <KontaktinformasjonVisning
-                                                label="Kontaktperson"
-                                                kontakt={kontaktinfo.kontaktPerson}
+                                {dødsbo.map((kontaktinfo: KontaktInfoDødsbo, index: number) => {
+                                    const kontaktForDødsbo = hentKontaktForDødsbo(kontaktinfo);
+
+                                    return (
+                                        <VStack key={index} gap="4">
+                                            {kontaktForDødsbo && (
+                                                <KontaktinformasjonVisning
+                                                    label={kontaktForDødsbo.label}
+                                                    kontakt={kontaktForDødsbo.kontakt}
+                                                    onVelgKontakt={onVelgKontakt}
+                                                    adresse={kontaktinfo}
+                                                />
+                                            )}
+                                            <OppsummeringPar
+                                                label="Adresselinje 1"
+                                                retning={'vertikal'}
+                                                verdi={kontaktinfo.adresselinje1}
                                             />
-                                        )}
-                                        {kontaktinfo.kontaktAdvokat && (
-                                            <KontaktinformasjonVisning
-                                                label="Kontaktadvokat"
-                                                kontakt={kontaktinfo.kontaktAdvokat}
+                                            <OppsummeringPar
+                                                label="Adresselinje 2"
+                                                retning={'vertikal'}
+                                                verdi={kontaktinfo.adresselinje2}
                                             />
-                                        )}
-                                        {kontaktinfo.kontaktOrganisasjon && (
-                                            <KontaktinformasjonVisning
-                                                label="Kontaktorganisasjon"
-                                                kontakt={kontaktinfo.kontaktOrganisasjon}
+                                            <OppsummeringPar
+                                                label="Postnummer"
+                                                retning={'vertikal'}
+                                                verdi={kontaktinfo.postnummer}
                                             />
-                                        )}
-                                        <OppsummeringPar
-                                            label="Adresselinje 1"
-                                            retning={'vertikal'}
-                                            verdi={kontaktinfo.adresselinje1}
-                                        />
-                                        <OppsummeringPar
-                                            label="Adresselinje 2"
-                                            retning={'vertikal'}
-                                            verdi={kontaktinfo.adresselinje2}
-                                        />
-                                        <OppsummeringPar
-                                            label="Postnummer"
-                                            retning={'vertikal'}
-                                            verdi={kontaktinfo.postnummer}
-                                        />
-                                        <OppsummeringPar
-                                            label="Poststedsnavn"
-                                            retning={'vertikal'}
-                                            verdi={kontaktinfo.poststedsnavn}
-                                        />
-                                        <OppsummeringPar
-                                            label="Landkode"
-                                            retning={'vertikal'}
-                                            verdi={kontaktinfo.landkode}
-                                        />
-                                    </VStack>
-                                ))}
+                                            <OppsummeringPar
+                                                label="Poststedsnavn"
+                                                retning={'vertikal'}
+                                                verdi={kontaktinfo.poststedsnavn}
+                                            />
+                                            <OppsummeringPar
+                                                label="Landkode"
+                                                retning={'vertikal'}
+                                                verdi={kontaktinfo.landkode}
+                                            />
+                                        </VStack>
+                                    );
+                                })}
                             </VStack>
                         ) : (
                             <BodyShort>Ingen dødsbo</BodyShort>
@@ -127,7 +143,17 @@ const DødsboPage = () => {
     );
 };
 
-const KontaktinformasjonVisning = ({ label, kontakt }: { label: string; kontakt: Kontaktinformasjon }) => (
+const KontaktinformasjonVisning = ({
+    label,
+    kontakt,
+    adresse,
+    onVelgKontakt,
+}: {
+    label: string;
+    kontakt: Kontaktinformasjon;
+    adresse: KontaktInfoDødsbo;
+    onVelgKontakt?: (kontakt: Kontaktinformasjon, adresse: KontaktInfoDødsbo) => void;
+}) => (
     <VStack gap="2">
         <Heading level="3" size="small">
             {label}
@@ -143,6 +169,11 @@ const KontaktinformasjonVisning = ({ label, kontakt }: { label: string; kontakt:
         )}
         {kontakt.organisasjonsnummer && (
             <OppsummeringPar label="Organisasjonsnummer" retning={'vertikal'} verdi={kontakt.organisasjonsnummer} />
+        )}
+        {onVelgKontakt && (
+            <Button type="button" variant="secondary" size="small" onClick={() => onVelgKontakt(kontakt, adresse)}>
+                Bruk som mottaker
+            </Button>
         )}
     </VStack>
 );
