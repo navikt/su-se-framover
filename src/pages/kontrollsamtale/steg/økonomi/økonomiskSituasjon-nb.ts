11c9be5a6e4863c60d2670ee3244e9886c11c233
sevdeoguz <sevde.oguz@nav.no>
2026-07-08 09:52:06 +0200

    tekst endringer

--- src/pages/kontrollsamtale/steg/økonomi/økonomiskSituasjon-nb.ts
+++ src/pages/kontrollsamtale/steg/økonomi/økonomiskSituasjon-nb.ts
@@ -1,6 +1,6 @@
 export default {
     'økonomiskSituasjon.label':
-        'Er det endringer i den økonomiske situasjonen for stønadsmottaker eller ektefelle/partner/samboer?',
+        'Er det endringer i den økonomiske situasjonen for bruker eller ektefelle/partner/samboer?',
     'økonomiskSituasjon.hjelpetekst':
         'Husk dokumentasjon som viser bruttoutbetalinger (f.eks. endring av utenlandsk/privat pensjon/formue).',
 };
