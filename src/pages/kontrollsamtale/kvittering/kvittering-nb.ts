94c0bdb282f0eb6ad3c5b4b5188c7a4c1c280a83
sevdeoguz <sevde.oguz@nav.no>
2026-07-14 12:29:39 +0200

    fjerner modal og legger til forklaring (#4544)

--- src/pages/kontrollsamtale/kvittering/kvittering-nb.ts
+++ src/pages/kontrollsamtale/kvittering/kvittering-nb.ts
@@ -1,4 +1,6 @@
 export default {
     'heading.kontrollNotatForNavnErMottatt': 'Skjema for {navn} er mottatt.',
     'heading.saksnummer': 'Saksnummer: {saksnummer}',
+
+    'info.skjema': 'Skriv ut kontrollsamtalenotatet og signer før du sender det til skanning.',
 };
