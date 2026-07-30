788da8a208088397530d4de6a29eb660c137ee47
Bjorn Golberg <bjorn.golberg@gmail.com>
2026-07-30 17:35:58 +0200

    Lukk og avslag søknad må vise feilmelding fra api

--- src/pages/saksbehandling/avsluttBehandling/lukkSøknad/AvslagForm.tsx
+++ src/pages/saksbehandling/avsluttBehandling/lukkSøknad/AvslagForm.tsx
@@ -2,6 +2,7 @@ import * as RemoteData from '@devexperts/remote-data-ts';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Controller, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import ApiErrorAlert from '~src/components/apiErrorAlert/ApiErrorAlert.tsx';
 import * as SøknadActions from '~src/features/søknad/SøknadActions.ts';
 import { useAsyncActionCreator } from '~src/lib/hooks.ts';
 import { useI18n } from '~src/lib/i18n.ts';
@@ -64,6 +65,7 @@ export const AvslagForm = (props: { søknad: Søknad; sakId: string }) => {
                     />
                 )}
             />
+            {RemoteData.isFailure(søknadLukketStatus) && <ApiErrorAlert error={søknadLukketStatus.error} />}
             <AvsluttBehandlingBunnknapper
                 sakId={sakId}
                 submitButtonText={formatMessage('knapp.lukkSøknad')}
