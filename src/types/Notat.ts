74cd3a4b7e2f6a633a2bbeda5025e49e925a75a7
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2026-08-11 12:45:27 +0200

    Ny type

--- src/types/Notat.ts
+++ src/types/Notat.ts
@@ -3,7 +3,8 @@ export type Tidspunkt = string;
 export type NavIdentBruker = string;
 
 export enum ReferanseType {
-    SØKNAD = 'SØKNAD',
+    SØKNAD = 'SØKNAD', //Søknadsbehandling
+    INNSENDT_SØKNAD = 'INNSENDT_SØKNAD', //Innsendt søknad uten behandling
     REVURDERING = 'REVURDERING',
 }
 
