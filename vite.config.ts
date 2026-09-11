3f757d5e4504c5d95f38561a18d40f78f30e45dd
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2026-09-11 11:03:37 +0200

    Fiks vite warn

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                app: './src/index.html',
+                app: './index.html',
             },
         },
         outDir: '../dist',
