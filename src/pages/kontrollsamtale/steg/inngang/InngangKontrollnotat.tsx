59a75c2c291abdd2909fa6fba2adb1c495c96b82
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2026-09-21 14:55:04 +0200

    oppdater melding

--- src/pages/kontrollsamtale/steg/inngang/InngangKontrollnotat.tsx
+++ src/pages/kontrollsamtale/steg/inngang/InngangKontrollnotat.tsx
@@ -140,8 +140,8 @@ const InngangKontrollnotat = () => {
             </div>
             {RemoteData.isSuccess(hentSakStatus) && !kanStarteBasertPåInnkallingsdato && (
                 <Alert variant={'warning'}>
-                    Personen har per nå ingen registrert planlagt kontrollsamtale. Dette må registreres før du kan
-                    opprette kontrollnotat.
+                    Personen har per nå ingen registrert åpen kontrollsamtale. Dette må registreres før du kan opprette
+                    kontrollnotat.
                 </Alert>
             )}
             <div className={styles.knapperContainer}>
