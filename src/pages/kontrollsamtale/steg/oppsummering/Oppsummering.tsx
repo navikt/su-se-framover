bc65ce42f2fb41da39c2bec8ed8d8f863afbe1a5
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2026-09-22 13:37:01 +0200

    Fjern redux og KISS

--- src/pages/kontrollsamtale/steg/oppsummering/Oppsummering.tsx
+++ src/pages/kontrollsamtale/steg/oppsummering/Oppsummering.tsx
@@ -2,8 +2,10 @@ import * as RemoteData from '@devexperts/remote-data-ts';
 import { Textarea } from '@navikt/ds-react';
 import { useNavigate, useParams } from 'react-router-dom';
 import KontrollsamtaleOppsummering from 'src/pages/kontrollsamtale/steg/oppsummering/components/Kontrollsamtaleoppsummering/KontrollsamtaleOppsummering.tsx';
+import * as kontrollsamtaleApi from '~src/api/kontrollsamtaleApi.ts';
+import ApiErrorAlert from '~src/components/apiErrorAlert/ApiErrorAlert.tsx';
 import { fritekstUpdated } from '~src/features/kontrollsamtale/kontrollsamtale.slice.ts';
-import { sendKontrollsamtaleNotat } from '~src/features/søknad/innsending.slice.ts';
+import { useApiCall } from '~src/lib/hooks.ts';
 import { useI18n } from '~src/lib/i18n.ts';
 import messages from '~src/pages/kontrollsamtale/steg/oppsummering/oppsummering-nb.ts';
 import Bunnknapper from '~src/pages/søknad/bunnknapper/Bunnknapper.tsx';
@@ -21,16 +23,14 @@ const Oppsummering = ({ forrigeUrl, nesteUrl, avbrytUrl }: Props) => {
     const { formatMessage } = useI18n({ messages: { ...messages } });
     const dispatch = useAppDispatch();
     const kontrollsamtale = useAppSelector((state) => state.kontrollsamtale);
-    const kontrollsamtaleNotat = useAppSelector((state) => state.innsending.kontrollsamtaleNotat);
+    const [sendKontrollsamtaleNotatStatus, sendKontrollsamtaleNotat] = useApiCall(
+        kontrollsamtaleApi.lagreKontrollsamtaleNotat,
+    );
     const { sakId } = useParams<{
         sakId: string;
     }>();
 
-    const onSubmit = async () => {
-        if (RemoteData.isPending(kontrollsamtaleNotat)) {
-            return;
-        }
-
+    const onSubmit = () => {
         if (!sakId) {
             throw new Error('Mangler sakId');
         }
@@ -50,8 +50,8 @@ const Oppsummering = ({ forrigeUrl, nesteUrl, avbrytUrl }: Props) => {
             return;
         }
 
-        const resultat = await dispatch(
-            sendKontrollsamtaleNotat({
+        sendKontrollsamtaleNotat(
+            {
                 sakId: sakId,
                 personligOppmøte: kontrollsamtale.personligOppmøte,
                 fullmaktOgLegeerklæring: kontrollsamtale.fullmaktOgLegeerklæring,
@@ -72,12 +72,9 @@ const Oppsummering = ({ forrigeUrl, nesteUrl, avbrytUrl }: Props) => {
                 andreForhold: kontrollsamtale.andreForhold,
                 skatteOpplysninger: kontrollsamtale.skatteOpplysninger,
                 fritekst: kontrollsamtale.fritekst?.trim() ? kontrollsamtale.fritekst.trim() : null,
-            }),
+            },
+            () => navigate(nesteUrl),
         );
-
-        if (sendKontrollsamtaleNotat.fulfilled.match(resultat)) {
-            navigate(nesteUrl);
-        }
     };
 
     return (
@@ -109,12 +106,15 @@ const Oppsummering = ({ forrigeUrl, nesteUrl, avbrytUrl }: Props) => {
                     }}
                     next={{
                         label: formatMessage('sendInnSkjema'),
-                        spinner: RemoteData.isPending(kontrollsamtaleNotat),
+                        spinner: RemoteData.isPending(sendKontrollsamtaleNotatStatus),
                     }}
                     avbryt={{
                         toRoute: avbrytUrl,
                     }}
                 />
+                {RemoteData.isFailure(sendKontrollsamtaleNotatStatus) && (
+                    <ApiErrorAlert error={sendKontrollsamtaleNotatStatus.error} />
+                )}
             </div>
         </form>
     );
